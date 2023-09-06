ea306d36f2cd70e9fbf05140258b08e7220aae2e
Redm4x <max_maxime_max@hotmail.com>
2023-09-06 16:15:24 -0400

    Update indexer ibc denom for usdc

--- indexer/src/indexers/akashStatsIndexer.ts
+++ indexer/src/indexers/akashStatsIndexer.ts
@@ -35,7 +35,7 @@ class ITotalResources {
 const denomMapping = {
   uakt: "uakt",
   "ibc/12C6A0C374171B595A0A9E18B83FA09D295FB1F2D8C6DAA3AC28683471752D84": "uusdc", // USDC on Sandbox
-  "ibc/F8D2BEE806C3F969B256416B37FF6E7E0B927F372993114B8E0B9E57DC8CD327": "uusdc", // USDC on Mainnet
+  "ibc/170C677610AC31DF0904FFE09CD3B5C657492170E7E52372E48756B71E56F2F1": "uusdc", // USDC on Mainnet
 };
 
 export class AkashStatsIndexer extends Indexer {
