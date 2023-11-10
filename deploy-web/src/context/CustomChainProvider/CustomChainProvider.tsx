650a07e019c45d620353520dbaa7eec7e03f4f72
Redm4x <2829180+Redm4x@users.noreply.github.com>
2023-11-10 14:35:05 -0500

    Use custom node

--- deploy-web/src/context/CustomChainProvider/CustomChainProvider.tsx
+++ deploy-web/src/context/CustomChainProvider/CustomChainProvider.tsx
@@ -4,23 +4,33 @@ import { wallets as leap } from "@cosmos-kit/leap-extension";
 import { wallets as cosmostation } from "@cosmos-kit/cosmostation";
 import { customRegistry } from "@src/utils/customRegistry";
 import { GasPrice } from "@cosmjs/stargate";
-import { akash, akashAssetList, akashSandbox, akashSandboxAssetList } from "@src/chains";
+import { akash, akashAssetList, akashSandbox, akashSandboxAssetList, akashTestnet, akashTestnetAssetList } from "@src/chains";
 import { useSelectedNetwork } from "@src/hooks/useSelectedNetwork";
 import { useChain } from "@cosmos-kit/react";
+import { useSettings } from "../SettingsProvider";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export function CustomChainProvider({ children }: Props) {
+  const { settings } = useSettings();
+
   return (
     <ChainProvider
-      chains={[akash, akashSandbox]}
-      assetLists={[akashAssetList, akashSandboxAssetList]}
+      chains={[akash, akashSandbox, akashTestnet]}
+      assetLists={[akashAssetList, akashSandboxAssetList, akashTestnetAssetList]}
       wallets={[...keplr, ...leap, ...cosmostation]}
       walletConnectOptions={{
         signClient: {
           projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
+        }
+      }}
+      endpointOptions={{
+        endpoints: {
+          akash: { rest: [settings.apiEndpoint], rpc: [settings.rpcEndpoint] },
+          "akash-sandbox": { rest: [settings.apiEndpoint], rpc: [settings.rpcEndpoint] },
+          "akash-testnet": { rest: [settings.apiEndpoint], rpc: [settings.rpcEndpoint] }
         }
       }}
       signerOptions={{
