880fbdfe731734e8e85824d98ca0328f2c50388e
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2023-11-10 11:11:05 -0500

    features/add-tailwind (#47)
    * added tailwind css config
    
    * added prettier plugin
    
    * added shadcn
    
    * remove unused import

--- deploy-web/src/pages/_app.tsx
+++ deploy-web/src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { CustomSnackbarProvider } from "@src/context/CustomSnackbarProvider";
 import { AppProps } from "next/app";
 import withDarkMode from "next-dark-mode";
 import "../styles/index.css";
+import "../styles/globals.css";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@src/queries";
 import { WalletProvider } from "@src/context/WalletProvider";
@@ -26,8 +27,14 @@ import { Provider } from "jotai";
 import { usePreviousRoute } from "@src/hooks/usePreviousRoute";
 import { PageHead } from "@src/components/layout/PageHead";
 import { CustomChainProvider } from "@src/context/CustomChainProvider";
-
 import "@interchain-ui/react/styles";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@src/@/lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
 
 interface Props extends AppProps {
   emotionCache?: EmotionCache;
@@ -63,7 +70,7 @@ const App: React.FunctionComponent<Props> = ({ Component, pageProps, emotionCach
   }, []);
 
   return (
-    <>
+    <main className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>
       <PageHead />
 
       <CacheProvider value={emotionCache}>
@@ -98,7 +105,7 @@ const App: React.FunctionComponent<Props> = ({ Component, pageProps, emotionCach
           </Provider>
         </QueryClientProvider>
       </CacheProvider>
-    </>
+    </main>
   );
 };
 
