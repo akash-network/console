71b68c6e8d320ff0b74fb437fa43da8d82a48380
Redm4x <2829180+Redm4x@users.noreply.github.com>
2023-11-13 10:30:20 -0500

    Decrease tracesSampleRate

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -36,7 +36,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 0.1
+  tracesSampleRate: 0.01
 });
 
 Sentry.setTag("chain", env.ActiveChain);
