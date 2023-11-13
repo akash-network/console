6c522f30679c3bdf4de89c18eb57eaf1b381325f
Maxime Cyr <2829180+Redm4x@users.noreply.github.com>
2023-11-13 10:52:43 -0500

    Decrease tracesSampleRate (#51)

--- api/src/index.ts
+++ api/src/index.ts
@@ -43,7 +43,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 0.1
+  tracesSampleRate: 0.01
 });
 
 const scheduler = new Scheduler({
