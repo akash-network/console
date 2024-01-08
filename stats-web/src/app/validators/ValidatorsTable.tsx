a853be1ab0c87f82724fa0b782c52da73886f519
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-01-08 14:48:40 -0500

    added validator detail page

--- stats-web/src/app/validators/ValidatorsTable.tsx
+++ stats-web/src/app/validators/ValidatorsTable.tsx
@@ -12,14 +12,7 @@ export function ValidatorsTable({}: IProps) {
 
   return (
     <div>
-      {validators?.length === 0 ? (
-        <div className="flex items-center p-4">
-          <SearchX size="1rem" />
-          &nbsp;This address has no deployments
-        </div>
-      ) : (
-        <DataTable data={validators || []} columns={columns} noResultsText="This address has no deployments." isLoading={isLoading} initialPageSize={1000} />
-      )}
+      <DataTable data={validators || []} columns={columns} noResultsText="This address has no deployments." isLoading={isLoading} initialPageSize={1000} />
     </div>
   );
 }
