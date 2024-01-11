b60a4ba11badfa8449442ad0bc782fe06c35d7bb
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-01-10 20:25:41 -0500

    fix table border color

--- stats-web/src/components/ui/table.tsx
+++ stats-web/src/components/ui/table.tsx
@@ -25,7 +25,7 @@ const TableFooter = React.forwardRef<HTMLTableSectionElement, React.HTMLAttribut
 TableFooter.displayName = "TableFooter";
 
 const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTMLTableRowElement>>(({ className, ...props }, ref) => (
-  <tr ref={ref} className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)} {...props} />
+  <tr ref={ref} className={cn("border-b border-b-muted-foreground/20 transition-colors hover:bg-muted/100 data-[state=selected]:bg-muted", className)} {...props} />
 ));
 TableRow.displayName = "TableRow";
 
