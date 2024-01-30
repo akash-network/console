eb3e716780f6dd006923d7533161848d78c2e53d
Redm4x <2829180+Redm4x@users.noreply.github.com>
2024-01-29 20:19:08 -0500

    Set correct swagger servers

--- api/src/routers/apiRouter.ts
+++ api/src/routers/apiRouter.ts
@@ -1,17 +1,21 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 import routesV1 from "../routes/v1";
+import { isProd } from "@src/utils/constants";
 
 export const apiRouter = new OpenAPIHono();
 
 function registerApiVersion(version: string, baseRouter: OpenAPIHono, versionRoutes: OpenAPIHono[]) {
   const versionRouter = new OpenAPIHono();
+
+  let servers = [{ url: `https://api.cloudmos.io/${version}`, description: "Production" }];
+  if (!isProd) {
+    servers.unshift({ url: `http://localhost:3080/${version}`, description: "Localhost" });
+  }
+
   versionRouter.doc(`/doc`, {
     openapi: "3.0.0",
-    servers: [
-      { url: `http://localhost:3080/${version}`, description: "Localhost" },
-      { url: `https://api-preview.cloudmos.io/${version}`, description: "Online Preview" }
-    ],
+    servers: servers,
     info: {
       title: "Cloudmos API",
       description: "Access Akash data from our indexer",
@@ -21,7 +25,7 @@ function registerApiVersion(version: string, baseRouter: OpenAPIHono, versionRou
   const swaggerInstance = swaggerUI({ url: `/${version}/doc` });
 
   versionRouter.get(`/swagger`, swaggerInstance);
-  versionRouter.get(`/swagger/`, swaggerInstance);
+  versionRouter.get(`/swagger/`, (c) => c.redirect(`/${version}/swagger`));
 
   versionRoutes.forEach((route) => versionRouter.route(`/`, route));
   baseRouter.route(`/${version}`, versionRouter);
