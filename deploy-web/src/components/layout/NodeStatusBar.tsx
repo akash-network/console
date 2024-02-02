8638043bf8f51d31472c32347d1572d432a24ea6
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-02-02 14:12:51 -0500

    fix linear loading

--- deploy-web/src/components/layout/NodeStatusBar.tsx
+++ deploy-web/src/components/layout/NodeStatusBar.tsx
@@ -8,30 +8,7 @@ import { LinearLoadingSkeleton } from "../shared/LinearLoadingSkeleton";
 import { useSelectedNetwork } from "@src/hooks/useSelectedNetwork";
 import { Button } from "../ui/button";
 
-// const useStyles = makeStyles()(theme => ({
-//   link: {
-//     "&&": {
-//       width: "100%",
-//       backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey[900] : theme.palette.grey[100],
-//       textDecoration: "none",
-//       color: "inherit",
-//       display: "inline-flex",
-//       fontSize: ".75rem",
-//       alignItems: "center",
-//       justifyContent: "center",
-//       padding: ".5rem",
-//       borderRadius: "4px",
-//       transition: "all .3s ease",
-//       "&:hover": {
-//         backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["300"]
-//       }
-//     }
-//   }
-// }));
-
 export const NodeStatusBar = () => {
-  // const { classes } = useStyles();
-  // const theme = useTheme();
   const { settings, isRefreshingNodeStatus } = useSettings();
   const { selectedNode, isCustomNode, customNode } = settings;
   const router = useRouter();
@@ -47,13 +24,10 @@ export const NodeStatusBar = () => {
       <LinearLoadingSkeleton isLoading={isRefreshingNodeStatus} />
       <div className="flex items-center justify-center">
         {shownNode && (
-          <Button size="sm" className="w-full" variant="secondary" onClick={() => router.push(UrlService.settings())}>
+          <Button size="sm" className="w-full" variant="outline" onClick={() => router.push(UrlService.settings())}>
             <div className="ml-2">{shownNode?.id?.length > 15 ? getSplitText(shownNode?.id, 0, 15) : shownNode?.id}</div>
 
-            <div
-              className="ml-2 text-xs"
-              // sx={{ marginLeft: ".5rem", fontSize: ".5rem !important" }}
-            >
+            <div className="ml-2 text-xs">
               <NodeStatus latency={Math.floor(shownNode?.latency)} status={shownNode?.status} variant="dense" />
             </div>
           </Button>
--- deploy-web/src/components/shared/LinearLoadingSkeleton.tsx
+++ deploy-web/src/components/shared/LinearLoadingSkeleton.tsx
@@ -13,14 +13,20 @@ export function LinearLoadingSkeleton({ isLoading }: IProps) {
     const timer = setInterval(() => {
       setProgress(prev => {
         const rest = 100 - prev;
-        const increment = Math.random() * rest;
-        const newProgress = prev + Math.max(increment, 13);
-        return newProgress >= 100 ? 0 : newProgress;
+        const maxPercent = prev > 75 ? 10 : 35;
+        const randomPercent = randomIntFromInterval(1, maxPercent);
+        const increment = (randomPercent / 100) * rest;
+        const newProgress = Math.min(prev + increment, 100);
+        return newProgress;
       });
     }, 500);
 
     return () => clearInterval(timer);
   }, []);
+
+  function randomIntFromInterval(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
   return <>{isLoading ? <Progress className="h-[4px] w-full" value={progress} color="secondary" /> : <div className="h-[4px] w-full" />}</>;
 }
