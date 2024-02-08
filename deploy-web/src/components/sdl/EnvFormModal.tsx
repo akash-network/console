76f8fe452396116781fa87e125b91d0503f57dbf
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-02-08 15:13:35 -0500

    Env form modal refactor

--- deploy-web/src/components/sdl/EnvFormModal.tsx
+++ deploy-web/src/components/sdl/EnvFormModal.tsx
@@ -1,12 +1,16 @@
+"use client";
 import { ReactNode, useEffect } from "react";
-import { makeStyles } from "tss-react/mui";
 import { Popup } from "../shared/Popup";
 import { Control, Controller, useFieldArray } from "react-hook-form";
-import { Box, IconButton, Paper, Switch, TextField, Typography, useTheme } from "@mui/material";
 import { EnvironmentVariable, RentGpusFormValues, SdlBuilderFormValues } from "@src/types";
-import DeleteIcon from "@mui/icons-material/Delete";
 import { nanoid } from "nanoid";
-import { CustomTooltip } from "../shared/CustomTooltip";
+import { Card, CardContent } from "../ui/card";
+import { cn } from "@src/utils/styleUtils";
+import { FormInput, Input } from "../ui/input";
+import { Bin } from "iconoir-react";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Switch } from "../ui/switch";
 
 type Props = {
   serviceIndex: number;
@@ -17,18 +21,16 @@ type Props = {
   children?: ReactNode;
 };
 
-const useStyles = makeStyles()(theme => ({
-  formControl: {
-    marginBottom: theme.spacing(1.5)
-  },
-  textField: {
-    width: "100%"
-  }
-}));
+// const useStyles = makeStyles()(theme => ({
+//   formControl: {
+//     marginBottom: theme.spacing(1.5)
+//   },
+//   textField: {
+//     width: "100%"
+//   }
+// }));
 
 export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceIndex, envs: _envs, onClose, hasSecretOption = true }) => {
-  const { classes } = useStyles();
-  const theme = useTheme();
   const {
     fields: envs,
     remove: removeEnv,
@@ -40,17 +42,17 @@ export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceI
   });
 
   useEffect(() => {
-    if (open && _envs.length === 0) {
+    if (_envs.length === 0) {
       onAddEnv();
     }
-  }, [open, _envs]);
+  }, []);
 
   const onAddEnv = () => {
     appendEnv({ id: nanoid(), key: "", value: "", isSecret: false });
   };
 
   const _onClose = () => {
-    const _envToRemove = [];
+    const _envToRemove: number[] = [];
 
     _envs.forEach((e, i) => {
       if (!e.key.trim()) {
@@ -73,14 +75,14 @@ export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceI
         {
           label: "Close",
           color: "primary",
-          variant: "text",
+          variant: "ghost",
           side: "left",
           onClick: _onClose
         },
         {
           label: "Add Variable",
           color: "secondary",
-          variant: "contained",
+          variant: "default",
           side: "right",
           onClick: onAddEnv
         }
@@ -89,90 +91,107 @@ export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceI
       maxWidth="sm"
       enableCloseOnBackdropClick
     >
-      <Box sx={{ paddingTop: "1rem" }}>
+      <div className="pt-4">
         {envs.map((env, envIndex) => {
           return (
-            <Paper key={env.id} elevation={2} sx={{ display: "flex", marginBottom: envIndex + 1 === envs.length ? 0 : ".5rem" }}>
-              <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center", flexDirection: { xs: "column", sm: "row" } }}>
-                <Controller
-                  control={control}
-                  name={`services.${serviceIndex}.env.${envIndex}.key`}
-                  render={({ field }) => (
-                    <TextField
-                      type="text"
-                      variant="outlined"
-                      label="Key"
-                      color="secondary"
-                      fullWidth
-                      value={field.value}
-                      size="small"
-                      onChange={event => field.onChange(event.target.value)}
-                    />
-                  )}
-                />
-
-                <Controller
-                  control={control}
-                  name={`services.${serviceIndex}.env.${envIndex}.value`}
-                  render={({ field }) => (
-                    <TextField
-                      sx={{ marginLeft: ".5rem" }}
-                      type="text"
-                      variant="outlined"
-                      label="Value"
-                      color="secondary"
-                      fullWidth
-                      value={field.value}
-                      size="small"
-                      onChange={event => field.onChange(event.target.value)}
-                    />
-                  )}
-                />
-              </Box>
-
-              <Box
-                sx={{
-                  paddingLeft: ".5rem",
-                  display: "flex",
-                  alignItems: "center",
-                  flexDirection: "column",
-                  justifyContent: envIndex > 0 ? "space-around" : "flex-end",
-                  width: "45px"
-                }}
-              >
-                {envIndex > 0 && (
-                  <IconButton onClick={() => removeEnv(envIndex)} size="small">
-                    <DeleteIcon />
-                  </IconButton>
-                )}
-
-                {hasSecretOption && (
+            <Card
+              key={env.id}
+              className={cn("flex", { ["mb-2"]: envIndex + 1 !== envs.length })}
+              // sx={{ display: "flex", marginBottom: envIndex + 1 === envs.length ? 0 : ".5rem" }}
+            >
+              <CardContent>
+                <div
+                  className="flex flex-grow flex-col items-center sm:flex-row"
+                  // sx={{ flexGrow: 1, display: "flex", alignItems: "center", flexDirection: { xs: "column", sm: "row" } }}
+                >
                   <Controller
                     control={control}
-                    name={`services.${serviceIndex}.env.${envIndex}.isSecret`}
+                    name={`services.${serviceIndex}.env.${envIndex}.key`}
                     render={({ field }) => (
-                      <CustomTooltip
-                        title={
-                          <>
-                            <Typography variant="body1">
-                              <strong>Secret</strong>
-                            </Typography>
-                            <Typography variant="body2">
-                              This is for secret variables containing sensitive information you don't want to be saved in your template.
-                            </Typography>
-                          </>
-                        }
-                      >
-                        <Switch checked={field.value || false} onChange={field.onChange} color="secondary" size="small" sx={{ margin: 0 }} />
-                      </CustomTooltip>
+                      <FormInput
+                        type="text"
+                        // variant="outlined"
+                        label="Key"
+                        color="secondary"
+                        // fullWidth
+                        value={field.value}
+                        // size="small"
+                        onChange={event => field.onChange(event.target.value)}
+                      />
                     )}
                   />
-                )}
-              </Box>
-            </Paper>
+
+                  <Controller
+                    control={control}
+                    name={`services.${serviceIndex}.env.${envIndex}.value`}
+                    render={({ field }) => (
+                      <FormInput
+                        className="ml-2"
+                        // sx={{ marginLeft: ".5rem" }}
+                        type="text"
+                        // variant="outlined"
+                        label="Value"
+                        color="secondary"
+                        // fullWidth
+                        value={field.value}
+                        // size="small"
+                        onChange={event => field.onChange(event.target.value)}
+                      />
+                    )}
+                  />
+                </div>
+
+                <div
+                  className={cn("flex w-[45px] flex-col items-center pl-2", { ["justify-around"]: envIndex > 0, ["justify-end"]: envIndex === 0 })}
+                  // sx={{
+                  //   paddingLeft: ".5rem",
+                  //   display: "flex",
+                  //   alignItems: "center",
+                  //   flexDirection: "column",
+                  //   justifyContent: envIndex > 0 ? "space-around" : "flex-end",
+                  //   width: "45px"
+                  // }}
+                >
+                  {envIndex > 0 && (
+                    <Button onClick={() => removeEnv(envIndex)} size="icon">
+                      <Bin />
+                    </Button>
+                  )}
+
+                  {hasSecretOption && (
+                    <Controller
+                      control={control}
+                      name={`services.${serviceIndex}.env.${envIndex}.isSecret`}
+                      render={({ field }) => (
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Switch
+                              checked={field.value || false}
+                              onChange={field.onChange}
+                              // color="secondary"
+                              // size="small" sx={{ margin: 0 }}
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <>
+                              <p>
+                                <strong>Secret</strong>
+                              </p>
+                              <p className="text-sm">
+                                This is for secret variables containing sensitive information you don't want to be saved in your template.
+                              </p>
+                            </>
+                          </TooltipContent>
+                        </Tooltip>
+                      )}
+                    />
+                  )}
+                </div>
+              </CardContent>
+            </Card>
           );
         })}
-      </Box>
+      </div>
     </Popup>
   );
 };
--- deploy-web/src/components/ui/input.tsx
+++ deploy-web/src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 "use client";
 import * as React from "react";
 import { cn } from "@src/utils/styleUtils";
+import { FormControl, FormItem, FormLabel } from "./form";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
@@ -19,14 +20,38 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
 });
 Input.displayName = "Input";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(({ className, type, label, ...props }, ref) => {
+  return (
+    <FormItem>
+      <FormLabel>{label}</FormLabel>
+      <FormControl>
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </FormControl>
+    </FormItem>
+  );
+});
+FormInput.displayName = "FormInput";
+
+export interface InputWithIconProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   startIcon?: React.ReactNode;
   endIcon: React.ReactNode;
   error?: string;
 }
 
-const InputWithIcon = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, label, startIcon, endIcon, error, ...props }, ref) => {
+const InputWithIcon = React.forwardRef<HTMLInputElement, InputWithIconProps>(({ className, type, label, startIcon, endIcon, error, ...props }, ref) => {
   return (
     <div>
       <label htmlFor={`${label}-input`} className="block text-sm font-medium leading-6 text-gray-900">
@@ -56,4 +81,4 @@ const InputWithIcon = React.forwardRef<HTMLInputElement, InputProps>(({ classNam
 });
 InputWithIcon.displayName = "InputWithIcon";
 
-export { Input, InputWithIcon };
+export { FormInput, Input, InputWithIcon };
