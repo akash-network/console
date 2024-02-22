eb1cf1f35a668a01ae1fd8f6e8287f7ed8843ab6
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-02-22 14:49:45 -0500

    end form modal

--- deploy-web/src/components/sdl/EnvFormModal.tsx
+++ deploy-web/src/components/sdl/EnvFormModal.tsx
@@ -4,13 +4,12 @@ import { Popup } from "../shared/Popup";
 import { Control, Controller, useFieldArray } from "react-hook-form";
 import { EnvironmentVariable, RentGpusFormValues, SdlBuilderFormValues } from "@src/types";
 import { nanoid } from "nanoid";
-import { Card, CardContent } from "../ui/card";
 import { cn } from "@src/utils/styleUtils";
-import { FormInput, Input } from "../ui/input";
+import { FormInput } from "../ui/input";
 import { Bin } from "iconoir-react";
 import { Button } from "../ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { Switch } from "../ui/switch";
+import { CustomTooltip } from "../shared/CustomTooltip";
 
 type Props = {
   serviceIndex: number;
@@ -20,15 +19,6 @@ type Props = {
   hasSecretOption?: boolean;
   children?: ReactNode;
 };
-
-// const useStyles = makeStyles()(theme => ({
-//   formControl: {
-//     marginBottom: theme.spacing(1.5)
-//   },
-//   textField: {
-//     width: "100%"
-//   }
-// }));
 
 export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceIndex, envs: _envs, onClose, hasSecretOption = true }) => {
   const {
@@ -91,104 +81,67 @@ export const EnvFormModal: React.FunctionComponent<Props> = ({ control, serviceI
       maxWidth="sm"
       enableCloseOnBackdropClick
     >
-      <div className="pt-4">
+      <div>
         {envs.map((env, envIndex) => {
           return (
-            <Card
-              key={env.id}
-              className={cn("flex", { ["mb-2"]: envIndex + 1 !== envs.length })}
-              // sx={{ display: "flex", marginBottom: envIndex + 1 === envs.length ? 0 : ".5rem" }}
-            >
-              <CardContent>
-                <div
-                  className="flex flex-grow flex-col items-center sm:flex-row"
-                  // sx={{ flexGrow: 1, display: "flex", alignItems: "center", flexDirection: { xs: "column", sm: "row" } }}
-                >
+            <div key={env.id} className={cn("flex", { ["mb-2"]: envIndex + 1 !== envs.length })}>
+              <div className="flex flex-grow flex-col items-end sm:flex-row">
+                <Controller
+                  control={control}
+                  name={`services.${serviceIndex}.env.${envIndex}.key`}
+                  render={({ field }) => (
+                    <FormInput type="text" label="Key" color="secondary" value={field.value} onChange={event => field.onChange(event.target.value)} />
+                  )}
+                />
+
+                <Controller
+                  control={control}
+                  name={`services.${serviceIndex}.env.${envIndex}.value`}
+                  render={({ field }) => (
+                    <div className="ml-2">
+                      <FormInput type="text" label="Value" color="secondary" value={field.value} onChange={event => field.onChange(event.target.value)} />
+                    </div>
+                  )}
+                />
+              </div>
+
+              <div className={cn("flex w-[50px] flex-col items-start pl-2", { ["justify-between"]: envIndex > 0, ["justify-end"]: envIndex === 0 })}>
+                {envIndex > 0 && (
+                  <Button onClick={() => removeEnv(envIndex)} size="icon" variant="ghost">
+                    <Bin />
+                  </Button>
+                )}
+
+                {hasSecretOption && (
                   <Controller
                     control={control}
-                    name={`services.${serviceIndex}.env.${envIndex}.key`}
+                    name={`services.${serviceIndex}.env.${envIndex}.isSecret`}
                     render={({ field }) => (
-                      <FormInput
-                        type="text"
-                        // variant="outlined"
-                        label="Key"
-                        color="secondary"
-                        // fullWidth
-                        value={field.value}
-                        // size="small"
-                        onChange={event => field.onChange(event.target.value)}
-                      />
+                      <CustomTooltip
+                        title={
+                          <>
+                            <p>
+                              <strong>Secret</strong>
+                            </p>
+                            <p className="text-sm">
+                              This is for secret variables containing sensitive information you don't want to be saved in your template.
+                            </p>
+                          </>
+                        }
+                      >
+                        <Switch
+                          checked={field.value || false}
+                          onCheckedChange={field.onChange}
+                          color="primary"
+                          // color="secondary"
+                          // size="small" sx={{ margin: 0 }}
+                        />
+                      </CustomTooltip>
                     )}
                   />
-
-                  <Controller
-                    control={control}
-                    name={`services.${serviceIndex}.env.${envIndex}.value`}
-                    render={({ field }) => (
-                      <FormInput
-                        className="ml-2"
-                        // sx={{ marginLeft: ".5rem" }}
-                        type="text"
-                        // variant="outlined"
-                        label="Value"
-                        color="secondary"
-                        // fullWidth
-                        value={field.value}
-                        // size="small"
-                        onChange={event => field.onChange(event.target.value)}
-                      />
-                    )}
-                  />
-                </div>
-
-                <div
-                  className={cn("flex w-[45px] flex-col items-center pl-2", { ["justify-around"]: envIndex > 0, ["justify-end"]: envIndex === 0 })}
-                  // sx={{
-                  //   paddingLeft: ".5rem",
-                  //   display: "flex",
-                  //   alignItems: "center",
-                  //   flexDirection: "column",
-                  //   justifyContent: envIndex > 0 ? "space-around" : "flex-end",
-                  //   width: "45px"
-                  // }}
-                >
-                  {envIndex > 0 && (
-                    <Button onClick={() => removeEnv(envIndex)} size="icon">
-                      <Bin />
-                    </Button>
-                  )}
-
-                  {hasSecretOption && (
-                    <Controller
-                      control={control}
-                      name={`services.${serviceIndex}.env.${envIndex}.isSecret`}
-                      render={({ field }) => (
-                        <Tooltip>
-                          <TooltipTrigger asChild>
-                            <Switch
-                              checked={field.value || false}
-                              onChange={field.onChange}
-                              // color="secondary"
-                              // size="small" sx={{ margin: 0 }}
-                            />
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            <>
-                              <p>
-                                <strong>Secret</strong>
-                              </p>
-                              <p className="text-sm">
-                                This is for secret variables containing sensitive information you don't want to be saved in your template.
-                              </p>
-                            </>
-                          </TooltipContent>
-                        </Tooltip>
-                      )}
-                    />
-                  )}
-                </div>
-              </CardContent>
-            </Card>
+                )}
+              </div>
+            </div>
           );
         })}
       </div>
