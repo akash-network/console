8f6ab9e443fe38ed668bea6142fa765d43db661c
Maxime Cyr <2829180+Redm4x@users.noreply.github.com>
2024-02-28 12:54:17 -0500

    Fix provider status tracking bugs (#112)
    * Add timeout to grpc calls
    
    * Move snapshot saving logic to providerStatusProvider
    
    * Cap available quantities to 0 to prevent negative numbers
    
    * Cleanup
    
    * Rename function
    
    * Use bulk create for gpus&cpus
    
    * Fetch status of recently online providers first

--- indexer/src/providers/statusEndpointHandlers/rest.ts
+++ indexer/src/providers/statusEndpointHandlers/rest.ts
@@ -1,11 +1,9 @@
-import { Provider, ProviderSnapshot } from "@shared/dbSchemas/akash";
-import { sequelize } from "@src/db/dbConnection";
-import { toUTC } from "@src/shared/utils/date";
+import { Provider } from "@shared/dbSchemas/akash";
 import axios from "axios";
-import { isSameDay } from "date-fns";
 import https from "https";
+import { ProviderStatusInfo } from "./types";
 
-export async function fetchAndSaveProviderStats(provider: Provider, cosmosSdkVersion: string, version: string, timeout: number) {
+export async function fetchProviderStatusFromREST(provider: Provider, timeout: number): Promise<ProviderStatusInfo> {
   const httpsAgent = new https.Agent({
     rejectUnauthorized: false
   });
@@ -20,74 +18,26 @@ export async function fetchAndSaveProviderStats(provider: Provider, cosmosSdkVer
   const activeResources = sumResources(response.data.cluster.inventory.active);
   const pendingResources = sumResources(response.data.cluster.inventory.pending);
   const availableResources = sumResources(response.data.cluster.inventory.available);
-  const checkDate = toUTC(new Date());
 
-  await sequelize.transaction(async (t) => {
-    const createdSnapshot = await ProviderSnapshot.create(
-      {
-        owner: provider.owner,
-        isOnline: true,
-        isLastOfDay: true,
-        checkDate: checkDate,
-        deploymentCount: response.data.manifest.deployments,
-        leaseCount: response.data.cluster.leases,
-        activeCPU: activeResources.cpu,
-        activeGPU: activeResources.gpu,
-        activeMemory: activeResources.memory,
-        activeStorage: activeResources.storage,
-        pendingCPU: pendingResources.cpu,
-        pendingGPU: pendingResources.gpu,
-        pendingMemory: pendingResources.memory,
-        pendingStorage: pendingResources.storage,
-        availableCPU: availableResources.cpu,
-        availableGPU: availableResources.gpu,
-        availableMemory: availableResources.memory,
-        availableStorage: availableResources.storage
-      },
-      { transaction: t }
-    );
-
-    if (provider.lastSnapshot && isSameDay(provider.lastSnapshot.checkDate, checkDate)) {
-      await ProviderSnapshot.update(
-        {
-          isLastOfDay: false
-        },
-        {
-          where: { id: provider.lastSnapshot.id },
-          transaction: t
-        }
-      );
-    }
-
-    await Provider.update(
-      {
-        lastSnapshotId: createdSnapshot.id,
-        isOnline: true,
-        error: null,
-        lastCheckDate: checkDate,
-        cosmosSdkVersion: cosmosSdkVersion,
-        akashVersion: version,
-        deploymentCount: response.data.manifest.deployments,
-        leaseCount: response.data.cluster.leases,
-        activeCPU: activeResources.cpu,
-        activeGPU: activeResources.gpu,
-        activeMemory: activeResources.memory,
-        activeStorage: activeResources.storage,
-        pendingCPU: pendingResources.cpu,
-        pendingGPU: pendingResources.gpu,
-        pendingMemory: pendingResources.memory,
-        pendingStorage: pendingResources.storage,
-        availableCPU: availableResources.cpu,
-        availableGPU: availableResources.gpu,
-        availableMemory: availableResources.memory,
-        availableStorage: availableResources.storage
-      },
-      {
-        where: { owner: provider.owner },
-        transaction: t
-      }
-    );
-  });
+  return {
+    resources: {
+      deploymentCount: response.data.manifest.deployments,
+      leaseCount: response.data.cluster.leases,
+      activeCPU: activeResources.cpu,
+      activeGPU: activeResources.gpu,
+      activeMemory: activeResources.memory,
+      activeStorage: activeResources.storage,
+      pendingCPU: pendingResources.cpu,
+      pendingGPU: pendingResources.gpu,
+      pendingMemory: pendingResources.memory,
+      pendingStorage: pendingResources.storage,
+      availableCPU: availableResources.cpu,
+      availableGPU: availableResources.gpu,
+      availableMemory: availableResources.memory,
+      availableStorage: availableResources.storage
+    },
+    nodes: []
+  };
 }
 
 function sumResources(resources) {
