155074dcb484b4e81a7b6e9b5df50580cccc5159
Maxime Cyr <2829180+Redm4x@users.noreply.github.com>
2024-04-11 16:41:17 -0400

    GPU pricing endpoint improvements (#157)
    * Cap allocated to allocatable
    
    * Add weighted average + fix median
    
    * Change var name in median calculation

--- api/src/utils/math.ts
+++ api/src/utils/math.ts
@@ -13,3 +13,33 @@ export function uaktToAKT(amount: number, precision = 2) {
 export function udenomToDenom(amount: number, precision = 2) {
   return round(amount / 1_000_000, precision);
 }
+
+export function median(values: number[]): number {
+  if (values.length === 0) {
+    throw new Error("Input array is empty");
+  }
+
+  const sortedValues = [...values].sort((a, b) => a - b);
+
+  const half = Math.floor(sortedValues.length / 2);
+
+  return sortedValues.length % 2 === 0 ? (sortedValues[half - 1] + sortedValues[half]) / 2 : sortedValues[half];
+}
+
+export function average(values: number[]): number {
+  if (values.length === 0) {
+    throw new Error("Input array is empty");
+  }
+
+  return values.reduce((acc, x) => acc + x, 0) / values.length;
+}
+
+export function weightedAverage(values: { value: number; weight: number }[]): number {
+  if (values.length === 0) {
+    throw new Error("Input array is empty");
+  }
+
+  const totalWeight = values.map((x) => x.weight).reduce((acc, x) => acc + x, 0);
+
+  return values.map((x) => x.value * x.weight).reduce((acc, x) => acc + x, 0) / totalWeight;
+}
