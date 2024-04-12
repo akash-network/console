6938c88d5bffdb3ba33090ea6d571dfc55b76df9
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-04-12 11:36:01 +0200

    feat(api): use akash-api protobuf instead of akashjs (#184)

--- deploy-web/src/utils/proto/v1beta4.ts
+++ deploy-web/src/utils/proto/v1beta4.ts
@@ -1,2 +1,1 @@
-export { MsgCreateBid, MsgCloseBid } from "@akashnetwork/akashjs/build/protobuf/akash/market/v1beta4/bid";
-export { MsgCreateLease, MsgWithdrawLease, MsgCloseLease } from "@akashnetwork/akashjs/build/protobuf/akash/market/v1beta4/lease";
+export { MsgCreateBid, MsgCloseBid, MsgCreateLease, MsgWithdrawLease, MsgCloseLease } from "@akashnetwork/akash-api/akash/market/v1beta4";
