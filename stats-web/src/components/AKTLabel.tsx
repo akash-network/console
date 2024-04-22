f06cbc874040865e2a3e259b4623af944a5ec3ec
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-04-22 10:23:42 -0400

    feat(stats-web): ✨ added geist font + update nextjs (#165)

--- stats-web/src/components/AKTLabel.tsx
+++ stats-web/src/components/AKTLabel.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export const AKTLabel: React.FunctionComponent<Props> = ({}) => {
-  return <span className="text-sm font-semibold ml-1">AKT</span>;
+  return <span className="ml-1 text-sm font-normal">AKT</span>;
 };
