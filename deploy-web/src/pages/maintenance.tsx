a5638b60c59d765356e560e8fe15035539dc3048
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-04-24 18:29:43 -0400

    fix(deploy-web): fix maintenance page

--- deploy-web/src/pages/maintenance.tsx
+++ deploy-web/src/pages/maintenance.tsx
@@ -14,12 +14,12 @@ const Maintenance: React.FunctionComponent<Props> = ({}) => {
 
       <div className="container pb-8 pt-4 sm:pt-8">
         <div className="py-12 text-center">
-          <Title className="mb-4 text-2xl text-muted-foreground sm:text-5xl">Maintenance</Title>
+          <Title className="mb-2 text-2xl sm:text-5xl">Maintenance</Title>
 
-          <Title subTitle>We'll be right back!</Title>
+          <Title subTitle className="!font-normal">We'll be right back!</Title>
 
-          <div className="pt-8">
-            <Tools className="text-3xl" />
+          <div className="flex items-center justify-center pt-8">
+            <Tools className="text-4xl text-primary" />
           </div>
         </div>
       </div>
