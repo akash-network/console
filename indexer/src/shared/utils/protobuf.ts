710f725b8033b8c51cf53f91b8970915d67be1dc
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-07 20:01:15 +0200

    feat(api): use all the types in indexer
    refs akash-network/support#184

--- indexer/src/shared/utils/protobuf.ts
+++ indexer/src/shared/utils/protobuf.ts
@@ -1,7 +1,6 @@
 import { Registry, isTsProtoGeneratedType, GeneratedType } from "@cosmjs/proto-signing";
 import { defaultRegistryTypes } from "@cosmjs/stargate";
 import { MsgUnjail } from "cosmjs-types/cosmos/slashing/v1beta1/tx";
-import omit from "lodash/omit";
 
 import * as v1beta1 from "@akashnetwork/akash-api/v1beta1";
 import * as v1beta2 from "@akashnetwork/akash-api/v1beta2";
@@ -11,7 +10,7 @@ import * as v1beta4 from "@akashnetwork/akash-api/v1beta4";
 const akashTypes: ReadonlyArray<[string, GeneratedType]> = [
   ...Object.values(v1beta1),
   ...Object.values(v1beta2),
-  ...Object.values(omit(v1beta3, 'DepositDeploymentAuthorization')),
+  ...Object.values(v1beta3),
   ...Object.values(v1beta4)
 ].map((x) => ["/" + x.$type, x]);
 const missingTypes: ReadonlyArray<[string, GeneratedType]> = [["/cosmos.slashing.v1beta1.MsgUnjail", MsgUnjail]];
