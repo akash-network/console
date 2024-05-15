14e3b5b7be1357ef6f6e25382560dad5b36d0f9d
Redm4x <2829180+Redm4x@users.noreply.github.com>
2024-05-15 00:11:09 -0400

    fix(api): fix swagger server list (#192)

--- api/src/routers/internalRouter.ts
+++ api/src/routers/internalRouter.ts
@@ -1,18 +1,13 @@
-import { isProd } from "@src/utils/constants";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 import routes from "../routes/internal";
+import { env } from "@src/utils/env";
 
 export const internalRouter = new OpenAPIHono();
 
-const servers = [{ url: `https://api.cloudmos.io/internal`, description: "Production" }];
-if (!isProd) {
-  servers.unshift({ url: `http://localhost:3080/internal`, description: "Localhost" });
-}
-
 internalRouter.doc(`/doc`, {
   openapi: "3.0.0",
-  servers: servers,
+  servers: [{ url: `${env.ServerOrigin}/internal` }],
   info: {
     title: "Cloudmos Internal API",
     description: "APIs for internal use that are not part of the public API. There is no garantees of stability or backward compatibility.",
