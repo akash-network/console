63875dac9e04d289ffe346d04a9fd5bbaaf93d0d
Redm4x <2829180+Redm4x@users.noreply.github.com>
2024-05-17 12:23:13 -0400

    fix(deploy-web): change export page to an authorization popup

--- deploy-web/src/pages/standalone/localstorage-export.tsx
+++ deploy-web/src/pages/standalone/localstorage-export.tsx
@@ -1,30 +1,101 @@
 "use client";
 
+import Button from "@mui/material/Button";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { extractLocalStorageData } from "@src/utils/localStorage";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ErrorIcon from "@mui/icons-material/Error";
+import Head from "next/head";
 
 const validExternalDomain = "https://console.akash.network";
 
 export default function Page() {
+  const [state, setState] = useState<"loading" | "authorization" | "done" | "error">("authorization");
   useEffect(() => {
-    exportLocalStorageData();
+    window.addEventListener("message", handleMessage);
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
   }, []);
 
-  function exportLocalStorageData() {
-    if (window.parent === window) {
-      console.log(`${window.location.origin} => No parent window found`);
+  function handleMessage(event: MessageEvent) {
+    if (event.origin !== validExternalDomain) {
       return;
     }
 
-    const data = extractLocalStorageData();
+    if (event.data === "DONE") {
+      setState("done");
 
-    console.log(`${window.location.origin} => Sending localstorage data to ${validExternalDomain}`);
-    window.parent.postMessage(data, { targetOrigin: validExternalDomain });
+      setTimeout(() => {
+        window.close();
+      }, 1_000);
+    }
+  }
+
+  function exportLocalStorageData() {
+    try {
+      setState("loading");
+
+      const data = extractLocalStorageData();
+
+      console.log(`Sending localstorage data to ${validExternalDomain}`);
+      window.opener.postMessage(data, { targetOrigin: validExternalDomain });
+    } catch (e) {
+      setState("error");
+      console.error(e);
+    }
   }
 
   return (
-    <div>
-      <p>Exporting local storage data to {validExternalDomain}</p>
-    </div>
+    <>
+      <Head>
+        <title>Cloudmos Import</title>
+      </Head>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "50px 20px" }}>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%", maxWidth: "300px", marginBottom: "20px" }}>
+          <img src="/android-chrome-192x192.png" width={100} alt="Cloudmos Logo" />
+          <ArrowForwardIcon fontSize="large" />
+          <img src="/images/akash-192x192.png" width={100} alt="Akash Logo" />
+        </div>
+        {state === "loading" && (
+          <div style={{ marginTop: "50px", textAlign: "center" }}>
+            <CircularProgress size="100px" color="secondary" />
+            <p>Importing into Akash Console</p>
+          </div>
+        )}
+        {state === "done" && (
+          <div style={{ marginTop: "50px", textAlign: "center" }}>
+            <CheckCircleOutlineIcon style={{ fontSize: "100px" }} color="success" />
+            <p>Import complete</p>
+          </div>
+        )}
+
+        {state === "error" && (
+          <div style={{ marginTop: "50px", textAlign: "center" }}>
+            <ErrorIcon style={{ fontSize: "100px" }} color="error" />
+            <p>An error occured</p>
+          </div>
+        )}
+        {state === "authorization" && (
+          <>
+            <div style={{ marginTop: "30px" }}>
+              Import data into <strong>Akash Console</strong>
+            </div>
+            <ul>
+              <li>Certificates</li>
+              <li>Deployment Names & SDL</li>
+              <li>Favorite Providers</li>
+            </ul>
+            <div style={{ marginTop: "30px", width: "300px" }}>
+              <Button onClick={exportLocalStorageData} variant="contained" color="secondary" size="large" fullWidth>
+                Import
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
