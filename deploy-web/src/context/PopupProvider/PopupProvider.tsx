661c55595a11db03d19e65be01196ae3d4fee301
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-22 12:53:40 +0200

    feat(setting): request user confirmation on local data import
    As a part of this implement handier popup tooling
    
    refs #56

---
+++ deploy-web/src/context/PopupProvider/PopupProvider.tsx
@@ -0,0 +1,62 @@
+import { Popup, PopupProps, CommonProps, ConfirmProps } from "@src/components/shared/Popup";
+import React, { FC, useCallback, useMemo, useState } from "react";
+import { firstValueFrom, Subject } from "rxjs";
+
+type ConfirmPopupProps = string | (Omit<CommonProps, "onClose" | "open"> & Omit<ConfirmProps, "onValidate" | "onCancel" | "variant">);
+
+type PopupProviderContext = {
+  confirm: (messageOrProps: ConfirmPopupProps) => Promise<boolean>;
+};
+
+const PopupContext = React.createContext<PopupProviderContext | undefined>(undefined);
+
+export const PopupProvider: FC = ({ children }) => {
+  const [popupProps, setPopupProps] = useState<PopupProps | undefined>();
+
+  const confirm = useCallback(
+    (messageOrProps: ConfirmPopupProps) => {
+      let subject: Subject<boolean> | undefined = new Subject<boolean>();
+
+      const closeWithResult = (result: boolean) => () => {
+        if (subject) {
+          subject.next(result);
+          subject.complete();
+          setPopupProps(undefined);
+          subject = undefined;
+        }
+      };
+      const reject = closeWithResult(false);
+      const props = typeof messageOrProps === "string" ? { message: messageOrProps } : messageOrProps;
+
+      setPopupProps({
+        title: "Confirm",
+        ...props,
+        open: true,
+        variant: "confirm",
+        onValidate: closeWithResult(true),
+        onCancel: reject,
+        onClose: reject
+      });
+
+      return firstValueFrom(subject);
+    },
+    [setPopupProps]
+  );
+
+  const context = useMemo(() => ({ confirm }), [confirm]);
+
+  return (
+    <PopupContext.Provider value={context}>
+      {children}
+      {popupProps && <Popup {...popupProps} />}
+    </PopupContext.Provider>
+  );
+};
+
+export const usePopup = () => {
+  const context = React.useContext(PopupContext);
+  if (!context) {
+    throw new Error("usePopup must be used within a PopupProvider");
+  }
+  return context;
+};
