f076d18dda2da46e3b040cf4f657a4e13cc10697
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-05-25 12:06:19 -0400

    fix(indexer): remove js map files

--- apps/indexer/src/monitors/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deploymentBalanceMonitor = exports.addressBalanceMonitor = void 0;
-const addressBalanceMonitor_1 = require("./addressBalanceMonitor");
-const deploymentBalanceMonitor_1 = require("./deploymentBalanceMonitor");
-exports.addressBalanceMonitor = new addressBalanceMonitor_1.AddressBalanceMonitor();
-exports.deploymentBalanceMonitor = new deploymentBalanceMonitor_1.DeploymentBalanceMonitor();
-//# sourceMappingURL=index.js.map
