268947b3e31bfd7c4847d50da77c84f51b110124
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-06-03 17:17:34 -0400

    fix(deploy-web): responsive grid uptime

--- apps/deploy-web/src/components/providers/ProviderDetail.tsx
+++ apps/deploy-web/src/components/providers/ProviderDetail.tsx
@@ -144,7 +144,7 @@ export const ProviderDetail: React.FunctionComponent<Props> = ({ owner, _provide
               />
             </div>
 
-            <div className="grid grid-cols-1 gap-4 space-y-4 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-4 space-y-4 lg:grid-cols-2">
               <div className="basis-1/2">
                 <ActiveLeasesGraph provider={provider} />
               </div>
