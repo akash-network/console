69256dddf0dcd3901c47c3a44e139e22237c453f
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-06-07 17:15:29 +0200

    style(dx): centralize code style tools
    refs #223

---
+++ packages/dev-config/.eslintrc.base.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended"],
+  plugins: ["simple-import-sort"],
+  ignorePatterns: ["node_modules", "dist", "build", "public"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [["^react", "^(?!@src)@?\\w"], ["^@src", "^\\.\\.(?!/?$)", "^\\.\\./?$", "^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"], ["^\\u0000"]]
+      }
+    ],
+    "space-infix-ops": ["error", { int32Hint: false }],
+    "object-curly-spacing": ["error", "always"],
+  },
+  overrides: [
+    {
+      files: ["**/sentry.*.config.js"],
+      parserOptions: {
+        sourceType: "module"
+      }
+    }
+  ]
+};
---
+++ packages/dev-config/.eslintrc.next.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  extends: [path.resolve(__dirname, "./.eslintrc.ts.js")],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["next/core-web-vitals"],
+      rules: {
+        "react/no-unescaped-entities": "warn",
+        "react-hooks/exhaustive-deps": "warn",
+        "import/no-anonymous-default-export": "warn",
+        "react/display-name": "warn"
+      }
+    }
+  ],
+  settings: {
+    next: {
+      rootDir: "."
+    }
+  }
+};
