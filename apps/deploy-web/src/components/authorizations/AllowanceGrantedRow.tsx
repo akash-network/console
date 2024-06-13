d8da6a30887d0168f6fc9a753e7f2ed5cd1f516e
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-06-13 14:48:35 +0200

    feat(wallet): implement fee granter as a global setting
    closes #219

--- apps/deploy-web/src/components/authorizations/AllowanceGrantedRow.tsx
+++ apps/deploy-web/src/components/authorizations/AllowanceGrantedRow.tsx
@@ -4,6 +4,7 @@ import { FormattedTime } from "react-intl";
 
 import { Address } from "@src/components/shared/Address";
 import { AKTAmount } from "@src/components/shared/AKTAmount";
+import { Checkbox } from "@src/components/ui/checkbox";
 import { TableCell, TableRow } from "@src/components/ui/table";
 import { AllowanceType } from "@src/types/grant";
 import { getAllowanceTitleByType } from "@src/utils/grants";
@@ -12,21 +13,24 @@ import { coinToUDenom } from "@src/utils/priceUtils";
 type Props = {
   allowance: AllowanceType;
   children?: ReactNode;
+  onSelect?: () => void;
+  selected?: boolean;
 };
 
-export const AllowanceGrantedRow: React.FunctionComponent<Props> = ({ allowance }) => {
+export const AllowanceGrantedRow: React.FunctionComponent<Props> = ({ allowance, selected, onSelect }) => {
+  const limit = allowance?.allowance.spend_limit[0];
   return (
     <TableRow className="[&>td]:px-2 [&>td]:py-1">
+      <TableCell>
+        <Checkbox className="ml-2" checked={selected} onCheckedChange={typeof onSelect === "function" ? checked => checked && onSelect() : undefined} />
+      </TableCell>
       <TableCell>{getAllowanceTitleByType(allowance)}</TableCell>
+      <TableCell>{allowance.granter && <Address address={allowance.granter} isCopyable />}</TableCell>
       <TableCell>
-        <Address address={allowance.granter} isCopyable />
+        {limit && <AKTAmount uakt={coinToUDenom(limit)} />}
+        {limit && "AKT"}
       </TableCell>
-      <TableCell>
-        <AKTAmount uakt={coinToUDenom(allowance.allowance.spend_limit[0])} /> AKT
-      </TableCell>
-      <TableCell align="right">
-        <FormattedTime year="numeric" month={"numeric"} day={"numeric"} value={allowance.allowance.expiration} />
-      </TableCell>
+      <TableCell align="right">{<FormattedTime year="numeric" month={"numeric"} day={"numeric"} value={allowance.allowance.expiration} />}</TableCell>
     </TableRow>
   );
 };
--- apps/deploy-web/src/pages/_app.tsx
+++ apps/deploy-web/src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Router from "next/router";
 import { ThemeProvider } from "next-themes";
 import NProgress from "nprogress"; //nprogress module
 
+import { AllowanceWatcher } from "@src/components/authorizations/AllowanceWatcher";
 import GoogleAnalytics from "@src/components/layout/CustomGoogleAnalytics";
 import { CustomIntlProvider } from "@src/components/layout/CustomIntlProvider";
 import { PageHead } from "@src/components/layout/PageHead";
@@ -71,6 +72,7 @@ const App: React.FunctionComponent<Props> = props => {
                                         <BackgroundTaskProvider>
                                           <TemplatesProvider>
                                             <LocalNoteProvider>
+                                              <AllowanceWatcher />
                                               <GoogleAnalytics />
                                               <Component {...pageProps} />
                                             </LocalNoteProvider>
