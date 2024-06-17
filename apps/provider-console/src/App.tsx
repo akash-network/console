a0ce2955a987f43c31cd500bacd54d5b315d4e1c
Jigar Patel <jigar@arc10.io>
2024-06-17 12:36:25 -0400

    Initialize provider console workspace

---
+++ apps/provider-console/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Provider Console</header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ apps/provider-console/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return <h1>Home Page</h1>;
+};
+
+export default Home;
+
