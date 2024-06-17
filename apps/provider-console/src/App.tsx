1dbd150e2ed69339310a0f15712c4472a762aa19
Jigar Patel <jigar@arc10.io>
2024-06-17 15:01:02 -0400

    added tailwindcss

--- apps/provider-console/src/App.tsx
+++ apps/provider-console/src/App.tsx
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Provider Console</header>
+      <header className="App-header">
+        <h1 className="text-3xl font-bold">Provider Console</h1>
+      </header>
     </div>
   );
 }
