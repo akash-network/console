06379186e6be593e7fc50fdb5f5fcdc66463ea55
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-07-03 12:46:58 +0530

    fix types

--- apps/deploy-web/src/types/global.ts
+++ apps/deploy-web/src/types/global.ts
@@ -1,7 +1,7 @@
 import { Keplr, Window as KeplrWindow } from "@keplr-wallet/types";
+
 declare global {
   interface Window extends KeplrWindow {
-    wallet: Keplr;
-    leap: Keplr;
+    wallet: Keplr | undefined;
   }
 }
