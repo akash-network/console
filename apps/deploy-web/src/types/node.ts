07fd696989d9d16c6e5b07071e2260f749fab4d0
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-07-09 16:33:05 -0400

    fix: improve the settings node fetching to avoid having a failing node (#262)

--- apps/deploy-web/src/types/node.ts
+++ apps/deploy-web/src/types/node.ts
@@ -1,33 +1,38 @@
 export interface NodeStatus {
-  node_info: NodeInfo;
-  application_version: ApplicationVersion;
-}
-
-export interface NodeInfo {
-  protocol_version: {
-    p2p: string;
-    block: string;
-    app: string;
+  node_info: {
+    protocol_version: {
+      p2p: string;
+      block: string;
+      app: string;
+    };
+    id: string;
+    listen_addr: string;
+    network: string;
+    version: string;
+    channels: string;
+    moniker: string;
+    other: {
+      tx_index: string;
+      rpc_address: string;
+    };
   };
-  id: string;
-  listen_addr: string;
-  network: string;
-  version: string;
-  channels: string;
-  moniker: string;
-  other: {
-    tx_index: string;
-    rpc_address: string;
+  sync_info: {
+    latest_block_hash: string;
+    latest_app_hash: string;
+    latest_block_height: string;
+    latest_block_time: string;
+    earliest_block_hash: string;
+    earliest_app_hash: string;
+    earliest_block_height: string;
+    earliest_block_time: string;
+    catching_up: boolean;
   };
-}
-
-export interface ApplicationVersion {
-  name: string;
-  server_name: string;
-  version: string;
-  commit: string;
-  build_tags: string;
-  go: string;
-  build_deps: string[];
-  cosmos_sdk_version: string;
+  validator_info: {
+    address: string;
+    pub_key: {
+      type: string;
+      value: string;
+    };
+    voting_power: string;
+  };
 }
