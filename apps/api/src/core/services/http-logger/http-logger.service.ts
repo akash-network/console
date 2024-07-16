6e59b6b9b2bb793014069d82fac489c7ff8408c2
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-07-16 18:20:34 +0200

    feat(pg): disable drizzle to avoid timestamps stringification

--- apps/api/src/core/services/http-logger/http-logger.service.ts
+++ apps/api/src/core/services/http-logger/http-logger.service.ts
@@ -1,7 +1,7 @@
 import { Context, Next } from "hono";
 import { singleton } from "tsyringe";
 
-import { LoggerService } from "@src/core";
+import { LoggerService } from "@src/core/services/logger/logger.service";
 import type { HonoInterceptor } from "@src/core/types/hono-interceptor.type";
 
 @singleton()
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,9 +4,12 @@ import "./open-telemetry";
 import { container } from "tsyringe";
 
 import { initApp } from "@src/app";
-import { PostgresMigratorService } from "@src/core";
+import type { PostgresMigratorService } from "@src/core";
 
-container
-  .resolve(PostgresMigratorService)
-  .migrate()
-  .then(() => initApp());
+const { BILLING_ENABLED } = process.env;
+
+const migrate =
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  BILLING_ENABLED === "true" ? container.resolve<PostgresMigratorService>(require("@src/core").PostgresMigratorService).migrate() : Promise.resolve();
+
+migrate.then(() => initApp());
