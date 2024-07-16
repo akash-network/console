d4be891aeacf76f7286e24ed5f4bce49a71bdc1a
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-07-16 18:57:49 -0400

    refactor: deployment name modal

--- apps/deploy-web/src/context/LocalNoteProvider/DeploymentNameModal.tsx
+++ apps/deploy-web/src/context/LocalNoteProvider/DeploymentNameModal.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { useEffect, useRef } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { InputWithIcon, Popup, Snackbar } from "@akashnetwork/ui/components";
+import { useForm } from "react-hook-form";
+import { FormField, FormInput, Popup, Snackbar } from "@akashnetwork/ui/components";
 import { useSnackbar } from "notistack";
 
 import { updateDeploymentLocalData } from "@src/utils/deploymentLocalDataUtils";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const formSchema = z.object({
+  name: z.string()
+});
 
 export const DeploymentNameModal = ({ dseq, onClose, onSaved, getDeploymentName }) => {
   const formRef = useRef<HTMLFormElement | null>(null);
   const { enqueueSnackbar } = useSnackbar();
-  const { handleSubmit, control, setValue } = useForm({
+  const { handleSubmit, control, setValue } = useForm<z.infer<typeof formSchema>>({
     defaultValues: {
       name: ""
-    }
+    },
+    resolver: zodResolver(formSchema)
   });
 
   useEffect(() => {
@@ -62,11 +69,11 @@ export const DeploymentNameModal = ({ dseq, onClose, onSaved, getDeploymentName 
       maxWidth="xs"
     >
       <form onSubmit={handleSubmit(onSubmit)} ref={formRef}>
-        <Controller
+        <FormField
           control={control}
           name="name"
           render={({ field }) => {
-            return <InputWithIcon {...field} label="Name" autoFocus type="text" />;
+            return <FormInput {...field} label="Name" autoFocus type="text" />;
           }}
         />
       </form>
