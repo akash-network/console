8fc8c9468bd44cbd2bee477be7e7b41c5575c556
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-07-24 12:25:36 -0400

    refactor: settings form

--- apps/deploy-web/src/components/settings/SettingsForm.tsx
+++ apps/deploy-web/src/components/settings/SettingsForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRef, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { Button, FormField, FormInput, Label, Spinner, SwitchWithLabel } from "@akashnetwork/ui/components";
+import { useForm } from "react-hook-form";
+import { Button, Form, FormField, FormInput, Label, Spinner, SwitchWithLabel } from "@akashnetwork/ui/components";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Autocomplete from "@mui/material/Autocomplete";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
@@ -33,14 +33,10 @@ export const SettingsForm: React.FunctionComponent = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [isNodesOpen, setIsNodesOpen] = useState(false);
   const { settings, setSettings, refreshNodeStatuses, isRefreshingNodeStatus } = useSettings();
-  const {
-    handleSubmit,
-    control,
-    reset,
-    formState: { errors }
-  } = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema)
   });
+  const { control, handleSubmit, reset } = form;
   const formRef = useRef<HTMLFormElement>(null);
   const { selectedNode, nodes } = settings;
 
@@ -71,11 +67,11 @@ export const SettingsForm: React.FunctionComponent = () => {
    *  Update the custom settings
    * @param {Object} data {apiEndpoint: string, rpcEndpoint: string}
    */
-  const onSubmit = data => {
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
     const customNodeUrl = new URL(data.apiEndpoint);
     setIsEditing(false);
 
-    const newSettings = { ...settings, ...data, customNode: { ...settings.customNode, id: customNodeUrl.hostname } };
+    const newSettings = { ...settings, ...data, customNode: { ...(settings.customNode as BlockchainNode), id: customNodeUrl.hostname } };
     setSettings(newSettings);
     refreshNodeStatuses(newSettings);
   };
@@ -87,67 +83,69 @@ export const SettingsForm: React.FunctionComponent = () => {
       </div>
 
       {settings.isCustomNode && (
-        <form className="pt-4" onSubmit={handleSubmit(onSubmit)} ref={formRef}>
-          <div className="mb-2 flex items-center">
-            <Label className="min-w-[150px] basis-[20%] pr-4">Api Endpoint:</Label>
+        <Form {...form}>
+          <form className="pt-4" onSubmit={handleSubmit(onSubmit)} ref={formRef}>
+            <div className="mb-2 flex items-center">
+              <Label className="min-w-[150px] basis-[20%] pr-4">Api Endpoint:</Label>
 
-            {isEditing ? (
-              <FormField
-                control={control}
-                name="apiEndpoint"
-                defaultValue={settings.apiEndpoint}
-                render={({ field }) => {
-                  return <FormInput {...field} type="text" className="flex-1" />;
-                }}
-              />
-            ) : (
-              <p className="flex-grow">{settings.apiEndpoint}</p>
-            )}
-          </div>
-
-          <div className="mb-2 flex items-center">
-            <Label className="min-w-[150px] basis-[20%] pr-4">Rpc Endpoint:</Label>
-
-            {isEditing ? (
-              <FormField
-                control={control}
-                name="rpcEndpoint"
-                defaultValue={settings.apiEndpoint}
-                render={({ field }) => {
-                  return <FormInput {...field} type="text" className="flex-1" />;
-                }}
-              />
-            ) : (
-              <p className="flex-grow">{settings.rpcEndpoint}</p>
-            )}
-          </div>
-
-          <div className="pt-4">
-            {!isEditing && (
-              <Button variant="default" onClick={() => setIsEditing(!isEditing)} size="sm">
-                Edit
-              </Button>
-            )}
-
-            {isEditing && (
-              <>
-                <Button
-                  variant="text"
-                  onClick={() => {
-                    reset({}, { keepDefaultValues: true });
-                    setIsEditing(false);
+              {isEditing ? (
+                <FormField
+                  control={control}
+                  name="apiEndpoint"
+                  defaultValue={settings.apiEndpoint}
+                  render={({ field }) => {
+                    return <FormInput {...field} type="text" className="flex-1" />;
                   }}
-                  size="sm"
-                >
-                  Cancel
+                />
+              ) : (
+                <p className="flex-grow">{settings.apiEndpoint}</p>
+              )}
+            </div>
+
+            <div className="mb-2 flex items-center">
+              <Label className="min-w-[150px] basis-[20%] pr-4">Rpc Endpoint:</Label>
+
+              {isEditing ? (
+                <FormField
+                  control={control}
+                  name="rpcEndpoint"
+                  defaultValue={settings.rpcEndpoint}
+                  render={({ field }) => {
+                    return <FormInput {...field} type="text" className="flex-1" />;
+                  }}
+                />
+              ) : (
+                <p className="flex-grow">{settings.rpcEndpoint}</p>
+              )}
+            </div>
+
+            <div className="pt-4">
+              {!isEditing && (
+                <Button variant="default" onClick={() => setIsEditing(!isEditing)} size="sm">
+                  Edit
                 </Button>
-                <Button variant="default" type="submit" className="ml-4" onClick={() => formRef.current?.dispatchEvent(new Event("submit"))} size="sm">
-                  Submit
-                </Button>
-              </>
-            )}
-          </div>
-        </form>
+              )}
+
+              {isEditing && (
+                <>
+                  <Button
+                    variant="text"
+                    onClick={() => {
+                      reset({}, { keepDefaultValues: true });
+                      setIsEditing(false);
+                    }}
+                    size="sm"
+                  >
+                    Cancel
+                  </Button>
+                  <Button variant="default" type="submit" className="ml-4" onClick={() => formRef.current?.dispatchEvent(new Event("submit"))} size="sm">
+                    Submit
+                  </Button>
+                </>
+              )}
+            </div>
+          </form>
+        </Form>
       )}
 
       {!settings.isCustomNode && (
