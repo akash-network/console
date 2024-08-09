ef2ba8f5db782b41c1b9a918cbb849d4b990f252
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-08-09 17:24:29 -0400

    fix(console): remove ssh toggle

--- apps/deploy-web/src/pages/new-deployment/index.tsx
+++ apps/deploy-web/src/pages/new-deployment/index.tsx
@@ -1,4 +1,4 @@
 import { NewDeploymentContainer } from "@src/components/new-deployment/NewDeploymentContainer";
 import { withSdlBuilder } from "@src/context/SdlBuilderProvider/SdlBuilderProvider";
 
-export default withSdlBuilder({ componentsSet: "ssh-toggled" })(NewDeploymentContainer);
+export default withSdlBuilder()(NewDeploymentContainer);
