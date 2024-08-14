10d9a305a2c802081f665441c8b8f196dfcb8e20
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-08-14 15:10:26 -0400

    feat(console): add pagination deployment deposit

--- apps/deploy-web/src/components/authorizations/DeploymentGrantTable.tsx
+++ apps/deploy-web/src/components/authorizations/DeploymentGrantTable.tsx
@@ -1,6 +1,6 @@
-import { Button, Table, TableBody, TableHead, TableHeader, TableRow } from "@akashnetwork/ui/components";
+import { Button, CustomPagination, Table, TableBody, TableHead, TableHeader, TableRow } from "@akashnetwork/ui/components";
 import { GrantType } from "@src/types/grant";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { GranterRow } from "./GranterRow";
 import { LinkTo } from "../shared/LinkTo";
 
@@ -13,6 +13,13 @@ interface Props {
 }
 
 const DeploymentGrantTable: React.FC<Props> = ({ grants, onEditGrant, setDeletingGrants, setSelectedGrants, selectedGrants }) => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const start = pageIndex * pageSize;
+  const end = start + pageSize;
+  const currentPageGrants = grants.slice(start, end);
+  const pageCount = Math.ceil(grants.length / pageSize);
+
   const onSelectGrant = (checked: boolean, grant: GrantType) => {
     setSelectedGrants(prev => {
       return checked ? prev.concat([grant]) : prev.filter(x => x.grantee !== grant.grantee);
@@ -35,48 +42,71 @@ const DeploymentGrantTable: React.FC<Props> = ({ grants, onEditGrant, setDeletin
     setSelectedGrants([]);
   };
 
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-1/4">Grantee</TableHead>
-          <TableHead className="w-1/4 text-center">Spending Limit</TableHead>
-          <TableHead className="w-1/4 text-center">Expiration</TableHead>
-          <TableHead className="w-1/4 text-center">
-            {selectedGrants.length > 0 && (
-              <div className="flex items-center justify-end space-x-4">
-                <LinkTo onClick={onClearSelection} className="text-xs">
-                  Clear
-                </LinkTo>
-                <Button onClick={onDeleteGrants} variant="destructive" size="sm" className="h-6 p-2 text-xs">
-                  Revoke selected ({selectedGrants.length})
-                </Button>
-              </div>
-            )}
-            {grants.length > 0 && selectedGrants.length === 0 && (
-              <div className="flex items-center justify-end">
-                <Button onClick={onDeleteAll} variant="destructive" size="sm" className="h-6 p-2 text-xs">
-                  Revoke all
-                </Button>
-              </div>
-            )}
-          </TableHead>
-        </TableRow>
-      </TableHeader>
+  const handleChangePage = (newPage: number) => {
+    setPageIndex(newPage);
+  };
 
-      <TableBody>
-        {grants.map(grant => (
-          <GranterRow
-            key={grant.grantee}
-            grant={grant}
-            onEditGrant={onEditGrant}
-            setDeletingGrant={onGrantDelete}
-            onSelectGrant={onSelectGrant}
-            checked={selectedGrants.some(x => x.grantee === grant.grantee && x.granter === grant.granter)}
+  const onPageSizeChange = (value: number) => {
+    setPageSize(value);
+    setPageIndex(0);
+  };
+
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/4">Grantee</TableHead>
+            <TableHead className="w-1/4 text-center">Spending Limit</TableHead>
+            <TableHead className="w-1/4 text-center">Expiration</TableHead>
+            <TableHead className="w-1/4 text-center">
+              {selectedGrants.length > 0 && (
+                <div className="flex items-center justify-end space-x-4">
+                  <LinkTo onClick={onClearSelection} className="text-xs">
+                    Clear
+                  </LinkTo>
+                  <Button onClick={onDeleteGrants} variant="destructive" size="sm" className="h-6 p-2 text-xs">
+                    Revoke selected ({selectedGrants.length})
+                  </Button>
+                </div>
+              )}
+              {grants.length > 0 && selectedGrants.length === 0 && (
+                <div className="flex items-center justify-end">
+                  <Button onClick={onDeleteAll} variant="destructive" size="sm" className="h-6 p-2 text-xs">
+                    Revoke all
+                  </Button>
+                </div>
+              )}
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {currentPageGrants.map(grant => (
+            <GranterRow
+              key={grant.grantee}
+              grant={grant}
+              onEditGrant={onEditGrant}
+              setDeletingGrant={onGrantDelete}
+              onSelectGrant={onSelectGrant}
+              checked={selectedGrants.some(x => x.grantee === grant.grantee && x.granter === grant.granter)}
+            />
+          ))}
+        </TableBody>
+      </Table>
+
+      {(grants?.length || 0) > 0 && (
+        <div className="flex items-center justify-center pt-6">
+          <CustomPagination
+            totalPageCount={pageCount}
+            setPageIndex={handleChangePage}
+            pageIndex={pageIndex}
+            pageSize={pageSize}
+            setPageSize={onPageSizeChange}
           />
-        ))}
-      </TableBody>
-    </Table>
+        </div>
+      )}
+    </div>
   );
 };
 
