f3f2c5f6db7d0b2ebd8f74a02d5a5ab7d40ecb81
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-08-15 14:50:51 +0200

    feat(env): improve env loading logic for the api

--- apps/api/src/dotenv.ts
+++ apps/api/src/dotenv.ts
@@ -1,4 +1,24 @@
 import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+import fs from "fs";
+import pino from "pino";
 
-dotenv.config({ path: ".env.local" });
-dotenv.config();
+const logger = pino().child({ context: "ENV" });
+
+const config = (path: string) => {
+  if (fs.existsSync(path)) {
+    dotenvExpand.expand(dotenv.config({ path }));
+    logger.info(`Loaded ${path}`);
+  }
+};
+config("env/.env.local");
+config("env/.env");
+
+const deploymentEnv = process.env.DEPLOYMENT_ENV;
+
+if (deploymentEnv && deploymentEnv !== "local") {
+  config(`env/.env.${deploymentEnv}`);
+}
+
+const network = process.env.NETWORK || "mainnet";
+config(`env/.env.${network}`);
