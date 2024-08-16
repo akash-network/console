0436a5ef8b1c4b6295ae2b217a7017797174e102
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-08-16 11:21:34 +0200

    fix(auth): avoid fetching profile via proxy

--- apps/deploy-web/src/services/user/user-http.service.ts
+++ apps/deploy-web/src/services/user/user-http.service.ts
@@ -1,13 +1,22 @@
 import { HttpService } from "@akashnetwork/http-sdk";
 import type { UserProfile } from "@auth0/nextjs-auth0/client";
+import { InternalAxiosRequestConfig } from "axios";
 
-import { authService } from "@src/services/auth/auth.service";
+import { ANONYMOUS_USER_TOKEN_KEY } from "@src/utils/constants";
 
 export class UserHttpService extends HttpService {
   constructor() {
     super();
     this.getProfile = this.getProfile.bind(this);
-    this.interceptors.request.use(authService.withAnonymousUserHeader);
+    this.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+      const token = localStorage.getItem(ANONYMOUS_USER_TOKEN_KEY);
+
+      if (token) {
+        config.headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return config;
+    });
   }
 
   async getProfile(url: string) {
