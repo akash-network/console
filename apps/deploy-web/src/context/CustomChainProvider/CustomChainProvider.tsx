7f564031eb21814107a884a2aed172b6580b0f05
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-08-30 12:17:40 -0400

    feat(console): add all leap wallets (#346)

--- apps/deploy-web/src/context/CustomChainProvider/CustomChainProvider.tsx
+++ apps/deploy-web/src/context/CustomChainProvider/CustomChainProvider.tsx
@@ -5,7 +5,7 @@ import "@interchain-ui/react/globalStyles";
 import { GasPrice } from "@cosmjs/stargate";
 import { wallets as cosmostation } from "@cosmos-kit/cosmostation-extension";
 import { wallets as keplr } from "@cosmos-kit/keplr";
-import { wallets as leap } from "@cosmos-kit/leap-extension";
+import { wallets as leap } from "@cosmos-kit/leap";
 import { ChainProvider } from "@cosmos-kit/react";
 import { useChain } from "@cosmos-kit/react";
 
