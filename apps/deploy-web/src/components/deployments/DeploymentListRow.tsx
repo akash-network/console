c677a0968c57d049bcc419556bfae532cbe2d706
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-08-30 11:16:46 -0400

    feat(console): managed wallets popup confirmation (#342)

--- apps/deploy-web/src/components/deployments/DeploymentListRow.tsx
+++ apps/deploy-web/src/components/deployments/DeploymentListRow.tsx
@@ -20,6 +20,7 @@ import { useRouter } from "next/navigation";
 import { event } from "nextjs-google-analytics";
 
 import { useWallet } from "@src/context/WalletProvider";
+import { useManagedDeploymentConfirm } from "@src/hooks/useManagedDeploymentConfirm";
 import { getShortText } from "@src/hooks/useShortText";
 import { useDenomData } from "@src/hooks/useWalletBalance";
 import { useAllLeases } from "@src/queries/useLeaseQuery";
@@ -110,6 +111,7 @@ export const DeploymentListRow: React.FunctionComponent<Props> = ({ deployment, 
   const avgCost = udenomToDenom(getAvgCostPerMonth(deploymentCost || 0));
   const storageDeploymentData = getDeploymentData(deployment?.dseq);
   const denomData = useDenomData(deployment.escrowAccount.balance.denom);
+  const { closeDeploymentConfirm } = useManagedDeploymentConfirm();
 
   function viewDeployment() {
     router.push(UrlService.deploymentDetails(deployment.dseq));
@@ -142,6 +144,12 @@ export const DeploymentListRow: React.FunctionComponent<Props> = ({ deployment, 
 
   const onCloseDeployment = async () => {
     handleMenuClose();
+
+    const isConfirmed = await closeDeploymentConfirm([deployment.dseq]);
+
+    if (!isConfirmed) {
+      return;
+    }
 
     const message = TransactionMessageData.getCloseDeploymentMsg(address, deployment.dseq);
     const response = await signAndBroadcastTx([message]);
