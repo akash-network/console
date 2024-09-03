f50db175fccefb9abc52ac979ac541e7fc5a3955
Redm4x <2829180+Redm4x@users.noreply.github.com>
2024-09-03 18:26:05 -0400

    chore(api): no longer report an error when ibc denom is not found

--- apps/api/src/utils/coin.ts
+++ apps/api/src/utils/coin.ts
@@ -1,48 +1,55 @@
 import { asset_lists } from "@chain-registry/assets";
-import * as Sentry from "@sentry/node";
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
 
+import { LoggerService } from "@src/core";
+
+const logger = new LoggerService({ context: "CoinUtil" });
+
 export function coinToAsset(coin: Coin) {
-  try {
-    if (coin.denom === "uakt") {
+  if (coin.denom === "uakt") {
+    return {
+      symbol: "AKT",
+      logoUrl: "https://console.akash.network/images/akash-logo.svg",
+      amount: parseInt(coin.amount) / 1_000_000
+    };
+  } else if (coin.denom === "akt") {
+    return {
+      symbol: "AKT",
+      logoUrl: "https://console.akash.network/images/akash-logo.svg",
+      amount: parseFloat(coin.amount)
+    };
+  } else {
+    const akashChain = asset_lists.find(c => c.chain_name === "akash");
+    const ibcAsset = akashChain.assets.find(a => a.base === coin.denom);
+
+    if (!ibcAsset) {
+      logger.info(`Unknown asset ${coin.denom}`);
+
       return {
-        symbol: "AKT",
-        logoUrl: "https://console.akash.network/images/akash-logo.svg",
-        amount: parseInt(coin.amount) / 1_000_000
-      };
-    } else if (coin.denom === "akt") {
-      return {
-        symbol: "AKT",
-        logoUrl: "https://console.akash.network/images/akash-logo.svg",
+        ibcToken: coin.denom,
         amount: parseFloat(coin.amount)
       };
-    } else {
-      const akashChain = asset_lists.find(c => c.chain_name === "akash");
-      const ibcAsset = akashChain.assets.find(a => a.base === coin.denom);
+    }
 
-      if (!ibcAsset) throw new Error(`Unknown asset ${coin.denom}`);
+    const displayAsset = ibcAsset.denom_units.find(d => d.denom === ibcAsset.display);
 
-      const displayAsset = ibcAsset.denom_units.find(d => d.denom === ibcAsset.display);
-
-      if (!displayAsset) throw new Error(`Unable to find display asset for ${coin.denom}`);
-
-      const displayAmount = parseInt(coin.amount) / Math.pow(10, displayAsset.exponent);
+    if (!displayAsset) {
+      logger.info(`Unable to find display asset for ${coin.denom}`);
 
       return {
-        symbol: ibcAsset.symbol,
         ibcToken: coin.denom,
-        logoUrl: ibcAsset.logo_URIs?.svg || ibcAsset?.logo_URIs?.png,
-        description: ibcAsset?.description,
-        amount: displayAmount
+        amount: parseFloat(coin.amount)
       };
     }
-  } catch (err) {
-    console.error(err);
-    Sentry.captureException(err);
+
+    const displayAmount = parseInt(coin.amount) / Math.pow(10, displayAsset.exponent);
 
     return {
+      symbol: ibcAsset.symbol,
       ibcToken: coin.denom,
-      amount: parseFloat(coin.amount)
+      logoUrl: ibcAsset.logo_URIs?.svg || ibcAsset?.logo_URIs?.png,
+      description: ibcAsset?.description,
+      amount: displayAmount
     };
   }
 }
