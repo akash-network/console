6aee8cccff4d30ad84a58da98e9cae4f3cbd4e7d
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-09-09 17:18:39 -0400

    fix(stats): fix resizing + added legend label

--- apps/stats-web/src/components/graph/Graph.tsx
+++ apps/stats-web/src/components/graph/Graph.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useRef, useEffect, useMemo } from 'react';
+import React, { useRef, useEffect, useMemo } from "react";
 
-
-import { createChart } from 'lightweight-charts';
+import { createChart } from "lightweight-charts";
 import { format } from "date-fns";
 import { useIntl } from "react-intl";
 import { useTheme } from "next-themes";
@@ -27,28 +26,39 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
   const intl = useIntl();
   const graphTheme = getTheme(resolvedTheme);
 
-  const initialData = useMemo(() => snapshotData
-    ? rangedData.map(_snapshot => (
-      {
-        time: format(_snapshot.date, 'yyyy-MM-dd'),
-        value: roundDecimal(snapshotMetadata.unitFn(_snapshot.value).value),
-      })).sort(function (a, b) {
-        return Number(new Date(a.time)) - Number(new Date(b.time));
-      }) : [], [rangedData]);
-  
-  const totalGraphData = useMemo(() => snapshotData
-  ? snapshotData.snapshots.map(_snapshot => (
-    {
-      time: format(_snapshot.date, 'yyyy-MM-dd'),
-      value: roundDecimal(snapshotMetadata.unitFn(_snapshot.value).value),
-    })).sort(function (a, b) {
-      return Number(new Date(a.time)) - Number(new Date(b.time));
-    }) : [], [rangedData]);
+  const initialData = useMemo(
+    () =>
+      snapshotData
+        ? rangedData
+            .map(_snapshot => ({
+              time: format(_snapshot.date, "yyyy-MM-dd"),
+              value: roundDecimal(snapshotMetadata.unitFn(_snapshot.value).value)
+            }))
+            .sort(function (a, b) {
+              return Number(new Date(a.time)) - Number(new Date(b.time));
+            })
+        : [],
+    [rangedData]
+  );
+
+  const totalGraphData = useMemo(
+    () =>
+      snapshotData
+        ? snapshotData.snapshots
+            .map(_snapshot => ({
+              time: format(_snapshot.date, "yyyy-MM-dd"),
+              value: roundDecimal(snapshotMetadata.unitFn(_snapshot.value).value)
+            }))
+            .sort(function (a, b) {
+              return Number(new Date(a.time)) - Number(new Date(b.time));
+            })
+        : [],
+    [rangedData]
+  );
 
   const chartContainerRef = useRef(null);
   const tooltipRef = useRef(null);
   const timer = useRef<number | null>(null);
-
 
   useEffect(() => {
     let graphData = [...initialData];
@@ -59,14 +69,14 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
       layout: {
         textColor: graphTheme.textColor,
         background: { color: "transparent" },
-        attributionLogo: false,
+        attributionLogo: false
       },
-      height: 400,
+      height: 400
     });
 
     chart.timeScale().fitContent();
 
-    const lineSeries = chart.addLineSeries({ color: customColors.akashRed, lineWidth: 2, });
+    const lineSeries = chart.addLineSeries({ color: customColors.akashRed, lineWidth: 2 });
     lineSeries.setData(graphData);
 
     const axisRightFormatter = val => nFormatter(val, 2);
@@ -75,34 +85,34 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
     chart.applyOptions({
       localization: {
         priceFormatter: axisRightFormatter,
-        timeFormatter: axisBottomFormatter,
+        timeFormatter: axisBottomFormatter
       },
       rightPriceScale: {
         scaleMargins: {
           top: 0.05,
-          bottom: 0.05,
+          bottom: 0.05
         }
       },
       crosshair: {
         // hide the horizontal crosshair line
         horzLine: {
           visible: false,
-          labelVisible: false,
+          labelVisible: false
         },
         // hide the vertical crosshair label
         vertLine: {
-          labelVisible: false,
-        },
+          labelVisible: false
+        }
       },
       // hide the grid lines
       grid: {
         vertLines: {
-          visible: false,
+          visible: false
         },
         horzLines: {
-          visible: false,
-        },
-      },
+          visible: false
+        }
+      }
     });
 
     const toolTipWidth = 80;
@@ -110,9 +120,9 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
     const toolTipMargin = 15;
 
     const toolTip = tooltipRef.current;
-    toolTip.style.display = 'none';
+    toolTip.style.display = "none";
 
-    chart.subscribeCrosshairMove((param) => {
+    chart.subscribeCrosshairMove(param => {
       if (
         param.point === undefined ||
         !param.time ||
@@ -121,14 +131,14 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
         param.point.y < 0 ||
         param.point.y > chartContainerRef.current.clientHeight
       ) {
-        toolTip.style.display = 'none';
+        toolTip.style.display = "none";
       } else {
         const data: any = param.seriesData.get(lineSeries);
         toolTip.innerHTML = `<div style='margin-bottom: 0.25rem; font-size: 0.75rem; line-height: 1rem'>
             ${format(new Date(param.time.toString()), "MMMM d, yy")}
             </div>
             <div style="font-weight: 700">${nFormatter(data?.value, 2)}</div>
-        `
+        `;
 
         const y = param.point.y;
         let left = param.point.x + toolTipMargin;
@@ -143,12 +153,12 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
 
         toolTip.style.left = `${left}px`;
         toolTip.style.top = `${top}px`;
-        toolTip.style.display = 'block';
+        toolTip.style.display = "block";
       }
     });
 
     chart.timeScale().subscribeVisibleLogicalRangeChange(logicalRange => {
-      if(logicalRange === null) {
+      if (logicalRange === null) {
         return;
       }
 
@@ -157,30 +167,30 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
       }
 
       timer.current = window.setTimeout(() => {
-        const rangeFrom = Math.round(logicalRange.from)
-        const range = Math.max(graphData.length - rangeFrom, 0)
-        graphData = [...totalGraphData.slice(range, -graphData.length), ...graphData]
+        const rangeFrom = Math.round(logicalRange.from);
+        const range = Math.max(graphData.length - rangeFrom, 0);
+        graphData = [...totalGraphData.slice(range, -graphData.length), ...graphData];
         if (!isDisposed && lineSeries) {
           lineSeries.setData(graphData);
         }
         timer.current = null;
-      }, 500)
+      }, 500);
     });
 
     // Handle resize
     const handleResize = () => {
       chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+      chart.resize(chartContainerRef.current.clientWidth, 400);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
       isDisposed = true;
       chart.remove();
     };
   }, [initialData]);
-
 
   return (
     <div className="relative h-[400px]">
@@ -188,7 +198,8 @@ const Graph: React.FunctionComponent<IGraphProps> = ({ rangedData, snapshotMetad
         <span className="text-md font-bold tracking-wide text-muted-foreground opacity-40">stats.akash.network</span>
       </div>
       <div ref={chartContainerRef} className="relative">
-        <div ref={tooltipRef} className="absolute hidden rounded-sm bg-primary px-3 py-2 leading-4 text-primary-foreground z-50"></div>
+        <div ref={tooltipRef} className="absolute z-50 hidden rounded-sm bg-primary px-3 py-2 leading-4 text-primary-foreground"></div>
+        {snapshotMetadata.legend && <div className="absolute right-0 top-1/2 z-50 translate-y-[-50%] text-xs">{snapshotMetadata.legend}</div>}
       </div>
     </div>
   );
