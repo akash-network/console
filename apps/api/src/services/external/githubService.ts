akash-network/cloudmos

chore: rename akashlytics to console

apps/api/src/services/external/githubService.ts
@@ -6,10 +6,10 @@ import { Auditor, ProviderAttributesSchema } from "@src/types/provider";
 import { env } from "@src/utils/env";
 
 export function getOctokit() {
-  const githubPAT = env.AkashlyticsGithubPAT;
+  const githubPAT = env.ConsoleGithubPAT;
 
   if (!githubPAT) {
-    throw new Error("AkashlyticsGithubPAT is missing");
+    throw new Error("ConsoleGithubPAT is missing");
   }
 
   return new Octokit({

akash-network/console

apps/api/src/services/external/githubService.ts
@@ -6,10 +6,10 @@ import { Auditor, ProviderAttributesSchema } from "@src/types/provider";
 import { env } from "@src/utils/env";
 
 export function getOctokit() {
-  const githubPAT = env.AKASHLYTICS_GITHUB_PAT;
+  const githubPAT = env.CONSOLE_GITHUB_PAT;
 
   if (!githubPAT) {
-    throw new Error("AKASHLYTICS_GITHUB_PAT is missing");
+    throw new Error("CONSOLE_GITHUB_PAT is missing");
   }
 
   return new Octokit({