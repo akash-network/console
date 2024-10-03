0293d28249625e1bd0f9e5098fa563f85b97c4bd
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-10-03 11:16:52 +0200

    feat(config): replace network ids constants with imports from network store
    refs #163, #313

--- apps/stats-web/src/hooks/useSelectedNetwork.ts
+++ apps/stats-web/src/hooks/useSelectedNetwork.ts
@@ -1,11 +1,11 @@
+import { MAINNET_ID } from "@akashnetwork/network-store";
 import { useAtom } from "jotai";
 import { useEffectOnce } from "usehooks-ts";
 
-import { mainnetId } from "@/lib/constants";
 import networkStore, { networks } from "@/store/networkStore";
 
 export const getSelectedNetwork = () => {
-  const selectedNetworkId = localStorage.getItem("selectedNetworkId") ?? mainnetId;
+  const selectedNetworkId = localStorage.getItem("selectedNetworkId") ?? MAINNET_ID;
   const selectedNetwork = networks.find(n => n.id === selectedNetworkId);
 
   // return mainnet if selected network is not found
@@ -16,7 +16,7 @@ export const useSelectedNetwork = () => {
   const [selectedNetwork, setSelectedNetwork] = useAtom(networkStore.selectedNetwork);
 
   useEffectOnce(() => {
-    const selectedNetworkId = localStorage.getItem("selectedNetworkId") ?? mainnetId;
+    const selectedNetworkId = localStorage.getItem("selectedNetworkId") ?? MAINNET_ID;
     setSelectedNetwork(networks.find(n => n.id === selectedNetworkId) || networks[0]);
   });
 
