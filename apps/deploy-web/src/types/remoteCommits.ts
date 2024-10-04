88e993ea14b0f95c015d297d3e353caaa0b083d5
dharamveergit <dharamveersingh.em@gmail.com>
2024-10-04 21:52:35 +0530

    fix: remote types

--- apps/deploy-web/src/types/remoteCommits.ts
+++ apps/deploy-web/src/types/remoteCommits.ts
@@ -2,18 +2,9 @@ import { components } from "@octokit/openapi-types";
 export type GitCommit = components["schemas"]["commit"];
 export interface GitLabCommit {
   id: string;
-  short_id: string;
-  created_at: string;
-  parent_ids: string[];
   title: string;
   message: string;
-  author_name: string;
-  author_email: string;
   authored_date: string;
-  committer_name: string;
-  committer_email: string;
-  committed_date: string;
-  web_url: string;
 }
 
 export interface BitBucketCommit {
@@ -21,13 +12,8 @@ export interface BitBucketCommit {
     type: string;
     hash: string;
     date: string;
-    author: Author;
     message: string;
-    summary: Summary;
-    links: CommitLinks;
-    parents: Parent[];
-    rendered: RenderedMessage;
-    repository: Repository;
+    author: Author;
   }[];
   pagelen: number;
 }
@@ -35,54 +21,4 @@ export interface BitBucketCommit {
 interface Author {
   type: string;
   raw: string;
-}
-
-interface Summary {
-  type: string;
-  raw: string;
-  markup: string;
-  html: string;
-}
-
-interface CommitLinks {
-  self: Link;
-  html: Link;
-  diff: Link;
-  approve: Link;
-  comments: Link;
-  statuses: Link;
-  patch: Link;
-}
-
-interface Link {
-  href: string;
-}
-
-interface Parent {
-  hash: string;
-  links: ParentLinks;
-  type: string;
-}
-
-interface ParentLinks {
-  self: Link;
-  html: Link;
-}
-
-interface RenderedMessage {
-  message: Summary;
-}
-
-interface Repository {
-  type: string;
-  full_name: string;
-  links: RepositoryLinks;
-  name: string;
-  uuid: string;
-}
-
-interface RepositoryLinks {
-  self: Link;
-  html: Link;
-  avatar: Link;
 }
