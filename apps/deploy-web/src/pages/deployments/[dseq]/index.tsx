394db4dca0bda6e291278957cb16b1a94f86e1d7
dharamveergit <dharamveersingh.em@gmail.com>
2024-10-04 22:09:53 +0530

    fix: revert vercel deploy

--- apps/deploy-web/src/pages/deployments/[dseq]/index.tsx
+++ apps/deploy-web/src/pages/deployments/[dseq]/index.tsx
@@ -1,19 +1,19 @@
 import { DeploymentDetail } from "@src/components/deployments/DeploymentDetail";
 
-// type Props = {
-//   dseq: string;
-// };
+type Props = {
+  dseq: string;
+};
 
-const DeploymentDetailPage: React.FunctionComponent = () => {
-  return <DeploymentDetail />;
+const DeploymentDetailPage: React.FunctionComponent<Props> = ({ dseq }) => {
+  return <DeploymentDetail dseq={dseq} />;
 };
 
 export default DeploymentDetailPage;
 
-// export async function getServerSideProps({ params }) {
-//   return {
-//     props: {
-//       dseq: params?.dseq
-//     }
-//   };
-// }
+export async function getServerSideProps({ params }) {
+  return {
+    props: {
+      dseq: params?.dseq
+    }
+  };
+}
