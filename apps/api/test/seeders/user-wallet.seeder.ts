f5d7233c6ce1e7fc880e817e7d8ff66967b8a547
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-11-08 10:36:37 +0100

    feat(deployment): implement top up message sending
    Also rework managed top up logic to rely on user wallets instead of grants
    
    refs #395

---
+++ apps/api/test/seeders/user-wallet.seeder.ts
@@ -0,0 +1,32 @@
+import { faker } from "@faker-js/faker";
+
+import { UserWalletOutput } from "@src/billing/repositories";
+
+import { AkashAddressSeeder } from "@test/seeders/akash-address.seeder";
+
+export class UserWalletSeeder {
+  static create({
+    id = faker.number.int({ min: 0, max: 1000 }),
+    userId = faker.string.uuid(),
+    address = AkashAddressSeeder.create(),
+    stripeCustomerId = faker.string.alphanumeric(),
+    deploymentAllowance = faker.number.float({ min: 0, max: 1000000 }),
+    feeAllowance = faker.number.float({ min: 0, max: 1000000 }),
+    isTrialing = faker.helpers.arrayElement([true, false]),
+    createdAt = faker.date.past(),
+    updatedAt = faker.date.past()
+  }: Partial<UserWalletOutput> = {}): UserWalletOutput {
+    return {
+      id,
+      userId,
+      address,
+      stripeCustomerId,
+      deploymentAllowance,
+      feeAllowance,
+      isTrialing,
+      creditAmount: deploymentAllowance,
+      createdAt,
+      updatedAt
+    };
+  }
+}
