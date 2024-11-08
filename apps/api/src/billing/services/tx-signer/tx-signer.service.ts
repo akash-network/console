2897556129259f782bcbd185ee3dccd65ecea21b
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-11-08 15:36:06 +0100

    feat(deployment): implement clean up of managed deployments
    refs #395

--- apps/api/src/billing/services/tx-signer/tx-signer.service.ts
+++ apps/api/src/billing/services/tx-signer/tx-signer.service.ts
@@ -70,7 +70,7 @@ export class TxSignerService {
     });
   }
 
-  private async getClientForAddressIndex(addressIndex: number): Promise<SimpleSigningStargateClient> {
+  async getClientForAddressIndex(addressIndex: number): Promise<SimpleSigningStargateClient> {
     const wallet = await this.getWalletForAddressIndex(addressIndex);
     const client = await SigningStargateClient.connectWithSigner(this.config.RPC_NODE_ENDPOINT, wallet, {
       registry: this.registry
