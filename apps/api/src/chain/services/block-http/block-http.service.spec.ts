bbedcef9bce8dbb85a6c2ee5825c511dc67cb41f
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-11-21 12:08:04 -0500

    fix(deployment): test fix

--- apps/api/src/chain/services/block-http/block-http.service.spec.ts
+++ apps/api/src/chain/services/block-http/block-http.service.spec.ts
@@ -3,15 +3,17 @@ import "@test/mocks/logger-service.mock";
 import { BlockHttpService as BlockHttpServiceCommon } from "@akashnetwork/http-sdk";
 import { faker } from "@faker-js/faker";
 
+import { BlockRepository } from "@src/chain/repositories/block.repository";
 import { BlockHttpService } from "./block-http.service";
 
 describe(BlockHttpService.name, () => {
   let service: BlockHttpService;
   let blockHttpService: BlockHttpServiceCommon;
+  let blockRepository: BlockRepository;
 
   beforeEach(() => {
     blockHttpService = new BlockHttpServiceCommon();
-    service = new BlockHttpService(blockHttpService);
+    service = new BlockHttpService(blockHttpService, blockRepository);
   });
 
   it("should get current height", async () => {
