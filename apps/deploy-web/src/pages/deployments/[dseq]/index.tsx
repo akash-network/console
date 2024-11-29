39ff40ef91f38862fe0dee005c7f7b42f032d08e
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-11-29 09:29:01 +0100

    refactor(template): use env var for cicd template check
    refs #477

--- apps/deploy-web/src/pages/deployments/[dseq]/index.tsx
+++ apps/deploy-web/src/pages/deployments/[dseq]/index.tsx
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 import { DeploymentDetail } from "@src/components/deployments/DeploymentDetail";
-import { CI_CD_TEMPLATE_ID } from "@src/config/remote-deploy.config";
 import { getValidatedServerSideProps } from "@src/lib/nextjs/getValidatedServerSIdeProps";
-import { services } from "@src/services/http/http-server.service";
 
 export default DeploymentDetail;
 
@@ -14,11 +12,8 @@ const contextSchema = z.object({
 });
 
 export const getServerSideProps = getValidatedServerSideProps(contextSchema, async ({ params }) => {
-  const remoteDeployTemplate = await services.template.findById(CI_CD_TEMPLATE_ID);
-
   return {
     props: {
-      remoteDeployTemplate,
       dseq: params.dseq
     }
   };
