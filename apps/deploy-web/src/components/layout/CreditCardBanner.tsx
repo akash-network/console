a5aa61bff15bfbb4f2e4bdf7664f983f79b10f3a
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-12-04 15:59:32 -0500

    fix(billing): banner light mode styling
    (#520)

--- apps/deploy-web/src/components/layout/CreditCardBanner.tsx
+++ apps/deploy-web/src/components/layout/CreditCardBanner.tsx
@@ -8,7 +8,7 @@ export function CreditCardBanner() {
     <div className="fixed top-0 z-10 flex h-[40px] w-full items-center justify-center space-x-4 bg-primary px-3 py-2">
       <span className="text-sm font-semibold text-white">Credit Card payments are now available!</span>
 
-      {!hasManagedWallet && <ConnectManagedWalletButton className="mb-2 mr-2 w-full md:mb-0 md:w-auto" size="sm" />}
+      {!hasManagedWallet && <ConnectManagedWalletButton className="mb-2 mr-2 w-full md:mb-0 md:w-auto text-white hover:text-white" size="sm" variant="text" />}
     </div>
   );
 }
