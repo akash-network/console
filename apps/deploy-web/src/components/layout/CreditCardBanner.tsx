b4e070547ae9f9cbe4ec96d6d7a5a61e2bba3823
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-12-05 13:11:49 -0500

    fix(billing): mobile banner styling
    (#525)

--- apps/deploy-web/src/components/layout/CreditCardBanner.tsx
+++ apps/deploy-web/src/components/layout/CreditCardBanner.tsx
@@ -5,10 +5,10 @@ export function CreditCardBanner() {
   const { hasManagedWallet } = useWallet();
 
   return (
-    <div className="fixed top-0 z-10 flex h-[40px] w-full items-center justify-center space-x-4 bg-primary px-3 py-2">
-      <span className="text-sm font-semibold text-white">Credit Card payments are now available!</span>
+    <div className="fixed top-0 z-10 flex h-[40px] w-full items-center justify-center bg-primary px-3 py-2 md:space-x-4">
+      <span className="text-xs font-semibold text-white md:text-sm">Credit Card payments are now available!</span>
 
-      {!hasManagedWallet && <ConnectManagedWalletButton className="mb-2 mr-2 w-full md:mb-0 md:w-auto text-white hover:text-white" size="sm" variant="text" />}
+      {!hasManagedWallet && <ConnectManagedWalletButton className="flex-shrink-0 text-white hover:text-white" size="sm" variant="text" />}
     </div>
   );
 }
