c45835c4236d9c32e7d3c878c05530aa0cc7978d
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-12-10 10:30:16 +0100

    ci: implement PR driven release workflow
    refs #345

--- packages/releaser/.release-it.js
+++ packages/releaser/.release-it.js
@@ -1,25 +1,9 @@
-const parseArgs = require('yargs-parser');
-const { addBangNotes } = require('conventional-changelog-conventionalcommits/utils')
+const { addBangNotes } = require("conventional-changelog-conventionalcommits/utils");
 const { DEFAULT_COMMIT_TYPES } = require("conventional-changelog-conventionalcommits");
 
 const version = "${version}";
 const packageName = process.env.npm_package_name;
 const scope = packageName.split("/")[1];
-const pathParts = process.env.npm_package_json.split('/');
-const app = pathParts[pathParts.length - 2];
-
-const options = parseArgs(process.argv, {
-  string: ['repo', 'preRelease'],
-  boolean: ['force-build'],
-  alias: { r: 'repo', f: 'force-build' },
-});
-
-if (!options.repo) {
-  console.error('"-r --repo" not specified');
-  process.exit(1);
-}
-
-const isPromotion = !options.preRelease;
 
 module.exports = {
   plugins: {
@@ -31,31 +15,31 @@ module.exports = {
         path: "."
       },
       tagPrefix: `${scope}/v`,
-      skipUnstable: isPromotion,
-      whatBump (commits) {
-        let level = undefined
-        let breakings = 0
-        let features = 0
+      whatBump(commits) {
+        let level = undefined;
+        let breakings = 0;
+        let features = 0;
 
         commits.forEach(commit => {
-          const isHiddenType = DEFAULT_COMMIT_TYPES.find((type) => type.type === commit.type)?.hidden || false;
-          addBangNotes(commit)
+          const isHiddenType = DEFAULT_COMMIT_TYPES.find(type => type.type === commit.type)?.hidden || false;
+          addBangNotes(commit);
           if (commit.notes.length > 0) {
-            breakings += commit.notes.length
-            level = 0
-          } else if (commit.type === 'feat' || commit.type === 'feature') {
-            features += 1
-            if (level === 2 || typeof level === 'undefined') {
-              level = 1
+            breakings += commit.notes.length;
+            level = 0;
+          } else if (commit.type === "feat" || commit.type === "feature") {
+            features += 1;
+            if (level === 2 || typeof level === "undefined") {
+              level = 1;
             }
-          } else if (!isHiddenType && typeof level === 'undefined') {
+          } else if (!isHiddenType && typeof level === "undefined") {
             level = 2;
           }
-        })
+        });
 
         return {
           level,
-          reason: breakings === 1
+          reason:
+            breakings === 1
               ? `There is ${breakings} BREAKING CHANGE and ${features} features`
               : `There are ${breakings} BREAKING CHANGES and ${features} features`
         };
@@ -63,24 +47,15 @@ module.exports = {
     }
   },
   git: {
-    push: true,
+    push: false,
+    tag: false,
     tagName: `${scope}/v${version}`,
     commitsPath: ".",
     commitMessage: `chore(release): released version ${scope}/v${version}`,
-    requireCommits: !isPromotion,
-    requireCommitsFail: false,
+    requireCommitsFail: false
   },
   npm: {
     publish: false,
     versionArgs: ["--workspaces false"]
-  },
-  github: {
-    release: true,
-    releaseName: `${scope}/v${version}`
-  },
-  hooks: {
-    'before:git:release': [
-      `build-image -r ${options.repo} -t ${version} ${options.forceBuild ? '-f' : ''} -a ${app}`,
-    ],
   }
 };
