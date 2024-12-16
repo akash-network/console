d7268f6609745a944815dd7a10c76f11dadbe0bd
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-12-16 10:26:43 -0500

    fix(wallet): start trial duplicate
    (#560)

--- apps/api/src/billing/controllers/wallet/wallet.controller.ts
+++ apps/api/src/billing/controllers/wallet/wallet.controller.ts
@@ -10,6 +10,7 @@ import { ManagedSignerService } from "@src/billing/services/managed-signer/manag
 import { RefillService } from "@src/billing/services/refill/refill.service";
 import { GetWalletOptions, WalletReaderService } from "@src/billing/services/wallet-reader/wallet-reader.service";
 import { WithTransaction } from "@src/core";
+import { Semaphore } from "@src/core/lib/semaphore.decorator";
 
 @scoped(Lifecycle.ResolutionScoped)
 export class WalletController {
@@ -20,6 +21,7 @@ export class WalletController {
     private readonly walletReaderService: WalletReaderService
   ) {}
 
+  @Semaphore()
   @WithTransaction()
   @Protected([{ action: "create", subject: "UserWallet" }])
   async create({ data: { userId } }: StartTrialRequestInput): Promise<WalletOutputResponse> {
