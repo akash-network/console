eaa42a5ecd04db87bcbd5f4d9ac75771f4e20244
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2024-12-30 11:54:00 +0100

    fix(billing): enable balances fetching back

--- apps/deploy-web/src/components/layout/CreditCardBanner.tsx
+++ apps/deploy-web/src/components/layout/CreditCardBanner.tsx
@@ -1,14 +1,14 @@
+import { useWallet } from "@src/context/WalletProvider/WalletProvider";
+import { ConnectManagedWalletButton } from "../wallet/ConnectManagedWalletButton";
+
 export function CreditCardBanner() {
+  const { hasManagedWallet } = useWallet();
+
   return (
     <div className="fixed top-0 z-10 flex h-[40px] w-full items-center justify-center bg-primary px-3 py-2 md:space-x-4">
-      <span className="text-xs font-semibold text-white md:text-sm">
-        Console is experiencing issues causing balance not being reflected accurately. Other functionality should work as expected. Reach out to the console
-        team on{" "}
-        <a href="https://discord.com/invite/akash" className="text-white underline">
-          discord
-        </a>
-        , if you experience issues.
-      </span>
+      <span className="text-xs font-semibold text-white md:text-sm">Credit Card payments are now available!</span>
+
+      {!hasManagedWallet && <ConnectManagedWalletButton className="flex-shrink-0 text-white hover:text-white" size="sm" variant="text" />}
     </div>
   );
 }
