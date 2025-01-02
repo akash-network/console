532f26e71b7347b42ca18a47bd198ea96e170840
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-01-02 20:16:43 +0100

    feat(user): remove anonymous user from localstorage if it's removed

--- apps/deploy-web/src/queries/useAnonymousUserQuery.ts
+++ apps/deploy-web/src/queries/useAnonymousUserQuery.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import * as Sentry from "@sentry/nextjs";
+import { AxiosError } from "axios";
 
 import { useWhen } from "@src/hooks/useWhen";
 import { userHttpService } from "@src/services/http/http-browser.service";
@@ -19,7 +20,9 @@ export interface UserOutput {
 }
 
 export function useAnonymousUserQuery(id?: string, options?: { enabled?: boolean }) {
-  const [userState, setUserState] = useState<{ user?: UserOutput; isLoading: boolean; token?: string }>({ isLoading: !!options?.enabled });
+  const [userState, setUserState] = useState<{ user?: UserOutput; isLoading: boolean; token?: string; error?: Error | AxiosError }>({
+    isLoading: !!options?.enabled
+  });
 
   useWhen(options?.enabled && !userState.user, async () => {
     try {
@@ -27,8 +30,8 @@ export function useAnonymousUserQuery(id?: string, options?: { enabled?: boolean
       const token = "token" in rest ? rest.token : undefined;
       setUserState({ user: fetched, token, isLoading: false });
     } catch (error) {
+      setUserState({ isLoading: false, error });
       Sentry.captureException(error);
-      setUserState({ isLoading: false });
     }
   });
 
