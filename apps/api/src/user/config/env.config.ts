8f473f0347585588f3b6607851965292137217a0
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-01-02 19:21:51 +0100

    fix(user): coerce env var properly

--- apps/api/src/user/config/env.config.ts
+++ apps/api/src/user/config/env.config.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-  STALE_ANONYMOUS_USERS_LIVE_IN_DAYS: z.number().optional().default(90)
+  STALE_ANONYMOUS_USERS_LIVE_IN_DAYS: z.number({ coerce: true }).optional().default(90)
 });
