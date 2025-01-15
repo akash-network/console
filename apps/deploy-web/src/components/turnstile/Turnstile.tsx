d485fbbf292a8a671d478eb1f03cfc208cda5e6f
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-01-15 13:21:56 +0100

    feat(auth): add a turnstile widget
    closes #627

---
+++ apps/deploy-web/src/components/turnstile/Turnstile.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { FC, useEffect, useMemo, useRef, useState } from "react";
+import { MdInfo } from "react-icons/md";
+import { Button } from "@akashnetwork/ui/components";
+import { Turnstile as ReactTurnstile, TurnstileInstance } from "@marsidev/react-turnstile";
+import classnames from "classnames";
+
+import { browserEnvConfig } from "@src/config/browser-env.config";
+
+type TurnstileStatus = "uninitialized" | "solved" | "expired" | "error" | "dismissed";
+
+const VISIBILITY_STATUSES: TurnstileStatus[] = ["uninitialized", "expired", "error"];
+
+export const Turnstile: FC = () => {
+  const turnstileRef = useRef<TurnstileInstance>();
+  const [status, setStatus] = useState<TurnstileStatus>("uninitialized");
+  const [isTimingOut, setIsTimingOut] = useState(false);
+  const isVisible = useMemo(() => !!browserEnvConfig.NEXT_PUBLIC_TURNSTILE_SITE_KEY && VISIBILITY_STATUSES.includes(status), [status]);
+  const hasActions = useMemo(() => isTimingOut || status === "error", [isTimingOut, status]);
+
+  useEffect(() => {
+    if (isVisible) {
+      const timeout = setTimeout(() => {
+        setIsTimingOut(true);
+      }, 5000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isVisible]);
+
+  return (
+    <>
+      <div className={classnames({ hidden: !isVisible }, "fixed inset-0 z-[101] flex content-center items-center justify-center bg-white bg-opacity-90")}>
+        <div className="flex flex-col items-center">
+          <h3 className="mb-2 text-2xl font-bold">We are verifying you are a human. This may take a few seconds</h3>
+          <p className="mb-8">Reviewing the security of your connection before proceeding</p>
+          <div className="h-[66px]">
+            <ReactTurnstile
+              ref={turnstileRef}
+              siteKey={browserEnvConfig.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+              onError={() => setStatus("error")}
+              onExpire={() => setStatus("expired")}
+              onSuccess={() => setStatus("solved")}
+            />
+          </div>
+          {status === "error" && <p className="text-red-600">Some error occurred</p>}
+          <div
+            className={classnames(
+              {
+                "invisible opacity-0": !hasActions,
+                "visible opacity-100": hasActions
+              },
+              "flex flex-col items-center transition-opacity duration-200"
+            )}
+          >
+            <div className="my-8">
+              <Button onClick={turnstileRef.current?.reset} className="mr-4">
+                Retry
+              </Button>
+              <Button onClick={() => setStatus("dismissed")} variant="link">
+                Dismiss
+              </Button>
+            </div>
+
+            <p>
+              <MdInfo className="mr-1 inline text-xl text-muted-foreground" />
+              <small>dismissing the check might result into some features not working properly</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- apps/deploy-web/src/pages/_app.tsx
+++ apps/deploy-web/src/pages/_app.tsx
@@ -18,6 +18,7 @@ import NProgress from "nprogress";
 import GoogleAnalytics from "@src/components/layout/CustomGoogleAnalytics";
 import { CustomIntlProvider } from "@src/components/layout/CustomIntlProvider";
 import { PageHead } from "@src/components/layout/PageHead";
+import { Turnstile } from "@src/components/turnstile/Turnstile";
 import { UserProviders } from "@src/components/user/UserProviders";
 import { BackgroundTaskProvider } from "@src/context/BackgroundTaskProvider";
 import { CertificateProvider } from "@src/context/CertificateProvider";
@@ -45,51 +46,55 @@ Router.events.on("routeChangeError", () => NProgress.done());
 
 const App: React.FunctionComponent<Props> = props => {
   const { Component, pageProps } = props;
-  return (
-    <main className={cn("h-full bg-background font-sans tracking-wide antialiased", GeistSans.variable)}>
-      <PageHead />
 
-      <AppCacheProvider {...props}>
-        <CustomIntlProvider>
-          <QueryClientProvider client={queryClient}>
-            <JotaiProvider store={store}>
-              <ThemeProvider attribute="class" defaultTheme="system" storageKey="theme" enableSystem disableTransitionOnChange>
-                <ColorModeProvider>
-                  <CustomSnackbarProvider>
-                    <GoogleAnalytics />
-                    <UserProviders>
-                      <PricingProvider>
-                        <TooltipProvider>
-                          <SettingsProvider>
-                            <CustomChainProvider>
-                              <PopupProvider>
-                                <WalletProvider>
-                                  <ChainParamProvider>
-                                    <CertificateProvider>
-                                      <BackgroundTaskProvider>
-                                        <TemplatesProvider>
-                                          <LocalNoteProvider>
-                                            <Component {...pageProps} />
-                                          </LocalNoteProvider>
-                                        </TemplatesProvider>
-                                      </BackgroundTaskProvider>
-                                    </CertificateProvider>
-                                  </ChainParamProvider>
-                                </WalletProvider>
-                              </PopupProvider>
-                            </CustomChainProvider>
-                          </SettingsProvider>
-                        </TooltipProvider>
-                      </PricingProvider>
-                    </UserProviders>
-                  </CustomSnackbarProvider>
-                </ColorModeProvider>
-              </ThemeProvider>
-            </JotaiProvider>
-          </QueryClientProvider>
-        </CustomIntlProvider>
-      </AppCacheProvider>
-    </main>
+  return (
+    <>
+      <Turnstile />
+      <main className={cn("h-full bg-background font-sans tracking-wide antialiased", GeistSans.variable)}>
+        <PageHead />
+
+        <AppCacheProvider {...props}>
+          <CustomIntlProvider>
+            <QueryClientProvider client={queryClient}>
+              <JotaiProvider store={store}>
+                <ThemeProvider attribute="class" defaultTheme="system" storageKey="theme" enableSystem disableTransitionOnChange>
+                  <ColorModeProvider>
+                    <CustomSnackbarProvider>
+                      <GoogleAnalytics />
+                      <UserProviders>
+                        <PricingProvider>
+                          <TooltipProvider>
+                            <SettingsProvider>
+                              <CustomChainProvider>
+                                <PopupProvider>
+                                  <WalletProvider>
+                                    <ChainParamProvider>
+                                      <CertificateProvider>
+                                        <BackgroundTaskProvider>
+                                          <TemplatesProvider>
+                                            <LocalNoteProvider>
+                                              <Component {...pageProps} />
+                                            </LocalNoteProvider>
+                                          </TemplatesProvider>
+                                        </BackgroundTaskProvider>
+                                      </CertificateProvider>
+                                    </ChainParamProvider>
+                                  </WalletProvider>
+                                </PopupProvider>
+                              </CustomChainProvider>
+                            </SettingsProvider>
+                          </TooltipProvider>
+                        </PricingProvider>
+                      </UserProviders>
+                    </CustomSnackbarProvider>
+                  </ColorModeProvider>
+                </ThemeProvider>
+              </JotaiProvider>
+            </QueryClientProvider>
+          </CustomIntlProvider>
+        </AppCacheProvider>
+      </main>
+    </>
   );
 };
 
