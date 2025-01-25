7031d2e545d0eb5bceb0bb6e8b3f0e1f0edd98e5
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-01-25 14:44:27 -0500

    chore: enhance documentation for provider attribute signature schema

--- packages/database/dbSchemas/akash/providerAttribute.ts
+++ packages/database/dbSchemas/akash/providerAttribute.ts
@@ -5,7 +5,7 @@ import { Required } from "../decorators/requiredDecorator";
 /**
  * ProviderAttribute model for Akash
  *
- * Provider attributes are used to store additional information about a provider.
+ * Provider attributes are used to store additional information about a provider. (MsgCreateProvider & MsgUpdateProvider)
  * This is used to store information about the provider's location, resource specifications, such as the country, region, city, etc.
  */
 @Table({
