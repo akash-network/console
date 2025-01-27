2d4f0d71ceac062e5b1f64b0779b7a1cf1d60142
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-01-26 22:02:58 -0500

    chore: add detailed documentation to day schema

--- packages/database/dbSchemas/base/day.ts
+++ packages/database/dbSchemas/base/day.ts
@@ -4,6 +4,11 @@ import { BelongsTo, Column, Default, HasMany, Model, PrimaryKey, Table } from "s
 import { Required } from "../decorators/requiredDecorator";
 import { Block } from "./block";
 
+/**
+ * Day model for Akash
+ *
+ * This is used to store the day data
+ */
 @Table({
   modelName: "day",
   indexes: [
@@ -13,16 +18,49 @@ import { Block } from "./block";
   ]
 })
 export class Day extends Model {
+  /**
+   * The database ID of the day
+   */
   @Required @PrimaryKey @Default(UUIDV4) @Column(DataTypes.UUID) id: string;
+  /**
+   * The date of the day
+   */
   @Required @Column date: Date;
+  /**
+   * The price of AKT on this day
+   */
   @Column(DataTypes.DOUBLE) aktPrice?: number;
+  /**
+   * The height of the first block in this day
+   */
   @Required @Column firstBlockHeight: number;
+  /**
+   * The height of the last block in this day
+   */
   @Column lastBlockHeight?: number;
+  /**
+   * The height of the last block in this day that has been processed by the indexer yet
+   */
   @Required @Column lastBlockHeightYet: number;
+  /**
+   * Whether the price of AKT has changed on this day
+   */
   @Required @Default(false) @Column aktPriceChanged: boolean;
 
+  /**
+   * The blocks in this day
+   */
   @HasMany(() => Block, { foreignKey: "dayId", constraints: false }) blocks: Block[];
+  /**
+   * The first block in this day
+   */
   @BelongsTo(() => Block, { foreignKey: "firstBlockHeight", constraints: false }) firstBlock: Block;
+  /**
+   * The last block in this day
+   */
   @BelongsTo(() => Block, { foreignKey: "lastBlockHeight", constraints: false }) lastBlock?: Block;
+  /**
+   * The last block in this day that has been processed by the indexer
+   */
   @BelongsTo(() => Block, { foreignKey: "lastBlockHeightYet", constraints: false }) lastBlockYet: Block;
 }
