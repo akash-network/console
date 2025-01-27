b4780d377067f83e03e0f9850d3afa6724c6a079
Jigar Patel <jigar@arc10.io>
2025-01-27 15:17:16 -0500

    feat: added google analytics

---
+++ apps/provider-console/src/components/layout/CustomGoogleAnalytics.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useReportWebVitals } from "next/web-vitals";
+import { event, GoogleAnalytics as GAnalytics } from "nextjs-google-analytics";
+
+import { browserEnvConfig } from "@src/config/browser-env.config";
+
+export default function GoogleAnalytics() {
+  useReportWebVitals(({ id, name, label, value }) => {
+    event(name, {
+      category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+      value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+      label: id, // id unique to current page load
+      nonInteraction: true // avoids affecting bounce rate.
+    });
+  });
+  return <>{!!browserEnvConfig.NEXT_PUBLIC_GA_MEASUREMENT_ID && <GAnalytics trackPageViews />}</>;
+}
