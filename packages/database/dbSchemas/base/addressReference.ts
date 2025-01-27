8c62d202850b2256966a3e45f9ab5286db69abec
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-01-26 21:43:31 -0500

    chore: add detailed documentation to address reference schema

--- packages/database/dbSchemas/base/addressReference.ts
+++ packages/database/dbSchemas/base/addressReference.ts
@@ -5,6 +5,11 @@ import { Required } from "../decorators/requiredDecorator";
 import { Message } from "./message";
 import { Transaction } from "./transaction";
 
+/**
+ * AddressReference model for Base
+ *
+ * This is used to store the reference of an address to a transaction or message
+ */
 @Table({
   modelName: "addressReference",
   indexes: [
@@ -13,11 +18,30 @@ import { Transaction } from "./transaction";
   ]
 })
 export class AddressReference extends Model {
+  /**
+   * The ID of the database transaction that this address reference belongs to
+   */
   @Required @Column(DataTypes.UUID) transactionId: string;
+  /**
+   * The ID of the database message that this address reference belongs to
+   */
   @Column(DataTypes.UUID) messageId?: string;
+  /**
+   * The address that this reference belongs to
+   */
   @Required @Column address: string;
+  /**
+   * The type of the reference
+   * ex: Signer, Receiver, Sender.
+   */
   @Required @Column type: string;
 
+  /**
+   * The message that this address reference belongs to
+   */
   @BelongsTo(() => Message, "messageId") message?: Message;
+  /**
+   * The transaction that this address reference belongs to
+   */
   @BelongsTo(() => Transaction, "transactionId") transaction: Transaction;
 }
