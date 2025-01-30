877e0f1cf1337d71e9d06f76d4c14aaf218e6e71
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-01-30 12:27:47 -0500

    fix(dx): fix e2e tests
    (#741)

--- apps/deploy-web/tailwind.config.ts
+++ apps/deploy-web/tailwind.config.ts
@@ -1,2 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-explicit-any
+declare const require: any;
 export default require("@akashnetwork/ui/tailwind")("deploy-web");
