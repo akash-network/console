0ca2745e7cca0f1a93ab016dd16aeb09d710bf9d
Jigar Patel <97186961+jigar-arc10@users.noreply.github.com>
2025-02-04 11:40:48 -0500

    fix(provider): change dynamic message for signing (#778)
    * fix(provider): change dynamic message for signing
    
    * fix: removed ga comment
    
    * fix: added dynamic url and missing walletId

--- apps/provider-console/src/utils/walletUtils.ts
+++ apps/provider-console/src/utils/walletUtils.ts
@@ -1,3 +1,4 @@
+import { browserEnvConfig } from "@src/config/browser-env.config";
 import { mainnetId } from "./constants";
 
 export type LocalWalletDataType = {
@@ -75,5 +76,7 @@ export function updateLocalStorageWalletName(address: string, name: string) {
 }
 
 export function getNonceMessage(nonce: string, walletAddress: string) {
-  return `provider-console-beta.akash.network wants you to sign in with your Keplr account - ${walletAddress} using Nonce - ${nonce}`;
+  const domain = window.location.origin;
+  const baseDomain = domain === "http://localhost:3000" ? browserEnvConfig.NEXT_PUBLIC_BETA_URL : domain;
+  return `${baseDomain} wants you to sign in with your Keplr account - ${walletAddress} using Nonce - ${nonce}`;
 }
