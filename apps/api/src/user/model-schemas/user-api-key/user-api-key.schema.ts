a6c0a9c2a00553b18ffcf1b7c65fce777c823f10
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-02-04 15:50:16 -0500

    feat(user): add api key database schema

---
+++ apps/api/src/user/model-schemas/user-api-key/user-api-key.schema.ts
@@ -0,0 +1,24 @@
+import { boolean, index, pgTable, timestamp, unique, uuid, varchar } from "drizzle-orm/pg-core";
+
+import { Users } from "@src/user/model-schemas";
+
+export const UserApiKeys = pgTable(
+  "user_api_keys",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id")
+      .references(() => Users.id, { onDelete: "cascade" })
+      .notNull(),
+    apiKey: varchar("api_key").notNull(),
+    description: varchar("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+    expiresAt: timestamp("expires_at"),
+    isActive: boolean("is_active").default(true).notNull()
+  },
+  table => ({
+    apiKeyIdx: unique("api_key_idx").on(table.apiKey),
+    userIdIdx: index("user_id_idx").on(table.userId),
+    isActiveIdx: index("is_active_idx").on(table.isActive)
+  })
+);
