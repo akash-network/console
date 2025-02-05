f44d8052ac957bf12a9c77d5204f3f6167d5342d
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-02-05 18:05:56 -0500

    feat(deployment): add new migration

---
+++ apps/api/drizzle/0011_certain_archangel.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS "api_keys" (
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"api_key" varchar NOT NULL,
+	"name" varchar NOT NULL,
+	"description" varchar,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"expires_at" timestamp,
+	"is_active" boolean DEFAULT true NOT NULL,
+	CONSTRAINT "api_keys_api_key_unique" UNIQUE("api_key")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "api_keys" ADD CONSTRAINT "api_keys_user_id_userSetting_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."userSetting"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
