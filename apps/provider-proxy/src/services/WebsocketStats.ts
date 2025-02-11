7bc5612e4eec20899c88ab657fedd352405e7501
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-02-11 17:47:38 +0200

    refactor: changes structure and reduce side-effects in provider proxy (#831)

--- apps/provider-proxy/src/services/WebsocketStats.ts
+++ apps/provider-proxy/src/services/WebsocketStats.ts
@@ -1,13 +1,75 @@
-import { ClientWebSocketStats } from "../ClientSocketStats";
+import { v4 as uuidv4 } from "uuid";
 
 export class WebsocketStats {
   private readonly items: ClientWebSocketStats[] = [];
 
-  add(item: ClientWebSocketStats): void {
+  create(): ClientWebSocketStats {
+    const item = new ClientWebSocketStats(uuidv4());
     this.items.push(item);
+    return item;
   }
 
   getItems(): ReadonlyArray<ClientWebSocketStats> {
     return this.items;
   }
 }
+
+export class ClientWebSocketStats {
+  readonly id: string;
+  private openedOn: Date;
+  private closedOn?: Date;
+  private usage: WebSocketUsage = "Unknown";
+
+  private usageStats: Record<WebSocketUsage, { count: number; data: number }> = {
+    StreamLogs: { count: 0, data: 0 },
+    StreamEvents: { count: 0, data: 0 },
+    Shell: { count: 0, data: 0 },
+    DownloadLogs: { count: 0, data: 0 },
+    Unknown: { count: 0, data: 0 }
+  };
+
+  constructor(id: string) {
+    this.id = id;
+    this.openedOn = new Date();
+  }
+
+  setUsage(usage: WebSocketUsage) {
+    this.usage = usage;
+
+    if (usage !== "Unknown") {
+      this.usageStats[usage].count += 1;
+    }
+  }
+
+  logDataTransfer(dataTransferred: number) {
+    this.usageStats[this.usage].data += dataTransferred;
+  }
+
+  close() {
+    this.closedOn = new Date();
+  }
+
+  isClosed() {
+    return !!this.closedOn;
+  }
+
+  getStats() {
+    return {
+      id: this.id,
+      openedOn: this.openedOn,
+      closedOn: this.closedOn,
+      usageStats: this.usageStats,
+      totalStats: (Object.keys(this.usageStats) as WebSocketUsage[]).reduce(
+        (s, n) => {
+          return {
+            count: s.count + this.usageStats[n].count,
+            data: s.data + this.usageStats[n].data
+          };
+        },
+        { count: 0, data: 0 }
+      )
+    };
+  }
+}
+
+export type WebSocketUsage = "StreamLogs" | "StreamEvents" | "Shell" | "DownloadLogs" | "Unknown";
