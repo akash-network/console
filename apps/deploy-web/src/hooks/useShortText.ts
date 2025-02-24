64f8fa02a5b98a75b08382db97c0534ed26ed959
József Kozma <jozsef.kozma@gmail.com>
2025-02-24 15:49:19 +0100

    fix(provider): render longer provider names, make them copiable (#898)
    * fix(provider): render longer provider names, make them copiable
    
    closes #858
    
    * fix(deployment): also use CopyTextToClipboardButton on URIs
    
    refs #858

--- apps/deploy-web/src/hooks/useShortText.ts
+++ apps/deploy-web/src/hooks/useShortText.ts
@@ -2,8 +2,9 @@ export const getShortText = (text: string = "", length: number) => {
   return text.length < length ? text : `${text.substring(0, length - 3)}...`;
 };
 
+export const SPLIT_TEXT_GLUE = "...";
 export const getSplitText = (text: string, start: number = 5, end: number = 5) => {
-  const splittedText = [text?.slice(0, start), "...", text?.slice(text?.length - end)].join("");
+  const splittedText = [text?.slice(0, start), SPLIT_TEXT_GLUE, text?.slice(text?.length - end)].join("");
 
   return splittedText;
 };
