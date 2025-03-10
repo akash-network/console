fb1390f84dd63de553dc1bc0bd1555ae192eb58a
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-03-10 11:38:56 +0100

    test(wallet): creates a unique wallet for each spec for each run

---
+++ apps/api/test/custom-jest-environment.ts
@@ -0,0 +1,24 @@
+import type { EnvironmentContext, JestEnvironmentConfig } from "@jest/environment";
+import NodeEnvironment from "jest-environment-node";
+
+import { localConfig } from "./services/local.config";
+import { TestWalletService } from "./services/test-wallet.service";
+
+export default class CustomJestEnvironment extends NodeEnvironment {
+  private readonly path: string;
+
+  constructor(config: JestEnvironmentConfig, context: EnvironmentContext) {
+    super(config, context);
+    this.path = context.testPath;
+  }
+
+  async setup() {
+    await super.setup();
+    if (localConfig.MASTER_WALLET_MNEMONIC) {
+      this.global.process.env.MASTER_WALLET_MNEMONIC = localConfig.MASTER_WALLET_MNEMONIC;
+    } else {
+      const mnemonic = TestWalletService.instance.getMnemonic(this.path);
+      this.global.process.env.MASTER_WALLET_MNEMONIC = mnemonic;
+    }
+  }
+}
