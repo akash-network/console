f5ade37ba7027c58a0a22a558c23a69bdd991cff
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-03-12 11:41:18 -0400

    feat(analytics): add pixels scripts

--- apps/deploy-web/src/components/layout/PageHead.tsx
+++ apps/deploy-web/src/components/layout/PageHead.tsx
@@ -10,6 +10,10 @@ export const PageHead: React.FunctionComponent<{ pageSeo?: NextSeoProps }> = ({ 
         <meta charSet="utf-8" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
         <link rel="icon" href="/favicon.ico" />
+
+        <script async src="https://pxl.growth-channel.net/s/8d425860-cf3c-49cf-a459-069a7dc7b1f8"></script>
+        <script async src="https://pxl.growth-channel.net/s/e94b4a7a-8431-4b9b-a679-290a1dbbab1b"></script>
+        <script async src="https://pxl.growth-channel.net/s/76250b26-c260-4776-874b-471ed290230d"></script>
       </Head>
 
       <DefaultSeo
