0d164f4179462117aaa51041237e71b71ef86974
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-03-12 20:52:35 -0400

    fix(provider): replace rpc for proxy

--- packages/net/src/NetConfig.ts
+++ packages/net/src/NetConfig.ts
@@ -4,7 +4,7 @@ export type SupportedChainNetworks = keyof typeof netConfigData;
 
 export class NetConfig {
   getBaseAPIUrl(network: SupportedChainNetworks): string {
-    return netConfigData[network].apiUrls[0];
+    return netConfigData[network].apiUrls[1];
   }
 
   getSupportedNetworks(): SupportedChainNetworks[] {
