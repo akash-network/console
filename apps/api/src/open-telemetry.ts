8551b0216418122057779ffaf973144946c9c450
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-04-08 17:11:00 +0200

    fix(observability): remove manual instrumentations
    these will be set up on infra level

--- apps/api/src/open-telemetry.ts
+++ apps/api/src/open-telemetry.ts
@@ -1,14 +1,5 @@
 import { LoggerService } from "@akashnetwork/logging";
 import { context, trace } from "@opentelemetry/api";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-
-new NodeTracerProvider().register();
-
-registerInstrumentations({
-  instrumentations: [new HttpInstrumentation()]
-});
 
 LoggerService.mixin = () => {
   const currentSpan = trace.getSpan(context.active());
