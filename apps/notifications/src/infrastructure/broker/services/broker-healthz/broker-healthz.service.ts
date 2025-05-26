32c01ba1051125520b704e0cdf900b978f5b9eb5
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-05-26 09:16:18 +0200

    feat(notifications): implements healthz

---
+++ apps/notifications/src/infrastructure/broker/services/broker-healthz/broker-healthz.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from "@nestjs/common";
+import { differenceInMilliseconds } from "date-fns";
+import { millisecondsInMinute } from "date-fns/constants";
+import { Client } from "pg";
+
+import { LoggerService } from "@src/common/services/logger/logger.service";
+import { HealthzService, ProbeResult } from "@src/common/types/healthz.type";
+import { StateService } from "@src/infrastructure/broker/services/state/state.service";
+
+@Injectable()
+export class BrokerHealthzService implements HealthzService {
+  name = "broker";
+
+  private dbFailedAt?: Date;
+
+  constructor(
+    private readonly stateService: StateService,
+    private readonly db: Client,
+    private readonly loggerService: LoggerService
+  ) {}
+
+  async getReadinessStatus(): Promise<ProbeResult> {
+    const isPostgresReady = await this.isPostgresReady();
+    const isBrokerReady = this.stateService.getState() === "active";
+
+    return {
+      status: isPostgresReady && isBrokerReady ? "ok" : "error",
+      data: {
+        postgres: isPostgresReady,
+        broker: isBrokerReady
+      }
+    };
+  }
+
+  async getLivenessStatus(threshold = millisecondsInMinute): Promise<ProbeResult> {
+    const isPostgresAlive = await this.isPostgresAlive(threshold);
+    const isBrokerReady = this.stateService.getState() === "active";
+
+    return {
+      status: isPostgresAlive ? "ok" : "error",
+      data: {
+        postgres: isPostgresAlive,
+        broker: isBrokerReady
+      }
+    };
+  }
+
+  private async isPostgresReady() {
+    return this.isPostgresConnected();
+  }
+
+  private async isPostgresAlive(threshold = millisecondsInMinute) {
+    if (await this.isPostgresConnected()) {
+      return true;
+    }
+
+    const dbFailingFor = this.dbFailedAt ? differenceInMilliseconds(new Date(), this.dbFailedAt) : 0;
+
+    return dbFailingFor < threshold;
+  }
+
+  private async isPostgresConnected(): Promise<boolean> {
+    try {
+      await this.db.query("SELECT 1");
+      this.dbFailedAt = undefined;
+
+      return true;
+    } catch (error) {
+      this.dbFailedAt = this.dbFailedAt || new Date();
+      this.loggerService.error(error);
+
+      return false;
+    }
+  }
+}
---
+++ apps/notifications/src/infrastructure/broker/services/state/state.service.spec.ts
@@ -0,0 +1,76 @@
+import { Test, type TestingModule } from "@nestjs/testing";
+import type { MockProxy } from "jest-mock-extended";
+import { Client } from "pg";
+import PgBoss from "pg-boss";
+
+import { PgBossHandlerService } from "@src/infrastructure/broker/services/pg-boss-handler/pg-boss-handler.service";
+import { StateService } from "./state.service";
+
+import { MockProvider } from "@test/mocks/provider.mock";
+
+describe(StateService.name, () => {
+  it("should be defined", async () => {
+    const { service } = await setup();
+    expect(service).toBeDefined();
+  });
+
+  it("should initialize with state 'stopped'", async () => {
+    const { service } = await setup();
+    expect(service.getState()).toBe("stopped");
+  });
+
+  it("should set state to 'active' after bootstrap", async () => {
+    const { service, pgBossHandlerService } = await setup();
+
+    await service.onApplicationBootstrap();
+
+    expect(pgBossHandlerService.startAllHandlers).toHaveBeenCalled();
+    expect(service.getState()).toBe("active");
+  });
+
+  it("should stop pgBoss and pg client and set state to 'stopped' on shutdown", async () => {
+    const { service, boss, pg } = await setup();
+
+    await service.onApplicationShutdown();
+
+    expect(boss.stop).toHaveBeenCalled();
+    expect(pg.end).toHaveBeenCalled();
+    expect(service.getState()).toBe("stopped");
+  });
+
+  it("should update state to 'stopped' when PgBoss emits 'stopped'", async () => {
+    const { service, boss } = await setup();
+
+    const stoppedListener = boss.on.mock.calls.find(([event]) => event === "stopped")?.[1];
+    expect(stoppedListener).toBeDefined();
+
+    stoppedListener?.();
+
+    expect(service.getState()).toBe("stopped");
+  });
+
+  async function setup(): Promise<{
+    module: TestingModule;
+    service: StateService;
+    pgBossHandlerService: MockProxy<PgBossHandlerService>;
+    boss: MockProxy<PgBoss>;
+    pg: MockProxy<Client>;
+  }> {
+    const module = await Test.createTestingModule({
+      providers: [StateService, MockProvider(PgBossHandlerService), MockProvider(PgBoss), MockProvider(Client)]
+    }).compile();
+
+    const service = module.get(StateService);
+    const pgBossHandlerService = module.get<MockProxy<PgBossHandlerService>>(PgBossHandlerService);
+    const boss = module.get<MockProxy<PgBoss>>(PgBoss);
+    const pg = module.get<MockProxy<Client>>(Client);
+
+    return {
+      module,
+      service,
+      pgBossHandlerService,
+      boss,
+      pg
+    };
+  }
+});
