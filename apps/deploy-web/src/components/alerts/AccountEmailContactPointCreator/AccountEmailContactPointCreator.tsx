02e243abe9a8c6ef4a5f7fceb957ff5342b5b879
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-06-05 11:57:45 +0200

    test(alert): covers AccountEmailContactPointCreator

--- apps/deploy-web/src/components/alerts/AccountEmailContactPointCreator/AccountEmailContactPointCreator.tsx
+++ apps/deploy-web/src/components/alerts/AccountEmailContactPointCreator/AccountEmailContactPointCreator.tsx
@@ -1,25 +1,28 @@
+import type { FC } from "react";
 import { LoadingButton } from "@akashnetwork/ui/components";
 
+import type { ChildrenProps } from "@src/components/alerts/ContactPointCreateContainer/ContactPointCreateContainer";
 import { ContactPointCreateContainer } from "@src/components/alerts/ContactPointCreateContainer/ContactPointCreateContainer";
 import { useUser } from "@src/hooks/useUser";
 
+export const AccountEmailContactPointCreateTrigger: FC<ChildrenProps & { email: string; testId?: string }> = props => {
+  return (
+    <LoadingButton
+      data-testid={props.testId}
+      loading={props.isLoading}
+      onClick={() =>
+        props.create({
+          name: "Primary account email",
+          emails: [props.email]
+        })
+      }
+    >
+      Use my account email
+    </LoadingButton>
+  );
+};
+
 export const AccountEmailContactPointCreator = () => {
   const { email } = useUser();
-  return (
-    <ContactPointCreateContainer>
-      {props => (
-        <LoadingButton
-          loading={props.isLoading}
-          onClick={() =>
-            props.create({
-              name: "Primary account email",
-              emails: [email as string]
-            })
-          }
-        >
-          Use my account email
-        </LoadingButton>
-      )}
-    </ContactPointCreateContainer>
-  );
+  return <ContactPointCreateContainer>{props => <AccountEmailContactPointCreateTrigger {...props} email={email as string} />}</ContactPointCreateContainer>;
 };
--- apps/deploy-web/src/components/alerts/ContactPointSelectForm/ContactPointSelect.tsx
+++ apps/deploy-web/src/components/alerts/ContactPointSelectForm/ContactPointSelect.tsx
@@ -2,17 +2,17 @@ import type { FC } from "react";
 import { useCallback, useState } from "react";
 import React from "react";
 import { useFormContext } from "react-hook-form";
-import { Button, FormField, FormLabel, Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@akashnetwork/ui/components";
+import { buttonVariants, FormField, FormLabel, Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@akashnetwork/ui/components";
+import { cn } from "@akashnetwork/ui/utils";
+import { Plus } from "iconoir-react";
+import Link from "next/link";
 
-import { ContactPointCreateContainer } from "@src/components/alerts/ContactPointCreateContainer/ContactPointCreateContainer";
 import type { ContactPointsOutput } from "@src/components/alerts/ContactPointsListContainer/ContactPointsListContainer";
 import { ContactPointsListContainer } from "@src/components/alerts/ContactPointsListContainer/ContactPointsListContainer";
 import { LoadingBlocker } from "@src/components/layout/LoadingBlocker/LoadingBlocker";
-import { useUser } from "@src/hooks/useUser";
 
 export const ContactPointSelect: FC = () => {
   const { control, setValue } = useFormContext();
-  const { email } = useUser();
   const [isInit, setIsInit] = useState(false);
 
   const initWithFirstValue = useCallback(
@@ -29,39 +29,34 @@ export const ContactPointSelect: FC = () => {
     <ContactPointsListContainer onFetched={initWithFirstValue}>
       {({ data, isFetched }) => (
         <LoadingBlocker isLoading={!isFetched}>
-          {data.length ? (
-            <>
-              <FormLabel htmlFor="contact-point-id">Contact Point</FormLabel>
-              <FormField
-                control={control}
-                name="contactPointId"
-                render={({ field }) => (
-                  <Select value={field.value || ""} onValueChange={field.onChange}>
-                    <SelectTrigger id="contact-point-id">
-                      <SelectValue placeholder="Select contact point" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        {data.map(contactPoint => (
-                          <SelectItem key={contactPoint.id} value={contactPoint.id}>
-                            {contactPoint.name}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                )}
-              />
-            </>
-          ) : (
-            <ContactPointCreateContainer
-              onCreate={contactPoint => {
-                setValue("contactPointId", contactPoint.id);
-              }}
-            >
-              {props => <Button onClick={() => props.create({ name: "Primary account email", emails: [email as string] })}>Use my account email</Button>}
-            </ContactPointCreateContainer>
-          )}
+          <FormLabel htmlFor="contact-point-id">Contact Point</FormLabel>
+          <div className="flex">
+            <FormField
+              control={control}
+              name="contactPointId"
+              render={({ field }) => (
+                <Select value={field.value || ""} onValueChange={field.onChange}>
+                  <SelectTrigger id="contact-point-id">
+                    <SelectValue placeholder="Select contact point" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {data.map(contactPoint => (
+                        <SelectItem key={contactPoint.id} value={contactPoint.id}>
+                          {contactPoint.name}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+            <div className="ml-2">
+              <Link href="/alerts/contact-points/new" className={cn(buttonVariants({ variant: "default" }), "inline-flex items-center")}>
+                <Plus />
+              </Link>
+            </div>
+          </div>
         </LoadingBlocker>
       )}
     </ContactPointsListContainer>
