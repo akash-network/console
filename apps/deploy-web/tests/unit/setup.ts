800a149d5ca4bd97c9d427d4f9a533f9dcb5c79e
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-06-08 06:30:46 +0300

    refactor: refactors turnstile and add tests (#1421)

--- apps/deploy-web/tests/unit/setup.ts
+++ apps/deploy-web/tests/unit/setup.ts
@@ -1,8 +1,10 @@
 import "@testing-library/jest-dom";
+import "whatwg-fetch";
 
 import { TextDecoder, TextEncoder } from "util";
 
 Object.assign(global, { TextDecoder, TextEncoder });
+
 beforeAll(() => {
   Object.defineProperty(window, "matchMedia", {
     writable: true,
