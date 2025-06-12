11eedcf30d2768e80db9d9ba9efa8d35f2e4e3b1
Sergii <sergiy.stotskiy@gmail.com>
2025-06-12 16:37:34 +0300

    feat: adds feature flags support to console-api's notifications proxy

--- apps/api/src/core/services/execution-context/execution-context.service.ts
+++ apps/api/src/core/services/execution-context/execution-context.service.ts
@@ -1,9 +1,19 @@
+import type { MongoAbility } from "@casl/ability";
 import { AsyncLocalStorage } from "node:async_hooks";
 import { singleton } from "tsyringe";
 
+import type { UserOutput } from "@src/user/repositories";
+import type { AppContext } from "../../types/app-context";
+
+interface ExecutionStorage {
+  CURRENT_USER: UserOutput;
+  ABILITY: MongoAbility;
+  HTTP_CONTEXT: AppContext;
+}
+
 @singleton()
 export class ExecutionContextService {
-  private readonly storage = new AsyncLocalStorage<Map<string, any>>();
+  private readonly storage = new AsyncLocalStorage<Map<string, unknown>>();
 
   private get context() {
     const store = this.storage.getStore();
@@ -15,12 +25,12 @@ export class ExecutionContextService {
     return store;
   }
 
-  set(key: string, value: any) {
+  set<K extends keyof ExecutionStorage>(key: K, value: ExecutionStorage[K] | undefined) {
     this.context.set(key, value);
   }
 
-  get(key: string) {
-    return this.context.get(key);
+  get<K extends keyof ExecutionStorage>(key: K): ExecutionStorage[K] | undefined {
+    return this.context.get(key) as ExecutionStorage[K] | undefined;
   }
 
   async runWithContext<R>(cb: (...args: any[]) => Promise<R>): Promise<R> {
