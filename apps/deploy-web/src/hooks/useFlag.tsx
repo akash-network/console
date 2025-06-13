f6c8b337102e4289171c3884a4ae236ecdfd9964
Sergii <sergiy.stotskiy@gmail.com>
2025-06-13 06:47:23 +0300

    refactor: removes old implementation of feature flags based on env variables

--- apps/deploy-web/src/hooks/useFlag.tsx
+++ apps/deploy-web/src/hooks/useFlag.tsx
@@ -2,6 +2,10 @@ import { useFlag as useFlagOriginal } from "@unleash/nextjs/client";
 
 import { browserEnvConfig } from "@src/config/browser-env.config";
 
-const useDummyFlag: typeof useFlagOriginal = () => true;
+const useDummyFlag: FeatureFlagHook = () => true;
 
-export const useFlag = browserEnvConfig.NEXT_PUBLIC_UNLEASH_ENABLE_ALL ? useDummyFlag : useFlagOriginal;
+export type FeatureFlag =
+  // keep new line
+  "alerts" | "console_web_anonymous_user_trial";
+export const useFlag: FeatureFlagHook = browserEnvConfig.NEXT_PUBLIC_UNLEASH_ENABLE_ALL ? useDummyFlag : useFlagOriginal;
+type FeatureFlagHook = (flag: FeatureFlag) => boolean;
