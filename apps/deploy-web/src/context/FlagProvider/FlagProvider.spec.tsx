55589565ccbfb021b031ccdbca9b82226edd0dde
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-06-16 17:08:25 +0200

    feat(auth): uses user aware feature flagging in console web

---
+++ apps/deploy-web/src/context/FlagProvider/FlagProvider.spec.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { UserAwareFlagProvider } from "./FlagProvider";
+
+import { render } from "@testing-library/react";
+
+describe(UserAwareFlagProvider.name, () => {
+  it("passes userId from useUser to the custom FlagProvider", () => {
+    const testUser = { id: "my-user-id" };
+    const customFlagProvider = ({ config, children }: any) => (
+      <div data-testid="flag-provider">
+        {config.context.userId}
+        {children}
+      </div>
+    );
+    const customUseUser = () => testUser;
+
+    const { getByTestId } = render(
+      <UserAwareFlagProvider components={{ FlagProvider: customFlagProvider, useUser: customUseUser }}>
+        <div data-testid="child" />
+      </UserAwareFlagProvider>
+    );
+
+    expect(getByTestId("flag-provider").textContent).toContain("my-user-id");
+    expect(getByTestId("child")).toBeInTheDocument();
+  });
+});
--- apps/deploy-web/src/context/FlagProvider/FlagProvider.tsx
+++ apps/deploy-web/src/context/FlagProvider/FlagProvider.tsx
@@ -1,7 +1,21 @@
 import { FlagProvider as FlagProviderOriginal } from "@unleash/nextjs";
 
 import { browserEnvConfig } from "@src/config/browser-env.config";
+import { useUser } from "@src/hooks/useUser";
+import type { FCWithChildren } from "@src/types/component";
 
 const DummyFlagProvider: typeof FlagProviderOriginal = props => <>{props.children}</>;
 
-export const FlagProvider = browserEnvConfig.NEXT_PUBLIC_UNLEASH_ENABLE_ALL ? DummyFlagProvider : FlagProviderOriginal;
+const COMPONENTS = {
+  FlagProvider: FlagProviderOriginal,
+  useUser
+};
+
+export type Props = { components?: typeof COMPONENTS };
+
+export const UserAwareFlagProvider: FCWithChildren<Props> = ({ children, components: c = COMPONENTS }) => {
+  const user = c.useUser();
+  return <c.FlagProvider config={{ context: { userId: user?.id } }}>{children}</c.FlagProvider>;
+};
+
+export const FlagProvider = browserEnvConfig.NEXT_PUBLIC_UNLEASH_ENABLE_ALL ? DummyFlagProvider : UserAwareFlagProvider;
