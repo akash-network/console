ecbd00b657a5c3bbf7c69b06530936c18dad13b3
Sergii <sergiy.stotskiy@gmail.com>
2025-06-17 04:23:40 +0300

    feat: adds auto instrumentation for provider proxy

--- apps/provider-proxy/src/setupOpenTelemetry.ts
+++ apps/provider-proxy/src/setupOpenTelemetry.ts
@@ -1,11 +1,5 @@
 import { LoggerService } from "@akashnetwork/logging";
 import { context, trace } from "@opentelemetry/api";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-
-registerInstrumentations({
-  instrumentations: [new HttpInstrumentation()]
-});
 
 LoggerService.mixin = () => {
   const currentSpan = trace.getSpan(context.active());
