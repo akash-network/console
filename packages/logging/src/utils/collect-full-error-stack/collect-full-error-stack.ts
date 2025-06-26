f85947efd64ac4b566f020d9a4691ab092fb46ab
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-06-26 05:37:27 +0300

    fix: makes axios not to throw on 400 error for getting deployment (#1552)

--- packages/logging/src/utils/collect-full-error-stack/collect-full-error-stack.ts
+++ packages/logging/src/utils/collect-full-error-stack/collect-full-error-stack.ts
@@ -1,4 +1,4 @@
-export function collectFullErrorStack(error: Error | (Error & { errors?: Error[] }) | undefined | null, indent = 0): string {
+export function collectFullErrorStack(error: Error | AggregateError | ErrorWithResponse | undefined | null, indent = 0): string {
   const currentError = error;
   if (!currentError) return "";
 
@@ -13,5 +13,32 @@ export function collectFullErrorStack(error: Error | (Error & { errors?: Error[]
     stack.push("\nErrors:", ...errorStacks);
   }
 
+  if ("response" in currentError && currentError.response) {
+    const requestedPath = currentError.response.config?.url
+      ? `${currentError.response.config.method?.toUpperCase() || "(HTTP method not specified)"} ${currentError.response.config?.url}`
+      : "Unknown request";
+    stack.push(
+      "\nResponse:",
+      `\n\nRequest: ${requestedPath}`,
+      `\n\nStatus: ${currentError.response.status}`,
+      `\n\nError: ${currentError.response.data?.message || "Not specified"} (code: ${currentError.response.data?.code || "Not specified"})`
+    );
+  }
+
   return stack.join("\n").replace(/^/gm, " ".repeat(indent));
 }
+
+type ErrorWithResponse = Error & {
+  response?: {
+    status: number;
+    config?: {
+      url: string;
+      method: string;
+    };
+    data?: {
+      message?: string;
+      code?: unknown;
+    };
+  };
+};
+type AggregateError = Error & { errors?: Error[] };
