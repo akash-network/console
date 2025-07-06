34aa633b7b0bd60b5d07383160cc546e53c5ba82
Sergii <sergiy.stotskiy@gmail.com>
2025-07-06 06:14:50 +0300

    refactor: moves services into DI

--- apps/deploy-web/src/services/feature-flag/index.ts
+++ apps/deploy-web/src/services/feature-flag/index.ts
@@ -3,4 +3,7 @@ import * as unleashModule from "@unleash/nextjs";
 import { serverEnvConfig } from "@src/config/server-env.config";
 import { FeatureFlagService } from "@src/services/feature-flag/feature-flag.service";
 
+/**
+ * @deprecated use the featureFlagService from DI
+ */
 export const featureFlagService = new FeatureFlagService(unleashModule, serverEnvConfig);
--- apps/deploy-web/src/services/server-side-notifications-api/server-side-notifications-api.service.ts
+++ apps/deploy-web/src/services/server-side-notifications-api/server-side-notifications-api.service.ts
@@ -3,6 +3,9 @@ import { requestFn } from "@openapi-qraft/react";
 
 import { serverEnvConfig } from "@src/config/server-env.config";
 
+/**
+ * @deprecated use from http-services.ts instead
+ */
 export const notificationsApi = createAPIClient({
   requestFn,
   baseUrl: serverEnvConfig.BASE_API_MAINNET_URL
