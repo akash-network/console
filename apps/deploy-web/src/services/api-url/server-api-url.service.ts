49aad971586f9a58fb84bbe05514f4f029b40ed7
Sergii <sergiy.stotskiy@gmail.com>
2025-07-11 10:53:39 +0300

    refactor: hides more services inside DI

--- apps/deploy-web/src/services/api-url/server-api-url.service.ts
+++
@@ -1,5 +0,0 @@
-import { serverEnvConfig } from "@src/config/server-env.config";
-import { ApiUrlService } from "@src/services/api-url/api-url.service";
-
-/** @deprecated use `useServices()` hook instead */
-export const serverApiUrlService = new ApiUrlService(serverEnvConfig);
--- apps/deploy-web/src/services/http/http-browser.service.ts
+++ apps/deploy-web/src/services/http/http-browser.service.ts
@@ -2,9 +2,12 @@ import { createAPIClient } from "@akashnetwork/react-query-sdk/notifications";
 import { requestFn } from "@openapi-qraft/react";
 
 import { browserEnvConfig } from "@src/config/browser-env.config";
+import { ApiUrlService } from "@src/services/api-url/api-url.service";
 import { createAppRootContainer } from "@src/services/app-di-container/app-di-container";
-import { browserApiUrlService } from "../api-url/browser-api-url.service";
 import { createChildContainer } from "../container/createContainer";
+import { BitbucketService } from "../remote-deploy/bitbucket-http.service";
+import { GitHubService } from "../remote-deploy/github-http.service";
+import { GitLabService } from "../remote-deploy/gitlab-http.service";
 import { UserProviderService } from "../user-provider/user-provider.service";
 
 const rootContainer = createAppRootContainer({
@@ -12,7 +15,7 @@ const rootContainer = createAppRootContainer({
   BASE_API_MAINNET_URL: browserEnvConfig.NEXT_PUBLIC_BASE_API_MAINNET_URL,
   BASE_PROVIDER_PROXY_URL: browserEnvConfig.NEXT_PUBLIC_PROVIDER_PROXY_URL,
   MANAGED_WALLET_NETWORK_ID: browserEnvConfig.NEXT_PUBLIC_MANAGED_WALLET_NETWORK_ID,
-  apiUrlService: browserApiUrlService
+  apiUrlService: () => new ApiUrlService(browserEnvConfig)
 });
 
 export const services = createChildContainer(rootContainer, {
@@ -22,5 +25,8 @@ export const services = createChildContainer(rootContainer, {
       requestFn,
       baseUrl: "/api/proxy",
       queryClient: services.queryClient
-    })
+    }),
+  githubService: () => new GitHubService(),
+  bitbucketService: () => new BitbucketService(),
+  gitlabService: () => new GitLabService()
 });
