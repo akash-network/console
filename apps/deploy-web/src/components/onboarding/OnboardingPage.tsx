8e480d3b6e5e8a40b601755f2cedd304981b3329
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-07-18 15:32:31 +0100

    feat(billing): refactor onboarding for testing

--- apps/deploy-web/src/components/onboarding/OnboardingPage.tsx
+++ apps/deploy-web/src/components/onboarding/OnboardingPage.tsx
@@ -3,13 +3,14 @@ import React, { type FC } from "react";
 import { CustomNextSeo } from "@src/components/shared/CustomNextSeo";
 import { domainName, UrlService } from "@src/utils/urlUtils";
 import { OnboardingContainer } from "./OnboardingContainer/OnboardingContainer";
+import { OnboardingView } from "./OnboardingView/OnboardingView";
 
 export const OnboardingPage: FC = () => {
   return (
     <div>
       <CustomNextSeo title="Free Trial" url={`${domainName}${UrlService.signup()}`} />
       <div className="container mx-auto px-4 py-12">
-        <OnboardingContainer />
+        <OnboardingContainer>{props => <OnboardingView {...props} />}</OnboardingContainer>
       </div>
     </div>
   );
