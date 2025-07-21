86e89a715f5d5e30b4780b45c2109949b0465f28
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-07-21 19:01:26 +0300

    chore: small fixes on api (#1704)

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -14,7 +14,6 @@ import { HonoErrorHandlerService } from "@src/core/services/hono-error-handler/h
 import type { OpenApiHonoHandler } from "@src/core/services/open-api-hono-handler/open-api-hono-handler";
 import { OpenApiDocsService } from "@src/core/services/openapi-docs/openapi-docs.service";
 import { RequestContextInterceptor } from "@src/core/services/request-context-interceptor/request-context.interceptor";
-import type { HonoInterceptor } from "@src/core/types/hono-interceptor.type";
 import { notificationsApiProxy } from "@src/notifications/routes/proxy/proxy.route";
 import packageJson from "../package.json";
 import { apiKeysRouter } from "./auth/routes/api-keys/api-keys.router";
@@ -98,7 +97,7 @@ const scheduler = new Scheduler({
 
 appHono.use(container.resolve(HttpLoggerIntercepter).intercept());
 appHono.use(container.resolve(RequestContextInterceptor).intercept());
-appHono.use(container.resolve<HonoInterceptor>(AuthInterceptor).intercept());
+appHono.use(container.resolve(AuthInterceptor).intercept());
 appHono.use(clientInfoMiddleware);
 
 appHono.route("/", legacyRouter);
