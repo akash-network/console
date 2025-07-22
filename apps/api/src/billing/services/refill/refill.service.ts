36893c47553b31406c8879708a6f086a2a2d3d81
József Kozma <jozsef.kozma@gmail.com>
2025-07-22 13:54:27 +0200

    fix: fix wallet creation error on stripe webhook (#1684)
    closes #1662
    
    Co-authored-by: Iaroslav Gryshaiev <ygrishajev@gmail.com>

--- apps/api/src/billing/services/refill/refill.service.ts
+++ apps/api/src/billing/services/refill/refill.service.ts
@@ -6,6 +6,7 @@ import { BillingConfig, InjectBillingConfig } from "@src/billing/providers";
 import { UserWalletOutput, UserWalletRepository } from "@src/billing/repositories";
 import { ManagedUserWalletService, WalletInitializerService } from "@src/billing/services";
 import { BalancesService } from "@src/billing/services/balances/balances.service";
+import { Semaphore } from "@src/core/lib/semaphore.decorator";
 import { AnalyticsService } from "@src/core/services/analytics/analytics.service";
 
 @singleton()
@@ -53,14 +54,8 @@ export class RefillService {
    * @param userId - The ID of the user to top up the wallet for
    */
   async topUpWallet(amountUsd: number, userId: UserWalletOutput["userId"]) {
-    let userWallet = await this.userWalletRepository.findOneBy({ userId });
-    let currentLimit: number = 0;
-
-    if (userWallet) {
-      currentLimit = await this.balancesService.retrieveDeploymentLimit(userWallet);
-    } else {
-      userWallet = await this.walletInitializerService.initialize(userId);
-    }
+    const userWallet = await this.getOrCreateUserWallet(userId);
+    const currentLimit = await this.balancesService.retrieveDeploymentLimit(userWallet);
 
     const nextLimit = currentLimit + amountUsd * 10000;
     const limits = { deployment: nextLimit, fees: this.config.FEE_ALLOWANCE_REFILL_AMOUNT };
@@ -72,5 +67,15 @@ export class RefillService {
     await this.balancesService.refreshUserWalletLimits(userWallet, { endTrial: true });
     this.analyticsService.track(userId!, "balance_top_up");
     this.logger.debug({ event: "WALLET_TOP_UP", userWallet, limits });
+  }
+
+  @Semaphore()
+  private async getOrCreateUserWallet(userId: UserWalletOutput["userId"]) {
+    const userWallet = await this.userWalletRepository.findOneBy({ userId });
+    if (userWallet) {
+      return userWallet;
+    }
+
+    return await this.walletInitializerService.initialize(userId);
   }
 }
