0ccaa4b0a8822049b461a7bd0cb064bc952c40f5
Sergii <sergiy.stotskiy@gmail.com>
2025-07-26 09:55:17 +0300

    refactor: speed up startup and fixes concurrency in shutdown

--- apps/api/src/core/services/shutdown-server/shutdown-server.ts
+++ apps/api/src/core/services/shutdown-server/shutdown-server.ts
@@ -1,33 +1,31 @@
 import type { Logger } from "@akashnetwork/logging";
 import type { ServerType } from "@hono/node-server";
 
-let isShuttingDown = false;
 /**
  * Shutdown the server and app services
  */
-export async function shutdownServer(server: ServerType, appLogger: Logger, onShutdown: () => void | Promise<void>): Promise<void> {
-  if (isShuttingDown) return;
-
-  isShuttingDown = true;
-
+export async function shutdownServer(server: ServerType, appLogger: Logger, onShutdown?: () => void | Promise<void>): Promise<void> {
   return new Promise(resolve => {
-    const shutdown = (error: unknown) => {
+    const shutdown = (error?: unknown) => {
       if (error) {
         appLogger.error({ event: "SERVER_CLOSE_ERROR", error });
       }
 
-      Promise.resolve(onShutdown())
+      Promise.resolve(onShutdown?.())
         .catch(error => {
           appLogger.error({ event: "CONTAINER_DISPOSE_ERROR", error });
         })
         .finally(() => {
-          isShuttingDown = false;
           resolve();
         });
     };
 
     try {
-      server.close(shutdown);
+      if (server.listening) {
+        server.close(shutdown);
+      } else {
+        shutdown();
+      }
     } catch (error) {
       shutdown(error);
     }
