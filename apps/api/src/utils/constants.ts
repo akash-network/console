05017acb217be1efdfbe545f868f9c8f816e718d
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-07-27 13:15:32 +0100

    feat(deployment): close trial deployments after 24h (#1725)
    * feat(deployment): close trial deployments that have been running for more than 24h
    
    * fix(analytics): gpu pricing test
    
    * fix(deployment): update test with new average block time
    
    * feat(deployment): add tests for the trial deployments clean up
    
    * feat: add clean up trial deployments job on staging
    
    * fix: max live blocks constant
    
    * fix(billing): pr fixes
    
    * fix(deployment): handle missing wallet address in trial deployments cleanup

--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -2,7 +2,7 @@ import { netConfig, type SupportedChainNetworks } from "@akashnetwork/net";
 
 import { env } from "./env";
 
-export const averageBlockTime = 6.174;
+export const averageBlockTime = 6;
 export const averageDaysInMonth = 30.437;
 export const averageHoursInAMonth = averageDaysInMonth * 24;
 export const averageBlockCountInAMonth = (averageDaysInMonth * 24 * 60 * 60) / averageBlockTime;
