abcb7dc9eaeca626e6ba69edb561ff0172cf6c1a
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-08-21 11:10:12 +0300

    fix: refactor http-sdk services to accept httpClient (#1829)

--- packages/http-sdk/src/coin-gecko/coin-gecko-http.service.ts
+++ packages/http-sdk/src/coin-gecko/coin-gecko-http.service.ts
@@ -1,24 +1,11 @@
-import type { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
-import axiosRetry from "axios-retry";
-
-import { HttpService } from "../http/http.service";
+import type { HttpClient } from "../utils/httpClient";
 import type { CoinGeckoCoinsResponse } from "./types";
 
-const RETRY_COUNT = 3;
-const RETRY_DELAY_MILLISECONDS = 100;
+export class CoinGeckoHttpService {
+  constructor(private readonly httpClient: HttpClient) {}
 
-export class CoinGeckoHttpService extends HttpService {
-  constructor(config?: Pick<AxiosRequestConfig, "baseURL">) {
-    super(config);
-
-    axiosRetry(this as unknown as AxiosInstance, {
-      retries: RETRY_COUNT,
-      retryDelay: retryCount => Math.pow(2, retryCount) * RETRY_DELAY_MILLISECONDS,
-      retryCondition: (error: AxiosError) => axiosRetry.isNetworkError(error) || (error.response?.status !== undefined && error.response.status >= 500)
-    });
-  }
-
-  async getMarketData(coin: string) {
-    return this.extractData(await this.get<CoinGeckoCoinsResponse>(`/api/v3/coins/${coin}`));
+  async getMarketData(coin: string): Promise<CoinGeckoCoinsResponse> {
+    const response = await this.httpClient.get<CoinGeckoCoinsResponse>(`/api/v3/coins/${coin}`);
+    return response.data;
   }
 }
