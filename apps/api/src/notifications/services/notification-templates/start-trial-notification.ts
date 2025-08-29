2a8141350145e0730a2d825359565edebf6d5ee1
Sergii <sergiy.stotskiy@gmail.com>
2025-08-29 13:15:13 +0300

    chore: properly format date in startTrialNotification

--- apps/api/src/notifications/services/notification-templates/start-trial-notification.ts
+++ apps/api/src/notifications/services/notification-templates/start-trial-notification.ts
@@ -2,11 +2,23 @@ import type { UserOutput } from "@src/user/repositories";
 import type { CreateNotificationInput } from "../notification/notification.service";
 
 export function startTrialNotification(user: UserOutput, vars: { trialEndsAt: string; deploymentLifetimeInHours: number }): CreateNotificationInput {
+  const trialEndsAt = new Date(vars.trialEndsAt).toLocaleString("en-US", {
+    timeZone: "UTC",
+    hour12: false,
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  });
   return {
     notificationId: `startTrial.${user.id}`,
     payload: {
       summary: "Welcome to Your Free Trial!",
-      description: `Your free trial with Akash Network has started! You now have access to all platform features. Trial deployments are limited to ${vars.deploymentLifetimeInHours} hours, and your trial will end on ${new Date(vars.trialEndsAt).toLocaleString()} by UTC. Explore and deploy on the decentralized cloud infrastructure.`
+      description:
+        `Your free trial with Akash Network has started! You now have access to all platform features. ` +
+        `Trial deployments are limited to ${vars.deploymentLifetimeInHours} hours, and your trial will end on ${trialEndsAt} by UTC. ` +
+        `Explore and deploy on the decentralized cloud infrastructure.`
     },
     user: {
       id: user.id,
