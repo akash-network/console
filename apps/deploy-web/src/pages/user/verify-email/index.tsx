b9d61d8d44d38d5dbbd08213f0d7170621d076ed
József Kozma <jozsef.kozma@gmail.com>
2025-09-08 17:05:37 +0200

    feat: auto-navigate to front page after a timeout
    refs #1863

--- apps/deploy-web/src/pages/user/verify-email/index.tsx
+++ apps/deploy-web/src/pages/user/verify-email/index.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-import { buttonVariants } from "@akashnetwork/ui/components";
-import { cn } from "@akashnetwork/ui/utils";
+import React, { useCallback, useState } from "react";
+import { AutoButton } from "@akashnetwork/ui/components";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import { ArrowRight } from "iconoir-react";
-import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { NextSeo } from "next-seo";
 
@@ -18,27 +16,35 @@ type VerificationResultProps = {
 };
 
 function VerificationResult({ isVerified }: VerificationResultProps) {
+  const gotoHome = useCallback(() => {
+    window.location.href = UrlService.home();
+  }, []);
+
   return (
     <div className="mt-10 text-center">
-      {isVerified ? <CheckCircleIcon className="mb-2 h-16 w-16 text-green-500" /> : <ErrorOutlineIcon className="mb-2 h-16 w-16 text-red-500" />}
-      <h5>
-        {isVerified ? (
-          <>
+      {isVerified ? (
+        <>
+          <CheckCircleIcon className="mb-2 h-16 w-16 text-green-500" />
+          <h5>
             Your email was verified.
             <br />
             You can continue using the application.
-          </>
-        ) : (
-          "Your email was not verified. Please try again."
-        )}
-      </h5>
-      {isVerified && (
-        <div className="pt-6">
-          <Link href={UrlService.home()} className={cn(buttonVariants({ variant: "default" }), "inline-flex items-center")}>
-            Continue
-            <ArrowRight className="ml-4" />
-          </Link>
-        </div>
+          </h5>
+          <AutoButton
+            onClick={gotoHome}
+            text={
+              <>
+                Continue <ArrowRight className="ml-4" />
+              </>
+            }
+            timeout={5000}
+          />
+        </>
+      ) : (
+        <>
+          <ErrorOutlineIcon className="mb-2 h-16 w-16 text-red-500" />
+          <h5>Your email was not verified. Please try again.</h5>
+        </>
       )}
     </div>
   );
