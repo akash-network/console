627181f4d7554483d5762c49a143bed9f645558d
József Kozma <jozsef.kozma@gmail.com>
2025-09-10 11:44:48 +0200

    refactor: simplify singleton instance of ManagementClient injected
    refs #1863

--- apps/api/src/core/providers/auth.provider.ts
+++ apps/api/src/core/providers/auth.provider.ts
@@ -1,26 +1,22 @@
 import { ManagementClient } from "auth0";
-import { container, singleton } from "tsyringe";
+import { container } from "tsyringe";
 
 import { AuthConfigService } from "@src/auth/services/auth-config/auth-config.service";
 
-@singleton()
-class ManagementClientFactory {
-  private static instance: ManagementClient | null = null;
+let managementClientInstance: ManagementClient | null = null;
 
-  static getInstance(): ManagementClient {
-    if (!ManagementClientFactory.instance) {
+container.register(ManagementClient, {
+  useFactory: () => {
+    if (!managementClientInstance) {
       const authConfig = container.resolve(AuthConfigService);
-      ManagementClientFactory.instance = new ManagementClient({
+
+      managementClientInstance = new ManagementClient({
         domain: authConfig.get("AUTH0_M2M_DOMAIN"),
         clientId: authConfig.get("AUTH0_M2M_CLIENT_ID"),
         clientSecret: authConfig.get("AUTH0_M2M_SECRET")
       });
     }
 
-    return ManagementClientFactory.instance;
+    return managementClientInstance;
   }
-}
-
-container.register(ManagementClient, {
-  useFactory: () => ManagementClientFactory.getInstance()
 });
