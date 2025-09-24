75fed6f9d6575e3ff1676cb2250b912f7b8cc2a6
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-09-24 11:26:50 +0200

    feat(bid): marks managed bids list with certs requirement
    refs #1913

--- apps/api/src/bid/controllers/bid/bid.controller.ts
+++ apps/api/src/bid/controllers/bid/bid.controller.ts
@@ -1,28 +1,17 @@
-import { BidHttpService } from "@akashnetwork/http-sdk";
-import assert from "http-assert";
 import { singleton } from "tsyringe";
 
-import { AuthService, Protected } from "@src/auth/services/auth.service";
+import { Protected } from "@src/auth/services/auth.service";
 import { ListBidsResponse } from "@src/bid/http-schemas/bid.schema";
-import { UserWalletRepository } from "@src/billing/repositories";
+import { BidService } from "@src/bid/services/bid/bid.service";
 
 @singleton()
 export class BidController {
-  constructor(
-    private readonly bidHttpService: BidHttpService,
-    private readonly authService: AuthService,
-    private readonly userWalletRepository: UserWalletRepository
-  ) {}
+  constructor(private readonly bidService: BidService) {}
 
   @Protected([{ action: "sign", subject: "UserWallet" }])
-  async list(dseq: string, userId?: string): Promise<ListBidsResponse> {
-    const { currentUser, ability } = this.authService;
+  async list(dseq: string): Promise<ListBidsResponse> {
+    const bids = await this.bidService.list(dseq);
 
-    const userWallet = await this.userWalletRepository.accessibleBy(ability, "sign").findOneByUserId(userId ?? currentUser.id);
-    assert(userWallet?.address, 404, "UserWallet Not Found");
-
-    const bids = await this.bidHttpService.list(userWallet.address, dseq);
-
-    return { data: bids as ListBidsResponse["data"] };
+    return { data: bids };
   }
 }
