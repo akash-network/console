b618851d975325bf6018068ada5daefdacf6e617
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-09-24 14:18:23 +0200

    fix(onboarding): improves onboarding notifications copy

--- apps/api/src/notifications/services/notification-templates/start-trial-notification.ts
+++ apps/api/src/notifications/services/notification-templates/start-trial-notification.ts
@@ -14,14 +14,15 @@ export function startTrialNotification(
     hour: "2-digit",
     minute: "2-digit"
   });
-  const credits = (vars.initialCredits / 1_000_000).toFixed(2);
+  const formatter = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 });
+  const credits = formatter.format(vars.initialCredits / 1_000_000);
   return {
     notificationId: `startTrial.${user.id}`,
     payload: {
       summary: `Welcome - here's ${credits} on the house!`,
       description:
         `Welcome to the Akash Supercloud - to get you going, we have loaded your account with ${credits} in trial credits! ` +
-        `Your trial will end at ${trialEndsAt} UTC` +
+        `Your trial will end at ${trialEndsAt} UTC. ` +
         `To ensure that all free trials get fair access to resources, trial deployments are limited to ${vars.deploymentLifetimeInHours} hours, ` +
         `Get started by deploying your first application today.`
     },
