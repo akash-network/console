3edfb186dd48e290e80365895cdaba439ced24b2
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-10-03 09:42:37 +0100

    feat(deployment): add lease list fallback (#1992)
    * feat(deployment): add lease list fallback
    
    * fix(deployment): pr fixes
    
    * fix(deployment): pr test fixes
    
    * fix: update snapshot
    
    * fix: tests
    
    * fix: tests

--- apps/api/src/deployment/controllers/lease/lease.controller.ts
+++ apps/api/src/deployment/controllers/lease/lease.controller.ts
@@ -5,6 +5,8 @@ import { AuthService, Protected } from "@src/auth/services/auth.service";
 import { UserWalletRepository } from "@src/billing/repositories";
 import { type GetDeploymentResponse } from "@src/deployment/http-schemas/deployment.schema";
 import { type CreateLeaseRequest } from "@src/deployment/http-schemas/lease.schema";
+import { type FallbackLeaseListResponse } from "@src/deployment/http-schemas/lease-rpc.schema";
+import { DatabaseLeaseListParams } from "@src/deployment/repositories/lease/lease.repository";
 import { LeaseService } from "@src/deployment/services/lease/lease.service";
 
 @singleton()
@@ -25,5 +27,9 @@ export class LeaseController {
     const result = await this.leaseService.createLeasesAndSendManifest(userWallet, input);
 
     return { data: result };
+  }
+
+  async listLeasesFallback(params: DatabaseLeaseListParams): Promise<FallbackLeaseListResponse> {
+    return await this.leaseService.listLeasesFallback(params);
   }
 }
