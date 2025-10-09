81a797944ca0b373229cb948411b6828a2798c74
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-10-09 14:59:20 +0200

    fix(onboarding): properly handles a non-registered user during email validation
    closes #1972

--- apps/api/test/functional/verify-email.spec.ts
+++ apps/api/test/functional/verify-email.spec.ts
@@ -1,10 +1,11 @@
-import type { ApiResponse, GetUsers200ResponseOneOfInner, GetUsersByEmailRequest } from "auth0";
+import type { ApiResponse, GetUsers200ResponseOneOfInner } from "auth0";
 import { ManagementClient } from "auth0";
 import { container } from "tsyringe";
 
 import { Auth0Service } from "@src/auth/services/auth0/auth0.service";
 import { UserAuthTokenService } from "@src/auth/services/user-auth-token/user-auth-token.service";
 import { app } from "@src/rest-app";
+import { UserRepository } from "@src/user/repositories";
 
 import { WalletTestingService } from "@test/services/wallet-testing.service";
 
@@ -69,7 +70,28 @@ describe("Syncing 'email verified' from auth0", () => {
     it("throws 404 when user is not found in auth0", async () => {
       const { user, token } = await setup({ emailVerified: false });
       const auth0Service = container.resolve(Auth0Service);
-      jest.spyOn(auth0Service, "getUserByEmail").mockResolvedValue(null);
+      jest.spyOn(auth0Service, "getUserByEmail").mockResolvedValueOnce(null);
+
+      const response = await app.request("/v1/verify-email", {
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${token}`,
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          data: {
+            email: user.email
+          }
+        })
+      });
+
+      expect(response.status).toBe(404);
+    });
+
+    it("throws 404 when user is not registered", async () => {
+      const { user, token } = await setup({ emailVerified: false });
+      const userRepository = await container.resolve(UserRepository);
+      await userRepository.deleteById(user.id);
 
       const response = await app.request("/v1/verify-email", {
         method: "POST",
@@ -95,22 +117,14 @@ describe("Syncing 'email verified' from auth0", () => {
     });
 
     const managementClient = container.resolve(ManagementClient);
-    jest
-      .spyOn(managementClient.usersByEmail, "getByEmail")
-      .mockImplementation(async ({ email }: GetUsersByEmailRequest): Promise<ApiResponse<GetUsers200ResponseOneOfInner[]>> => {
-        if (email.toLowerCase() === user.email.toLowerCase()) {
-          return Promise.resolve({
-            data: [
-              {
-                user_id: user.userId,
-                email_verified: input.emailVerified
-              }
-            ]
-          } as ApiResponse<GetUsers200ResponseOneOfInner[]>);
+    jest.spyOn(managementClient.usersByEmail, "getByEmail").mockResolvedValueOnce({
+      data: [
+        {
+          user_id: user.userId,
+          email_verified: input.emailVerified
         }
-
-        return Promise.resolve({ data: [] as GetUsers200ResponseOneOfInner[] } as ApiResponse<GetUsers200ResponseOneOfInner[]>);
-      });
+      ]
+    } as ApiResponse<GetUsers200ResponseOneOfInner[]>);
 
     const userAuthTokenService = container.resolve(UserAuthTokenService);
     jest.spyOn(userAuthTokenService, "getValidUserId").mockResolvedValue(user.userId);
