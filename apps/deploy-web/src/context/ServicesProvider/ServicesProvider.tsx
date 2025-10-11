aad1dad90f1175d3c3012c9589368554311e36a1
Sergii <sergiy.stotskiy@gmail.com>
2025-10-11 16:50:40 +0300

    fix: fixes logic of failing single request if blockchain is available

--- apps/deploy-web/src/context/ServicesProvider/ServicesProvider.tsx
+++ apps/deploy-web/src/context/ServicesProvider/ServicesProvider.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
-import { AuthzHttpService, CertificatesService, isHttpError } from "@akashnetwork/http-sdk";
+import { AuthzHttpService, CertificatesService } from "@akashnetwork/http-sdk";
 
 import { withInterceptors } from "@src/services/app-di-container/app-di-container";
 import { services as rootContainer } from "@src/services/app-di-container/browser-di-container";
 import type { DIContainer, Factories } from "@src/services/container/createContainer";
 import { createChildContainer } from "@src/services/container/createContainer";
+import type { FallbackableHttpClient } from "@src/services/createFallbackableHttpClient/createFallbackableHttpClient";
 import { createFallbackableHttpClient } from "@src/services/createFallbackableHttpClient/createFallbackableHttpClient";
 import { WalletBalancesService } from "@src/services/wallet-balances/wallet-balances.service";
 import type { SettingsContextType } from "../SettingsProvider/SettingsProviderContext";
@@ -37,37 +38,35 @@ function createAppContainer<T extends Factories>(settingsState: SettingsContextT
     walletBalancesService: () => new WalletBalancesService(di.authzHttpService, di.chainApiHttpClient, di.appConfig.NEXT_PUBLIC_MASTER_WALLET_ADDRESS),
     certificatesService: () => new CertificatesService(di.chainApiHttpClient),
     chainApiHttpClient: () => {
-      let inflightPingRequest: Promise<unknown> | undefined;
-      const chainApiHttpClient = withInterceptors(
+      let inflightPingRequest: Promise<{ isBlockchainDown: boolean }> | undefined;
+      const chainApiHttpClient: FallbackableHttpClient = withInterceptors(
         createFallbackableHttpClient(rootContainer.createAxios, rootContainer.fallbackChainApiHttpClient, {
           baseURL: settingsState.settings?.apiEndpoint,
           shouldFallback: () => !!settingsState.settings?.isBlockchainDown,
-          onUnavailableError: error => {
+          onUnavailableError: (error): Promise<void> | void => {
             if (settingsState.settings?.isBlockchainDown) return;
 
-            const BLOCKCHAIN_PING_URL = "/cosmos/base/tendermint/v1beta1/node_info";
             // ensure blockchain is really unavailable and it's not an issue with some endpoint
             inflightPingRequest ??= chainApiHttpClient
-              .get(BLOCKCHAIN_PING_URL, { adapter: "fetch", timeout: 5000 })
-              .then(() => {
-                // if blockchain is available, we want original request to fail and NOT fallback to fallbackChainApiHttpClient
-                return Promise.reject(error);
-              })
-              .catch(error => {
-                if (isHttpError(error) && error.config?.url?.includes(BLOCKCHAIN_PING_URL)) {
-                  if (settingsState.settings?.isBlockchainDown) return;
-                  settingsState.setSettings(prev => ({ ...prev, isBlockchainDown: true }));
-                  return;
-                }
-
-                return Promise.reject(error);
+              .get("/cosmos/base/tendermint/v1beta1/node_info", { adapter: "fetch", timeout: 5000 })
+              .then(() => ({ isBlockchainDown: false }))
+              .catch(() => {
+                if (settingsState.settings?.isBlockchainDown) return { isBlockchainDown: true };
+                settingsState.setSettings(prev => ({ ...prev, isBlockchainDown: true }));
+                return { isBlockchainDown: true };
               })
               .finally(() => {
                 setTimeout(() => {
                   inflightPingRequest = undefined;
-                }, 3000); // keep ping result in cache for few seconds to handle delayed requests
+                }, 10_000); // keep ping result in cache for few seconds to handle delayed requests
               });
-            return inflightPingRequest;
+            return inflightPingRequest.then(result => {
+              if (!result.isBlockchainDown) {
+                // if blockchain is available, then we have an issue wit some endpoint
+                // and want the original request to fail and NOT fallback to fallbackChainApiHttpClient
+                return Promise.reject(error);
+              }
+            });
           },
           onSuccess: () => {
             settingsState.refreshNodeStatuses();
