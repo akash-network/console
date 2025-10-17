58d9b64740fe12c0ec60922adec3a1f3cad543e5
ritoban23 <ankudutt101@gmail.com>
2025-10-17 15:19:41 +0530

    refactor: export collectOtel to eliminate duplication

--- apps/api/src/core/providers/logging.provider.ts
+++ apps/api/src/core/providers/logging.provider.ts
@@ -1,14 +1,11 @@
 import { LoggerService as LoggerServiceOriginal } from "@akashnetwork/logging";
 import { HttpLoggerIntercepter } from "@akashnetwork/logging/hono";
-import { context, trace } from "@opentelemetry/api";
+import { collectOtel } from "@akashnetwork/logging/otel";
 import { container, injectable } from "tsyringe";
 
 // Set up OpenTelemetry mixin for all LoggerService instances
 // This collects trace information and adds it to log entries
-LoggerServiceOriginal.mixin = () => {
-  const currentSpan = trace.getSpan(context.active());
-  return { ...currentSpan?.spanContext() };
-};
+LoggerServiceOriginal.mixin = collectOtel;
 
 container.register(HttpLoggerIntercepter, { useValue: new HttpLoggerIntercepter(LoggerServiceOriginal.forContext("HTTP")) });
 
