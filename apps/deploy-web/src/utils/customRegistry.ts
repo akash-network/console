773d1b8ae44bc56fd0da5681ed38d48dd5b39c05
Serhii Stotskyi <serhii@akash.network>
2025-10-18 03:51:24 +0300

    fix: fixes proto types of deployment authorization (#2080)

--- apps/deploy-web/src/utils/customRegistry.ts
+++ apps/deploy-web/src/utils/customRegistry.ts
@@ -1,11 +1,23 @@
 import * as v1 from "@akashnetwork/chain-sdk/private-types/akash.v1";
 import * as v1beta4 from "@akashnetwork/chain-sdk/private-types/akash.v1beta4";
 import * as v1beta5 from "@akashnetwork/chain-sdk/private-types/akash.v1beta5";
+import * as cosmosv1 from "@akashnetwork/chain-sdk/private-types/cosmos.v1";
+import * as cosmosv1alpha1 from "@akashnetwork/chain-sdk/private-types/cosmos.v1alpha1";
+import * as cosmosv1beta1 from "@akashnetwork/chain-sdk/private-types/cosmos.v1beta1";
+import * as cosmosv2alpha1 from "@akashnetwork/chain-sdk/private-types/cosmos.v2alpha1";
 import type { GeneratedType } from "@cosmjs/proto-signing";
 import { Registry } from "@cosmjs/proto-signing";
 
-const akashTypes: ReadonlyArray<[string, GeneratedType]> = [...Object.values(v1), ...Object.values(v1beta4), ...Object.values(v1beta5)]
-  .filter(x => "$type" in x)
+const akashTypes: ReadonlyArray<[string, GeneratedType]> = [
+  ...Object.values(v1),
+  ...Object.values(v1beta4),
+  ...Object.values(v1beta5),
+  ...Object.values(cosmosv1),
+  ...Object.values(cosmosv1beta1),
+  ...Object.values(cosmosv1alpha1),
+  ...Object.values(cosmosv2alpha1)
+]
+  .filter(x => x && "$type" in x)
   .map(x => ["/" + x.$type, x as unknown as GeneratedType]);
 
 export const registry = new Registry([...akashTypes]);
