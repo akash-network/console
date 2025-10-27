44e3f3cfffa531b5d2268aaf20149b038ccff7ff
Sergii <sergiy.stotskiy@gmail.com>
2025-10-27 06:53:17 +0100

    fix: uses formatDistanceToNow instead of differenceInX to calc time left

--- apps/api/src/notifications/services/notification-templates/before-trial-ends-notification.ts
+++ apps/api/src/notifications/services/notification-templates/before-trial-ends-notification.ts
@@ -1,4 +1,4 @@
-import { differenceInDays } from "date-fns";
+import { formatDistanceToNow } from "date-fns";
 
 import type { ResolvedValue } from "@src/notifications/services/notification-data-resolver/notification-data-resolver.service";
 import type { UserOutput } from "@src/user/repositories";
@@ -9,13 +9,13 @@ export function beforeTrialEndsNotification(
   vars: { trialEndsAt: string; paymentLink: string; remainingCredits: ResolvedValue<number>; activeDeployments: ResolvedValue<number> }
 ): CreateNotificationInput {
   const trialEndsDate = new Date(vars.trialEndsAt);
-  const daysLeft = trialEndsDate.getTime() < Date.now() ? 0 : differenceInDays(trialEndsDate, new Date());
+  const timeLeft = trialEndsDate.getTime() < Date.now() ? "in a few seconds" : formatDistanceToNow(trialEndsDate, { addSuffix: true });
   return {
-    notificationId: `beforeTrialEnds.${daysLeft}.${user.id}`,
+    notificationId: `beforeTrialEnds.${trialEndsDate.toISOString()}.${user.id}`,
     payload: {
       summary: "Your Free Trial is Ending Soon",
       description:
-        `Your free trial with Akash Network will end in ${daysLeft} days. You still have $${vars.remainingCredits} in free credits available and, ` +
+        `Your free trial with Akash Network will end ${timeLeft}. You still have $${vars.remainingCredits} in free credits available and, ` +
         `${vars.activeDeployments} deployments that will be lost when your free trial ends. ` +
         `To retain the remaining free credits and to ensure your deployments keep running when the trial ends, purchase some credits today by visiting <a href="${vars.paymentLink}">Akash Console Payment Setup</a>.`
     },
