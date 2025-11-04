520cae37573f1e4256f9511dc24daf7fefe4dbb1
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-11-04 13:55:37 -0500

    fix: remove and check

--- packages/net/src/generated/netConfigData.ts
+++ packages/net/src/generated/netConfigData.ts
@@ -29,10 +29,10 @@ export const netConfigData = {
     ]
   },
   sandbox: {
-    version: "v0.38.1",
-    faucetUrl: "http://faucet.sandbox-01.aksh.pw/",
-    apiUrls: ["https://api.sandbox-01.aksh.pw:443"],
-    rpcUrls: ["https://rpc.sandbox-01.aksh.pw:443"]
+    version: null,
+    faucetUrl: null,
+    apiUrls: [],
+    rpcUrls: []
   },
   "sandbox-2": {
     version: "v1.0.0",
@@ -41,15 +41,15 @@ export const netConfigData = {
     rpcUrls: ["https://rpc.sandbox-2.aksh.pw:443"]
   },
   "testnet-02": {
-    version: "v0.23.1-rc0",
-    faucetUrl: "https://faucet.testnet-02.aksh.pw",
-    apiUrls: ["https://api.testnet-02.aksh.pw:443"],
-    rpcUrls: ["https://rpc.testnet-02.aksh.pw:443"]
+    version: null,
+    faucetUrl: null,
+    apiUrls: [],
+    rpcUrls: []
   },
   "testnet-7": {
-    version: "v1.0.0-rc60",
-    faucetUrl: "https://faucet.dev.akash.pub/",
-    apiUrls: ["https://testnetapi.akashnet.net"],
-    rpcUrls: ["https://testnetrpc.akashnet.net:443"]
+    version: null,
+    faucetUrl: null,
+    apiUrls: [],
+    rpcUrls: []
   }
 };
