18fa5a768d8076f17d1109b3e7adbada3ed7bb95
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-11-05 14:49:05 -0500

    fix: tests

--- apps/deploy-web/tests/ui/fixture/test-env.config.ts
+++ apps/deploy-web/tests/ui/fixture/test-env.config.ts
@@ -1,12 +1,10 @@
-import type { SupportedChainNetworks } from "@akashnetwork/net";
-import { netConfig } from "@akashnetwork/net";
 import { z } from "zod";
 
 export const testEnvSchema = z.object({
   BASE_URL: z.string().default("http://localhost:3000"),
   TEST_WALLET_MNEMONIC: z.string(),
   UI_CONFIG_SIGNATURE_PRIVATE_KEY: z.string().optional(),
-  NETWORK_ID: z.enum(netConfig.getSupportedNetworks() as [SupportedChainNetworks]).default("sandbox")
+  NETWORK_ID: z.enum(["mainnet", "sandbox"]).default("sandbox")
 });
 
 export const testEnvConfig = testEnvSchema.parse({
@@ -17,7 +15,5 @@ export const testEnvConfig = testEnvSchema.parse({
 
 export const PROVIDERS_WHITELIST = {
   mainnet: ["provider.hurricane.akash.pub", "provider.europlots.com"],
-  sandbox: ["provider.provider-02.sandbox-01.aksh.pw", "provider.europlots-sandbox.com"],
-  "testnet-02": [],
-  "testnet-7": []
-} satisfies Record<SupportedChainNetworks, string[]>;
+  sandbox: ["provider.provider-02.sandbox-01.aksh.pw", "provider.europlots-sandbox.com"]
+} satisfies Record<"mainnet" | "sandbox", string[]>;
