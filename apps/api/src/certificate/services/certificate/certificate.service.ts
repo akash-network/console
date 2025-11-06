a30c3f127f79ee32d9ec18cde7c27240789dabd8
Serhii Stotskyi <serhii@akash.network>
2025-11-06 11:38:22 +0200

    refactor: moves certs and transaction tests into unit scope (#2163)

--- apps/api/src/certificate/services/certificate/certificate.service.ts
+++ apps/api/src/certificate/services/certificate/certificate.service.ts
@@ -1,11 +1,11 @@
-import { certificateManager } from "@akashnetwork/chain-sdk";
 import assert from "http-assert";
 import { singleton } from "tsyringe";
 
 import { AuthService } from "@src/auth/services/auth.service";
-import { UserWalletRepository } from "@src/billing/repositories";
+import { UserWalletOutput, UserWalletRepository } from "@src/billing/repositories";
 import { RpcMessageService } from "@src/billing/services";
 import { ManagedSignerService } from "@src/billing/services/managed-signer/managed-signer.service";
+import { CertificateManager } from "../../providers/certificate-manager.provider";
 
 interface CertificateOutput {
   certPem: string;
@@ -18,16 +18,16 @@ export class CertificateService {
   constructor(
     private readonly userWalletRepository: UserWalletRepository,
     private readonly authService: AuthService,
+    private readonly certificateManager: CertificateManager,
     private readonly rpcMessageService: RpcMessageService,
     private readonly managedSignerService: ManagedSignerService
   ) {}
 
-  async create(): Promise<CertificateOutput> {
-    const userWallet = await this.userWalletRepository.accessibleBy(this.authService.ability, "sign").findOneByUserId(this.authService.currentUser.id);
-
+  async create(input: { userId: UserWalletOutput["userId"] }): Promise<CertificateOutput> {
+    const userWallet = await this.userWalletRepository.accessibleBy(this.authService.ability, "sign").findOneByUserId(input.userId);
     assert(userWallet?.address, 404, "UserWallet not found");
 
-    const { cert: crtpem, publicKey: pubpem, privateKey: encryptedKey } = await certificateManager.generatePEM(userWallet.address);
+    const { cert: crtpem, publicKey: pubpem, privateKey: encryptedKey } = await this.certificateManager.generatePEM(userWallet.address);
     const createCertificateMsg = this.rpcMessageService.getCreateCertificateMsg(userWallet.address, crtpem, pubpem);
     const messages = [createCertificateMsg];
 
