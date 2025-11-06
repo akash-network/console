a16fc57d545c1d78a12f3de624ad85c893c56441
Serhii Stotskyi <serhii@akash.network>
2025-11-06 12:40:09 +0200

    refactor: rewrites SQL query to fetch transactions by address ref (#2149)

--- apps/indexer/drizzle/0002_gigantic_ender_wiggin.sql
+++ apps/indexer/drizzle/0002_gigantic_ender_wiggin.sql
@@ -5,5 +5,5 @@ DROP INDEX IF EXISTS "lease_owner_dseq_gseq_oseq";--> statement-breakpoint
 ALTER TABLE "bid" ALTER COLUMN "bseq" SET DEFAULT 0;--> statement-breakpoint
 ALTER TABLE "lease" ALTER COLUMN "bseq" SET DEFAULT 0;--> statement-breakpoint
 
-CREATE INDEX "bid_owner_dseq_gseq_oseq_provider_bseq" ON "bid" USING btree ("owner" text_ops,"dseq" text_ops,"gseq" int4_ops,"oseq" int4_ops,"provider" text_ops,"bseq" int4_ops);--> statement-breakpoint
-CREATE UNIQUE INDEX "lease_owner_dseq_gseq_oseq_provider_bseq" ON "lease" USING btree ("owner" text_ops,"dseq" text_ops,"gseq" int4_ops,"oseq" int4_ops,"providerAddress" text_ops,"bseq" int4_ops);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "bid_owner_dseq_gseq_oseq_provider_bseq" ON "bid" USING btree ("owner" text_ops,"dseq" text_ops,"gseq" int4_ops,"oseq" int4_ops,"provider" text_ops,"bseq" int4_ops);--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "lease_owner_dseq_gseq_oseq_provider_bseq" ON "lease" USING btree ("owner" text_ops,"dseq" text_ops,"gseq" int4_ops,"oseq" int4_ops,"providerAddress" text_ops,"bseq" int4_ops);--> statement-breakpoint
