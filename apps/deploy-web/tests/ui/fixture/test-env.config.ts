0f7ff478dffafc3ce79845588d401c89c2c8f4bd
József Kozma <jozsef.kozma@gmail.com>
2025-11-08 15:56:54 +0100

    fix: fixing e2e test for authorizations

--- apps/deploy-web/tests/ui/fixture/test-env.config.ts
+++ apps/deploy-web/tests/ui/fixture/test-env.config.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const testEnvSchema = z.object({
   BASE_URL: z.string().default("http://localhost:3000"),
   TEST_WALLET_MNEMONIC: z.string(),
+  AUTHORIZING_WALLET_MNEMONIC: z.string(),
   UI_CONFIG_SIGNATURE_PRIVATE_KEY: z.string().optional(),
   NETWORK_ID: z.enum(["mainnet", "sandbox"]).default("sandbox")
 });
@@ -10,6 +11,7 @@ export const testEnvSchema = z.object({
 export const testEnvConfig = testEnvSchema.parse({
   BASE_URL: process.env.BASE_URL,
   TEST_WALLET_MNEMONIC: process.env.TEST_WALLET_MNEMONIC,
+  AUTHORIZING_WALLET_MNEMONIC: process.env.AUTHORIZING_WALLET_MNEMONIC,
   UI_CONFIG_SIGNATURE_PRIVATE_KEY: process.env.UI_CONFIG_SIGNATURE_PRIVATE_KEY
 });
 
