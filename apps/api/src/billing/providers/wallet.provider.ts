98d227007e10751b4928169c80381a434221850f
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2025-11-18 14:18:34 +0100

    refactor(billing): cleans up top up master wallets

--- apps/api/src/billing/providers/wallet.provider.ts
+++ apps/api/src/billing/providers/wallet.provider.ts
@@ -9,16 +9,6 @@ import type { MasterWalletType } from "@src/billing/types/wallet.type";
 export const MANAGED_MASTER_WALLET: InjectionToken<Wallet> = "MANAGED_MASTER_WALLET";
 container.register(MANAGED_MASTER_WALLET, { useFactory: instancePerContainerCachingFactory(c => c.resolve(WALLET_FACTORY)(config.MASTER_WALLET_MNEMONIC)) });
 
-export const UAKT_TOP_UP_MASTER_WALLET: InjectionToken<Wallet> = "UAKT_TOP_UP_MASTER_WALLET";
-container.register(UAKT_TOP_UP_MASTER_WALLET, {
-  useFactory: instancePerContainerCachingFactory(c => c.resolve(WALLET_FACTORY)(config.UAKT_TOP_UP_MASTER_WALLET_MNEMONIC))
-});
-
-export const USDC_TOP_UP_MASTER_WALLET: InjectionToken<Wallet> = "USDC_TOP_UP_MASTER_WALLET";
-container.register(USDC_TOP_UP_MASTER_WALLET, {
-  useFactory: instancePerContainerCachingFactory(c => c.resolve(WALLET_FACTORY)(config.USDC_TOP_UP_MASTER_WALLET_MNEMONIC))
-});
-
 export const InjectWallet = (walletType: MasterWalletType) => inject(`${walletType}_MASTER_WALLET`);
 
 export const WALLET_FACTORY: InjectionToken<WalletFactory> = "WALLET_FACTORY";
