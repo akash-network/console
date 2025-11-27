944ae4811e567bd4a9109ddacf85a32bb0438188
Sergii <sergiy.stotskiy@gmail.com>
2025-11-27 14:23:22 +0100

    chore: adds local eslint plugin and rule that can detect mnemonics

---
+++ packages/dev-config/eslint-plugin-akash/rules/no-mnemonic.js
@@ -0,0 +1,21 @@
+const bip39 = require("bip39");
+
+module.exports = {
+  create: context => ({
+    Literal(node) {
+      if (typeof node.value === "string") {
+        const mnemonicRegex = /^([a-z]{3,}\s+){11,23}[a-z]{3,}$/;
+        const possibleMnemonic = node.value.trim();
+        if (mnemonicRegex.test(possibleMnemonic) && bip39.validateMnemonic(possibleMnemonic)) {
+          context.report({
+            node,
+            message:
+              "Hardcoded mnemonic phrase detected. " +
+              "Don't hardcode mnemonic phrases because it's impossible to distinguish whether its production or test mnemonic. " +
+              "Use generated mnemonic for test purposes and hide production mnemonic in secret store."
+          });
+        }
+      }
+    }
+  })
+};
