ebc70f145cf10872ea5ef603635b86fe7401c90c
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2025-12-02 19:42:10 -0500

    fix: show usdc for managed deposit

--- apps/deploy-web/src/components/sdl/DeploymentMinimumEscrowAlertText.tsx
+++ apps/deploy-web/src/components/sdl/DeploymentMinimumEscrowAlertText.tsx
@@ -1,19 +1,15 @@
 import type { FC } from "react";
-import { useState } from "react";
 
 import { useChainParam } from "@src/context/ChainParamProvider";
 import { useWallet } from "@src/context/WalletProvider";
-import { useDenomData } from "@src/hooks/useWalletBalance";
 
 export const DeploymentMinimumEscrowAlertText: FC = () => {
   const { isManaged } = useWallet();
-  const [sdlDenom] = useState("uakt");
-  const depositData = useDenomData(sdlDenom);
   const { minDeposit } = useChainParam();
 
   return isManaged ? (
     <>
-      To create a deployment, you need to have at least <b>${depositData?.min}</b> in an escrow account.{" "}
+      To create a deployment, you need to have at least <b>${minDeposit.usdc}</b> in an escrow account.{" "}
     </>
   ) : (
     <>
