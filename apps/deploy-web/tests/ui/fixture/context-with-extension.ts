c6cb87484726da985979a699fdd6fec8239bfb5a
Sergii <sergiy.stotskiy@gmail.com>
2025-12-12 02:50:44 +0100

    test: upgrades Leap extension for e2e tests

--- apps/deploy-web/tests/ui/fixture/context-with-extension.ts
+++ apps/deploy-web/tests/ui/fixture/context-with-extension.ts
@@ -1,51 +1,43 @@
 import type { BrowserContext, Page } from "@playwright/test";
 import { chromium } from "@playwright/test";
+import os from "os";
 import path from "path";
 
 import { selectChainNetwork } from "../actions/selectChainNetwork";
 import { injectUIConfig, test as baseTest } from "./base-test";
 import { testEnvConfig } from "./test-env.config";
-import { approveWalletOperation, awaitWalletAndApprove, connectWalletViaLeap, getExtensionPage } from "./wallet-setup";
+import { awaitWalletAndApprove, connectWalletViaLeap, getExtensionPage, setupWallet } from "./wallet-setup";
+
+export const PATH_TO_EXTENSION = path.join(os.tmpdir(), "console-deploy-web-e2e-tests", "Leap");
 
 // @see https://playwright.dev/docs/chrome-extensions
-export const test = baseTest.extend<{
-  context: BrowserContext;
-  extensionId: string;
-  extPage: Page;
-}>({
+export const test = baseTest.extend<ExtensionContext>({
   // eslint-disable-next-line no-empty-pattern
   context: async ({}, use) => {
-    const pathToExtension = path.join(__dirname, "Leap");
     const args = [
       // keep new line
-      `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`
+      `--disable-extensions-except=${PATH_TO_EXTENSION}`,
+      `--load-extension=${PATH_TO_EXTENSION}`
     ];
 
-    const context = await chromium.launchPersistentContext(testEnvConfig.USER_DATA_DIR, {
+    const userDataDirForTest = `${testEnvConfig.USER_DATA_DIR}-${crypto.randomUUID()}`;
+    const context = await chromium.launchPersistentContext(userDataDirForTest, {
       channel: "chromium",
       args,
       permissions: ["clipboard-read", "clipboard-write"]
     });
 
-    await use(context);
-    await context.close();
-  },
-  extensionId: async ({ context }, use) => {
-    let [background] = context.serviceWorkers();
-    if (!background) {
-      background = await context.waitForEvent("serviceworker");
+    try {
+      await use(context);
+    } finally {
+      await context.close();
     }
-
-    const extensionId = background.url().split("/")[2];
-    await use(extensionId);
   },
   page: [
-    async ({ context, extensionId }, use) => {
-      const extPage = await getExtensionPage(context, extensionId);
-
-      await approveWalletOperation(extPage);
-      const [page] = await Promise.all([context.newPage(), extPage.close()]);
+    async ({ context }, use) => {
+      const extPage = await context.waitForEvent("page", { timeout: 2_000 }).catch(() => getExtensionPage(context));
+      await setupWallet(extPage);
+      const page = await context.newPage();
       await injectUIConfig(page);
 
       if (testEnvConfig.NETWORK_ID !== "mainnet") {
@@ -53,7 +45,6 @@ export const test = baseTest.extend<{
           await page.goto(testEnvConfig.BASE_URL);
           await connectWalletViaLeap(context, page);
           await selectChainNetwork(page, testEnvConfig.NETWORK_ID);
-          await page.waitForLoadState("load");
           await connectWalletViaLeap(context, page);
         } catch (error) {
           console.log("the default network is non-functional, uses fallback");
@@ -61,15 +52,16 @@ export const test = baseTest.extend<{
           // Fallback in case the default network is non-functional.
           //  E.g., during network upgrade when sandbox is already on a different version from mainnet
           await page.goto(`${testEnvConfig.BASE_URL}?network=${testEnvConfig.NETWORK_ID}`);
-          await awaitWalletAndApprove(context, page, extensionId);
+          await awaitWalletAndApprove(context, page);
         }
       }
 
-      await use(page);
-      await page.evaluate(networkId => {
-        localStorage.clear();
-        localStorage.setItem("selectedNetworkId", networkId);
-      }, testEnvConfig.NETWORK_ID);
+      try {
+        await extPage.close();
+        await use(page);
+      } finally {
+        await page.close();
+      }
     },
     { scope: "test", timeout: 5 * 60 * 1000 }
   ]
@@ -80,7 +72,6 @@ export const expect = test.expect;
 export interface ExtensionContext {
   context: BrowserContext;
   page: Page;
-  extensionId: string;
 }
 
 declare global {
