d35580dec5f125fef511978bd995dc46f16e05ea
Sergii <sergiy.stotskiy@gmail.com>
2025-12-16 17:15:55 +0100

    fix: fixes background on login page for dark theme

--- apps/deploy-web/src/components/auth/AuthLayout/AuthLayout.tsx
+++ apps/deploy-web/src/components/auth/AuthLayout/AuthLayout.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 export function AuthLayout({ sidebar, children }: Props) {
   const theme = useCookieTheme();
-  const inversedClass = theme === "dark" ? "bg-[#171717]" : "dark";
+  const sidebarClass = theme === "dark" ? "bg-[#171717] lg:bg-[hsl(var(--background))]" : "bg-[hsl(var(--background))] dark";
   return (
     <div className="relative flex h-screen">
       <div
-        className={`absolute flex h-full w-full items-center justify-center overflow-y-auto bg-[hsl(var(--background))] lg:relative lg:w-1/2 ${inversedClass}`}
+        className={`absolute flex h-full w-full items-center justify-center overflow-y-auto lg:static lg:w-1/2 ${sidebarClass}`}
         style={{
           backgroundImage: "radial-gradient(circle, rgba(255, 255, 255, 0.07) 2px, transparent 2px)",
           backgroundSize: "24px 24px",
