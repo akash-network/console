e40abab2ce1f22573040c779876b2590f00d54de
Sergii <sergiy.stotskiy@gmail.com>
2025-12-30 05:41:51 +0100

    fix: ensures turnstile is completely disabled when NEXT_PUBLIC_TURNSTILE_ENABLED is false

--- apps/deploy-web/src/config/server-env.config.ts
+++ apps/deploy-web/src/config/server-env.config.ts
@@ -2,5 +2,5 @@ import "@akashnetwork/env-loader";
 
 import { validateRuntimeEnvVars } from "./env-config.schema";
 
-/** @deprecated use services.config from server-di-container.service.ts instead */
+/** @deprecated use services.privateConfig from server-di-container.service.ts instead */
 export const serverEnvConfig = validateRuntimeEnvVars(process.env);
