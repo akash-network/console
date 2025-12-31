3996b66c9e141f893ee8a256167d93be5866fd96
Sergii <sergiy.stotskiy@gmail.com>
2025-12-31 05:16:36 +0100

    chore: removes extra header addition to all requests in e2e tests

--- apps/deploy-web/tests/ui/fixture/test-env.config.ts
+++ apps/deploy-web/tests/ui/fixture/test-env.config.ts
@@ -9,13 +9,17 @@ export const testEnvSchema = z.object({
     .transform(url => url.replace(/\/+$/, "")),
   TEST_WALLET_MNEMONIC: z.string(),
   NETWORK_ID: z.enum(["mainnet", "sandbox", "testnet"]).default("sandbox"),
-  USER_DATA_DIR: z.string().default(path.join(tmpdir(), "akash-console-web-ui-tests", crypto.randomUUID()))
+  USER_DATA_DIR: z.string().default(path.join(tmpdir(), "akash-console-web-ui-tests", crypto.randomUUID())),
+  E2E_TESTING_CLIENT_TOKEN: z.string({
+    required_error: "This token is used to adjust configuration of the app for e2e testing. Can be any random string but should match the one used by app."
+  })
 });
 
 export const testEnvConfig = testEnvSchema.parse({
   BASE_URL: process.env.BASE_URL,
   TEST_WALLET_MNEMONIC: process.env.TEST_WALLET_MNEMONIC,
-  USER_DATA_DIR: process.env.USER_DATA_DIR
+  USER_DATA_DIR: process.env.USER_DATA_DIR,
+  E2E_TESTING_CLIENT_TOKEN: process.env.E2E_TESTING_CLIENT_TOKEN
 });
 
 export const PROVIDERS_WHITELIST = {
