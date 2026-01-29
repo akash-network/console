254c91e6a44d525b5d5fc05db80ea62d2642d390
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2026-01-29 17:18:39 -0500

    fix: update default value of TEMPLATE_REFRESH_ENABLED to false in env.config.ts

--- apps/api/src/template/config/env.config.ts
+++ apps/api/src/template/config/env.config.ts
@@ -6,7 +6,7 @@ export const envSchema = z.object({
     .number()
     .optional()
     .default(15 * 60),
-  TEMPLATE_REFRESH_ENABLED: z.coerce.boolean().optional().default(true)
+  TEMPLATE_REFRESH_ENABLED: z.coerce.boolean().optional().default(false)
 });
 
 export type TemplateConfig = z.infer<typeof envSchema>;
