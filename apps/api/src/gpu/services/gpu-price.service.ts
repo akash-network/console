9000f41653386b56ea345a06a7b512646300864c
Sergii <sergiy.stotskiy@gmail.com>
2026-01-30 02:54:39 +0100

    refactor: optimizes gpu-prices endpoint

--- apps/api/src/gpu/services/gpu-price.service.ts
+++ apps/api/src/gpu/services/gpu-price.service.ts
@@ -1,5 +1,6 @@
 import { MsgCreateBid as MsgCreateBidV4 } from "@akashnetwork/akash-api/akash/market/v1beta4";
 import { MsgCreateBid as MsgCreateBidV5 } from "@akashnetwork/chain-sdk/private-types/akash.v1beta5";
+import { Day } from "@akashnetwork/database/dbSchemas/base";
 import { addDays, minutesToSeconds } from "date-fns";
 import { inject, injectable } from "tsyringe";
 
@@ -11,6 +12,7 @@ import { LoggerService } from "@src/core";
 import { DeploymentRepository } from "@src/deployment/repositories/deployment/deployment.repository";
 import { GpuRepository } from "@src/gpu/repositories/gpu.repository";
 import type { GpuBidType, GpuProviderType, GpuWithPricesType, ProviderWithBestBid } from "@src/gpu/types/gpu.type";
+import { forEachInChunks } from "@src/utils/array/array";
 import { averageBlockCountInAMonth, averageBlockCountInAnHour } from "@src/utils/constants";
 import { average, median, round, weightedAverage } from "@src/utils/math";
 import { decodeMsg, uint8arrayToString } from "@src/utils/protobuf";
@@ -54,77 +56,79 @@ export class GpuPriceService {
       throw new Error("No block found");
     }
 
-    // Fetch all deployments with GPU resources created during this period and their related MsgCreateBid messages
-    const deployments = await this.deploymentRepository.findAllWithGpuResources(firstBlockToUse.height);
-
     // Fetch all days for the period to have historical AKT prices
     const days = await this.dayRepository.getDaysAfter(addDays(new Date(), -(daysToInclude + 2)));
+    const daysById = days.reduce((group, day) => group.set(day.id, day), new Map<string, Day>());
 
-    // Decode the MsgCreateBid messages and calculate the hourly and monthly price for each bid
-    const gpuBids = deployments
-      .flatMap(d =>
-        d.relatedMessages.map(x => {
-          const day = days.find(day => day.id === x.block.dayId);
-
-          // Determine the message version and decode accordingly
-          let decodedBid: MsgCreateBidV4 | MsgCreateBidV5;
-          let provider: string;
-
-          if (x.type.includes("v1beta5")) {
-            decodedBid = decodeMsg(this.#typeRegistry, `/${MsgCreateBidV5.$type}`, x.data) as MsgCreateBidV5;
-            provider = decodedBid.id?.provider || "";
-          } else {
-            decodedBid = decodeMsg(this.#typeRegistry, `/${MsgCreateBidV4.$type}`, x.data) as MsgCreateBidV4;
-            provider = decodedBid.provider || "";
-          }
-
-          if (!day || !day.aktPrice) return null; // Ignore bids for days where we don't have the AKT price
-
-          if (decodedBid?.price?.denom !== "uakt") return null; // Ignore USDC bids for simplicity
-
-          return {
-            height: x.height,
-            txHash: x.transaction.hash,
-            datetime: x.block.datetime,
-            provider: provider,
-            aktTokenPrice: day.aktPrice,
-            hourlyPrice: this.blockPriceToHourlyPrice(parseFloat(decodedBid.price.amount), day?.aktPrice),
-            monthlyPrice: this.blockPriceToMonthlyPrice(parseFloat(decodedBid.price.amount), day?.aktPrice),
-            hourlyPriceUakt: this.blockPriceToHourlyUakt(parseFloat(decodedBid.price.amount)),
-            monthlyPriceUakt: this.blockPriceToMonthlyUakt(parseFloat(decodedBid.price.amount)),
-            deployment: {
-              owner: d.owner,
-              cpuUnits: decodedBid.resourcesOffer
-                .flatMap((r: any) => (r.resources?.cpu?.units?.val ? parseInt(uint8arrayToString(r.resources.cpu.units.val)) : 0))
-                .reduce((a: number, b: number) => a + b, 0),
-              memoryUnits: decodedBid.resourcesOffer
-                .flatMap((r: any) => (r.resources?.memory?.quantity?.val ? parseInt(uint8arrayToString(r.resources.memory.quantity.val)) : 0))
-                .reduce((a: number, b: number) => a + b, 0),
-              storageUnits: decodedBid.resourcesOffer
-                .flatMap((r: any) => r.resources?.storage)
-                .map((s: any) => (s?.quantity?.val ? parseInt(uint8arrayToString(s.quantity.val)) : 0))
-                .reduce((a: number, b: number) => a + b, 0),
-              gpus: decodedBid.resourcesOffer
-                .filter((x: any) => (x.resources?.gpu?.units?.val ? parseInt(uint8arrayToString(x.resources.gpu.units.val)) : 0) > 0)
-                .flatMap((r: any) => (r.resources?.gpu?.attributes ? this.getGpusFromAttributes(r.resources.gpu.attributes) : []))
-            },
-            data: decodedBid
-          };
-        })
-      )
-      .filter(x => x)
-      .filter(x => x?.deployment?.gpus?.length === 1) as GpuBidType[]; // Ignore bids for deployments with more than 1 GPU
-
+    // Fetch all deployments with GPU resources created during this period and their related MsgCreateBid messages
+    const deployments = this.deploymentRepository.findAllWithGpuResources({ minHeight: firstBlockToUse.height });
     const gpuModels: GpuWithPricesType[] = gpus.map(x => ({ ...x, prices: [] as GpuBidType[] }));
+    const gpusByModelAndVendor = Object.groupBy(gpuModels, x => `${x.vendor}-${x.model}`);
 
-    // Add bids to their corresponding GPU models
-    for (const bid of gpuBids) {
-      const gpu = bid.deployment.gpus[0];
-      const matchingGpuModels = gpuModels.filter(x => x.vendor === gpu.vendor && x.model === gpu.model);
+    for await (const d of deployments) {
+      // Decode the MsgCreateBid messages and calculate the hourly and monthly price for each bid
+      d.relatedMessages.forEach(x => {
+        const day = daysById.get(x.block.dayId);
 
-      for (const gpuModel of matchingGpuModels) {
-        gpuModel.prices.push(bid);
-      }
+        // Determine the message version and decode accordingly
+        let decodedBid: MsgCreateBidV4 | MsgCreateBidV5;
+        let provider: string;
+
+        if (x.type.includes("v1beta5")) {
+          decodedBid = decodeMsg(this.#typeRegistry, `/${MsgCreateBidV5.$type}`, x.data) as MsgCreateBidV5;
+          provider = decodedBid.id?.provider || "";
+        } else {
+          decodedBid = decodeMsg(this.#typeRegistry, `/${MsgCreateBidV4.$type}`, x.data) as MsgCreateBidV4;
+          provider = decodedBid.provider || "";
+        }
+
+        if (!day || !day.aktPrice) return; // Ignore bids for days where we don't have the AKT price
+
+        if (decodedBid?.price?.denom !== "uakt") return; // Ignore USDC bids for simplicity
+
+        const bidGpus = decodedBid.resourcesOffer
+          .filter((x: any) => (x.resources?.gpu?.units?.val ? parseInt(uint8arrayToString(x.resources.gpu.units.val)) : 0) > 0)
+          .flatMap((r: any) => (r.resources?.gpu?.attributes ? this.getGpusFromAttributes(r.resources.gpu.attributes) : []));
+
+        // Ignore bids for deployments with more than 1 GPU
+        if (bidGpus.length !== 1) return;
+
+        const bid = {
+          height: x.height,
+          txHash: x.transaction.hash,
+          datetime: x.block.datetime,
+          provider: provider,
+          aktTokenPrice: day.aktPrice,
+          hourlyPrice: this.blockPriceToHourlyPrice(parseFloat(decodedBid.price.amount), day?.aktPrice),
+          monthlyPrice: this.blockPriceToMonthlyPrice(parseFloat(decodedBid.price.amount), day?.aktPrice),
+          hourlyPriceUakt: this.blockPriceToHourlyUakt(parseFloat(decodedBid.price.amount)),
+          monthlyPriceUakt: this.blockPriceToMonthlyUakt(parseFloat(decodedBid.price.amount)),
+          deployment: {
+            owner: d.owner,
+            cpuUnits: decodedBid.resourcesOffer
+              .flatMap((r: any) => (r.resources?.cpu?.units?.val ? parseInt(uint8arrayToString(r.resources.cpu.units.val)) : 0))
+              .reduce((a: number, b: number) => a + b, 0),
+            memoryUnits: decodedBid.resourcesOffer
+              .flatMap((r: any) => (r.resources?.memory?.quantity?.val ? parseInt(uint8arrayToString(r.resources.memory.quantity.val)) : 0))
+              .reduce((a: number, b: number) => a + b, 0),
+            storageUnits: decodedBid.resourcesOffer
+              .flatMap((r: any) => r.resources?.storage)
+              .map((s: any) => (s?.quantity?.val ? parseInt(uint8arrayToString(s.quantity.val)) : 0))
+              .reduce((a: number, b: number) => a + b, 0),
+            gpus: bidGpus
+          },
+          data: decodedBid
+        } as GpuBidType;
+
+        const gpu = bid.deployment.gpus[0];
+        const matchingGpuModels = gpusByModelAndVendor[`${gpu.vendor}-${gpu.model}`];
+        if (!matchingGpuModels) return;
+
+        // Add bids to their corresponding GPU models
+        for (const gpuModel of matchingGpuModels) {
+          gpuModel.prices.push(bid);
+        }
+      });
     }
 
     // Sort GPUs by vendor, model, ram, interface
@@ -132,65 +136,73 @@ export class GpuPriceService {
       (a, b) => a.vendor.localeCompare(b.vendor) || a.model.localeCompare(b.model) || a.ram.localeCompare(b.ram) || a.interface.localeCompare(b.interface)
     );
 
-    const totalAllocatable = gpuModels.map(x => x.allocatable).reduce((a, b) => a + b, 0);
-    const totalAllocated = gpuModels.map(x => x.allocated).reduce((a, b) => a + b, 0);
-
-    return {
-      availability: {
-        total: totalAllocatable,
-        available: totalAllocatable - totalAllocated
-      },
-      models: gpuModels.map(x => {
-        /*
-        For each providers get their most relevent bid based on this order of priority:
+    let totalAllocatable = 0;
+    let totalAllocated = 0;
+    const models: unknown[] = [];
+    await forEachInChunks(gpuModels, async x => {
+      totalAllocatable += x.allocatable;
+      totalAllocated += x.allocated;
+      /*
+        For each providers get their most relevant bid based on this order of priority:
             1- Most recent bid from the pricing bot (those deployment have tiny cpu/ram/storage specs to improve gpu price accuracy)
             2- Cheapest bid with matching ram and interface
             3- Cheapest bid with matching ram
             4- Cheapest remaining bid
             5- If no bids are found, increase search range from 14 to 31 days and repeat steps 2-4
       */
-        const providersWithBestBid = x.providers
-          .map(p => {
-            const providerBids = x.prices.filter(b => b.provider === p.owner);
-            const providerBidsLast14d = providerBids.filter(x => x.datetime > addDays(new Date(), -14));
 
-            const pricingBotAddress = this.#gpuConfig.PRICING_BOT_ADDRESS;
-            const bidsFromPricingBot = providerBids.filter(x => x.deployment.owner === pricingBotAddress && x.deployment.cpuUnits === 100);
+      const lastBidsForPeriod = addDays(new Date(), -14);
+      const pricingBotAddress = this.#gpuConfig.PRICING_BOT_ADDRESS;
+      const pricesByProvider = Map.groupBy(x.prices, b => b.provider);
 
-            let bestBid = null;
-            if (bidsFromPricingBot.length > 0) {
-              bestBid = bidsFromPricingBot.sort((a, b) => b.height - a.height)[0];
-            } else {
-              bestBid = this.findBestProviderBid(providerBidsLast14d, x) ?? this.findBestProviderBid(providerBids, x);
-            }
+      const providersWithBestBid: ProviderWithBestBid[] = [];
+      x.providers.forEach(p => {
+        const providerBids = pricesByProvider.get(p.owner) ?? [];
+        const providerBidsLast14d = providerBids.filter(b => b.datetime > lastBidsForPeriod);
+        const bidsFromPricingBot = providerBids.filter(b => b.deployment.owner === pricingBotAddress && b.deployment.cpuUnits === 100);
 
-            return {
-              provider: p,
-              bestBid: bestBid
-            };
-          })
-          .filter(x => x.bestBid) as ProviderWithBestBid[];
+        let bestBid = null;
+        if (bidsFromPricingBot.length > 0) {
+          bestBid = bidsFromPricingBot.reduce((best, bid) => (bid.height > best.height ? bid : best));
+        } else {
+          bestBid = this.findBestProviderBid(providerBidsLast14d, x) ?? this.findBestProviderBid(providerBids, x);
+        }
 
-        return {
-          vendor: x.vendor,
-          model: x.model,
-          ram: x.ram,
-          interface: x.interface,
-          availability: {
-            total: x.allocatable,
-            available: x.allocatable - x.allocated
-          },
-          providerAvailability: {
-            total: x.providers.length,
-            available: x.availableProviders.length,
-            providers: debug ? x.providers : undefined
-          },
-          price: this.getPricing(providersWithBestBid),
-          priceUakt: this.getPricingUakt(providersWithBestBid),
-          bidCount: debug ? x.prices.length : undefined,
-          providersWithBestBid: debug ? providersWithBestBid : undefined
-        };
-      })
+        if (bestBid) {
+          providersWithBestBid.push({
+            provider: p,
+            bestBid: bestBid
+          });
+        }
+      });
+
+      models.push({
+        vendor: x.vendor,
+        model: x.model,
+        ram: x.ram,
+        interface: x.interface,
+        availability: {
+          total: x.allocatable,
+          available: x.allocatable - x.allocated
+        },
+        providerAvailability: {
+          total: x.providers.length,
+          available: x.availableProviders.length,
+          providers: debug ? x.providers : undefined
+        },
+        price: this.getPricing(providersWithBestBid),
+        priceUakt: this.getPricingUakt(providersWithBestBid),
+        bidCount: debug ? x.prices.length : undefined,
+        providersWithBestBid: debug ? providersWithBestBid : undefined
+      });
+    });
+
+    return {
+      availability: {
+        total: totalAllocatable,
+        available: totalAllocatable - totalAllocated
+      },
+      models
     };
   }
 
