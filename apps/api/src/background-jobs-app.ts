3f5bfbad905305676c30a0a8eddcc055e6886926
Sergii <sergiy.stotskiy@gmail.com>
2026-02-03 14:48:02 +0100

    fix: adds execution context to http handling in bg jobs server

--- apps/api/src/background-jobs-app.ts
+++ apps/api/src/background-jobs-app.ts
@@ -1,13 +1,17 @@
+import { HttpLoggerInterceptor } from "@akashnetwork/logging/hono";
 import { createOtelLogger } from "@akashnetwork/logging/otel";
 import { Hono } from "hono";
 import { container } from "tsyringe";
 
 import { CORE_CONFIG, JobQueueService, startServer } from "@src/core";
 import { healthzRouter } from "@src/healthz/routes/healthz.router";
+import { RequestContextInterceptor } from "./core/services/request-context-interceptor/request-context.interceptor";
 
 export async function bootstrap(): Promise<void> {
   const app = new Hono();
 
+  app.use(container.resolve(HttpLoggerInterceptor).intercept());
+  app.use(container.resolve(RequestContextInterceptor).intercept());
   app.route("/", healthzRouter);
 
   const server = await startServer(app, createOtelLogger({ context: "BACKGROUND_JOBS" }), process, { port: container.resolve(CORE_CONFIG).PORT });
