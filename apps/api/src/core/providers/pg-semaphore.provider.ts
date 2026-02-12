5ff03f5dc73318ad8aa9f452cee8c9bd843f4027
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2026-02-12 11:19:17 -0500

    fix: switch to in-memory semaphore (#2718)

--- apps/api/src/core/providers/pg-semaphore.provider.ts
+++ apps/api/src/core/providers/pg-semaphore.provider.ts
@@ -1,24 +1,26 @@
-import postgres from "postgres";
-import { container, instancePerContainerCachingFactory } from "tsyringe";
+// import postgres from "postgres";
+// import { container, instancePerContainerCachingFactory } from "tsyringe";
 
-import { DisposableRegistry } from "@src/core/lib/disposable-registry/disposable-registry";
+// import { DisposableRegistry } from "@src/core/lib/disposable-registry/disposable-registry";
 import { SemaphoreFactory } from "@src/core/lib/pg-semaphore";
-import { APP_INITIALIZER, ON_APP_START } from "@src/core/providers/app-initializer";
-import { CORE_CONFIG } from "@src/core/providers/config.provider";
+// import { APP_INITIALIZER, ON_APP_START } from "@src/core/providers/app-initializer";
+// import { CORE_CONFIG } from "@src/core/providers/config.provider";
 
-container.register(APP_INITIALIZER, {
-  useFactory: instancePerContainerCachingFactory(c => {
-    const config = c.resolve(CORE_CONFIG);
-    const client = postgres(config.POSTGRES_DB_URI, {
-      max: 1,
-      idle_timeout: 30,
-      connect_timeout: 10
-    });
-    c.resolve(DisposableRegistry).register({ dispose: () => client.end() });
-    SemaphoreFactory.configure(client);
+SemaphoreFactory.useMemory();
 
-    return {
-      [ON_APP_START]: async () => {}
-    };
-  })
-});
+// container.register(APP_INITIALIZER, {
+//   useFactory: instancePerContainerCachingFactory(c => {
+//     const config = c.resolve(CORE_CONFIG);
+//     const client = postgres(config.POSTGRES_DB_URI, {
+//       max: 1,
+//       idle_timeout: 30,
+//       connect_timeout: 10
+//     });
+//     c.resolve(DisposableRegistry).register({ dispose: () => client.end() });
+//     SemaphoreFactory.configure(client);
+
+//     return {
+//       [ON_APP_START]: async () => {}
+//     };
+//   })
+// });
