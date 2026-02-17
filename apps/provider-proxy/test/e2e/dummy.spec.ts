0eae7bb9db1a1ffad228fd984725195a731d81d1
Serhii Stotskyi <serhii@akash.network>
2026-02-17 18:14:05 +0100

    chore: adds e2e tests for provider-proxy (#2755)

--- apps/provider-proxy/test/e2e/dummy.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("Place for future e2e tests", () => {
-  it("temporary just do nothing test to setup workflows", () => {
-    expect(true).toBe(true);
-  });
-});
