ffaac516d5c8a5185388eb33a404412ef67cf358
Serhii Stotskyi <serhii@akash.network>
2026-02-18 10:25:29 +0100

    refactor: migrates provider-proxy build to tsup (#2735)
    * refactor: migrates provider-proxy build to tsup
    
    * chore: fixes ci comments
    
    * chore: adds esbuild to trusted deps
    
    * chore: adds optional rollup dep for linux platform

--- apps/provider-proxy/src/services/WebsocketStats.ts
+++ apps/provider-proxy/src/services/WebsocketStats.ts
@@ -1,10 +1,10 @@
-import { v4 as uuidv4 } from "uuid";
+import { randomUUID } from "node:crypto";
 
 export class WebsocketStats {
   private items: ClientWebSocketStats[] = [];
 
   create(): ClientWebSocketStats {
-    const item = new ClientWebSocketStats(uuidv4());
+    const item = new ClientWebSocketStats(randomUUID());
     this.items.push(item);
 
     if (this.items.length > 100_000) {
