1e84f0042ca6e904a13a8051d7276b7c3b464136
Sergii <sergiy.stotskiy@gmail.com>
2026-02-20 13:54:31 +0100

    refactor: migrate user related models to drizzle

--- apps/api/src/routers/dashboardRouter.ts
+++ apps/api/src/routers/dashboardRouter.ts
@@ -1,22 +1,23 @@
-import { Template, UserSetting } from "@akashnetwork/database/dbSchemas/user";
 import { Hono } from "hono";
+import { container } from "tsyringe";
 
 import { privateMiddleware } from "@src/middlewares/privateMiddleware";
+import { UserRepository } from "@src/user/repositories/user/user.repository";
+import { UserTemplateRepository } from "@src/user/repositories/user-template/user-template.repository";
 
 export const dashboardRouter = new Hono();
 
 dashboardRouter.use("*", privateMiddleware);
 
 dashboardRouter.get("/stats", async c => {
-  const userCountRequest = UserSetting.count();
-  const publicTemplateCountRequest = Template.count({
-    where: { isPublic: true }
-  });
-  const privateTemplateCountRequest = Template.count({
-    where: { isPublic: false }
-  });
+  const userRepository = container.resolve(UserRepository);
+  const templateRepository = container.resolve(UserTemplateRepository);
 
-  const [userCount, publicTemplateCount, privateTemplateCount] = await Promise.all([userCountRequest, publicTemplateCountRequest, privateTemplateCountRequest]);
+  const [userCount, publicTemplateCount, privateTemplateCount] = await Promise.all([
+    userRepository.count(),
+    templateRepository.count({ isPublic: true }),
+    templateRepository.count({ isPublic: false })
+  ]);
 
   return c.json({
     userCount,
