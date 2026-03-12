beb47ecf9ae5106f092d82d321b987b6bf7ca256
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2026-03-12 10:36:51 +0100

    chore(config): removes some redundant env vars

--- apps/api/src/auth/config/env.config.ts
+++ apps/api/src/auth/config/env.config.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-  ANONYMOUS_USER_TOKEN_SECRET: z.string(),
   AUTH0_M2M_DOMAIN: z.string(),
   AUTH0_M2M_CLIENT_ID: z.string(),
   AUTH0_M2M_SECRET: z.string(),
