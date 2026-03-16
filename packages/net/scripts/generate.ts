5b2a6730fc66d16c0b50eb88ccc510b6e583ba25
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2026-03-15 20:02:58 -0500

    feat(config): switch from testnet-oracle to testnet-upgrade

--- packages/net/scripts/generate.ts
+++ packages/net/scripts/generate.ts
@@ -43,11 +43,27 @@ async function main() {
       fetchText(`${baseConfigUrl}/faucet-url.txt`).catch(() => null)
     ]);
 
+    let apiUrls = meta?.apis?.rest?.map(({ address }) => address) ?? [];
+    let rpcUrls = meta?.apis?.rpc?.map(({ address }) => address) ?? [];
+    let version = meta?.codebase?.recommended_version ?? null;
+
+    if (!apiUrls.length && !rpcUrls.length) {
+      const [apiNode, rpcNode, versionTxt] = await Promise.all([
+        fetchText(`${baseConfigUrl}/api-nodes.txt`).catch(() => null),
+        fetchText(`${baseConfigUrl}/rpc-nodes.txt`).catch(() => null),
+        fetchText(`${baseConfigUrl}/version.txt`).catch(() => null)
+      ]);
+
+      if (apiNode?.trim()) apiUrls = [apiNode.trim()];
+      if (rpcNode?.trim()) rpcUrls = [rpcNode.trim()];
+      if (versionTxt?.trim()) version = versionTxt.trim();
+    }
+
     const networkConfig = {
-      version: meta?.codebase?.recommended_version ?? null,
+      version,
       faucetUrl: faucetUrl?.trim() ?? null,
-      apiUrls: meta?.apis?.rest?.map(({ address }) => address) ?? [],
-      rpcUrls: meta?.apis?.rpc?.map(({ address }) => address) ?? []
+      apiUrls,
+      rpcUrls
     };
 
     if (networkConfig.apiUrls.length || networkConfig.rpcUrls.length) {
