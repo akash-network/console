e94076ef0f1c737d76df8e816f7ff6287b1d70bd
Sergii <sergiy.stotskiy@gmail.com>
2026-03-30 14:36:20 +0200

    fix: enables lazy initialization for network configuration

--- apps/deploy-web/src/pages/api/blockchain-config.ts
+++ apps/deploy-web/src/pages/api/blockchain-config.ts
@@ -3,6 +3,7 @@ import { netConfig } from "@akashnetwork/net";
 import { z } from "zod";
 
 import { defineApiHandler } from "@src/lib/nextjs/defineApiHandler/defineApiHandler";
+import type { AppServices } from "@src/services/app-di-container/server-di-container.service";
 
 export default defineApiHandler({
   route: "/api/blockchain-config",
@@ -12,33 +13,48 @@ export default defineApiHandler({
     })
   }),
   async handler({ query, res, services }) {
-    const config = {
-      mainnet: [
+    const allNetworksConfig = createNetworksConfig(services.privateConfig);
+    let networkConfig: Array<ReturnType<typeof nodeWithId>>;
+    try {
+      networkConfig = allNetworksConfig[query.network];
+    } catch (error) {
+      res.status(422).json({ error: `Unable to fetch ${query.network} blockchain network config. Does this network exist?` });
+      return;
+    }
+
+    res.status(200).json(networkConfig);
+  }
+});
+
+function createNetworksConfig(privateConfig: Pick<AppServices["privateConfig"], "DEFAULT_REST_API_NODE_URL_MAINNET" | "DEFAULT_RPC_NODE_URL_MAINNET">) {
+  const networks = {
+    get mainnet() {
+      return [
         nodeWithId({
-          api: services.privateConfig.DEFAULT_REST_API_NODE_URL_MAINNET ?? netConfig.getBaseAPIUrl("mainnet"),
-          rpc: services.privateConfig.DEFAULT_RPC_NODE_URL_MAINNET ?? netConfig.getBaseRpcUrl("mainnet")
+          api: privateConfig.DEFAULT_REST_API_NODE_URL_MAINNET ?? netConfig.getBaseAPIUrl("mainnet"),
+          rpc: privateConfig.DEFAULT_RPC_NODE_URL_MAINNET ?? netConfig.getBaseRpcUrl("mainnet")
         })
-      ],
-      sandbox: [
+      ];
+    },
+    get sandbox() {
+      return [
         nodeWithId({
           api: netConfig.getBaseAPIUrl("sandbox"),
           rpc: netConfig.getBaseRpcUrl("sandbox")
         })
-      ],
-      testnet: [
+      ];
+    },
+    get testnet() {
+      return [
         nodeWithId({
           api: netConfig.getBaseAPIUrl("testnet"),
           rpc: netConfig.getBaseRpcUrl("testnet")
         })
-      ]
-    };
-    const data = config[query.network]?.map(node => {
-      node.id = new URL(node.api).hostname;
-      return node;
-    });
-    res.status(200).json(data);
-  }
-});
+      ];
+    }
+  };
+  return networks;
+}
 
 function nodeWithId(node: { api: string; rpc: string }) {
   return {
