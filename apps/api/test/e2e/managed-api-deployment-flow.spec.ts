30f9f2219460fdc671e78dd66742fa57668f50c2
Console Developer <1159966+stalniy@users.noreply.github.com>
2026-09-08 12:33:51 +0200

    test: adds e2e test for patching env varibles in deployment

--- apps/api/test/e2e/managed-api-deployment-flow.spec.ts
+++ apps/api/test/e2e/managed-api-deployment-flow.spec.ts
@@ -5,6 +5,7 @@ import { ConstantBackoff, handleWhenResult, retry } from "cockatiel";
 import { CompactEncrypt } from "jose";
 import fs from "node:fs";
 import path from "node:path";
+import { setTimeout as delay } from "node:timers/promises";
 import { describe, expect, it } from "vitest";
 import { z } from "zod";
 
@@ -183,31 +184,13 @@ describe("Managed Wallet API Deployment Flow", () => {
             count: 1
     `;
 
-    const { data: sdlSecretsContext } = await api.v1.getSDLSecretsContext();
-    const pubKey = await crypto.subtle.importKey("jwk", sdlSecretsContext.jwk, { name: "RSA-OAEP", hash: "SHA-256" }, false, ["encrypt"]);
-    const sealedSecrets = await new CompactEncrypt(
-      new TextEncoder().encode(
-        JSON.stringify({
-          TEST_SECRET: "secret-value",
-          TEST_ANOTHER_SECRET: "another secret value"
-        })
-      )
-    )
-      .setProtectedHeader({
-        alg: "RSA-OAEP-256",
-        enc: "A256GCM",
-        kid: sdlSecretsContext.kid,
-        sub: sdlSecretsContext.sub,
-        exp: Math.floor(Date.now() / 1000) + 5 * 60
-      })
-      .encrypt(pubKey);
-
     console.log("Creating deployment...");
-    const deploymentResponse = await api.v1.createDeployment({
-      data: {
-        sdl,
-        sealedSecrets
-      } as any // sealedSecrets currently is a hidden field in the API spec
+    const deploymentResponse = await createDeployment(api, {
+      sdl,
+      secrets: {
+        TEST_SECRET: "secret-value",
+        TEST_ANOTHER_SECRET: "another secret value"
+      }
     });
 
     expect(deploymentResponse.data).toMatchObject({
@@ -266,17 +249,113 @@ describe("Managed Wallet API Deployment Flow", () => {
           })
         })
       });
-
-      const closedDeploymentResponse = await api.v1.closeDeployment({
-        dseq: deploymentResponse.data.dseq
-      });
-      expect(closedDeploymentResponse.data.success).toBe(true);
-    } catch (e) {
-      // Final step: Close deployment in case of error to release escrow funds and cleanup
+    } finally {
       await api.v1.closeDeployment({
         dseq: deploymentResponse.data.dseq
       });
-      throw e;
+    }
+  });
+
+  it("can patch deployment's env variables", { timeout: 2 * 60 * 1000 }, async () => {
+    const { api } = await setup();
+    const sdl = `
+      version: "2.0"
+      services:
+        web:
+          image: ghcr.io/akash-network/hello-akash-world:2.1.0
+          env:
+            - TEST_ENV=ac-secret://TEST_SECRET
+            - TEST_ANOTHER_ENV=ac-secret://TEST_ANOTHER_SECRET
+            - TEST_VAR=test me
+          expose:
+            - port: 3000
+              as: 80
+              to:
+                - global: true
+      profiles:
+        compute:
+          web:
+            resources:
+              cpu:
+                units: 0.5
+              memory:
+                size: 512Mi
+              storage:
+                - size: 512Mi
+        placement:
+          dcloud:
+            pricing:
+              web:
+                denom: uact
+                amount: 1000
+      deployment:
+        web:
+          dcloud:
+            profile: web
+            count: 1
+    `;
+
+    console.log("Creating deployment...");
+    const deploymentResponse = await createDeployment(api, {
+      sdl,
+      secrets: {
+        TEST_SECRET: "secret-value",
+        TEST_ANOTHER_SECRET: "another secret value"
+      }
+    });
+
+    try {
+      const bid = await waitForBids(api, deploymentResponse.data.dseq);
+      await api.v1.createLease({
+        leases: [
+          {
+            dseq: bid.bid.id.dseq,
+            gseq: bid.bid.id.gseq,
+            oseq: bid.bid.id.oseq,
+            provider: bid.bid.id.provider
+          }
+        ],
+        manifest: deploymentResponse.data.manifest
+      });
+
+      await delay(5000); // Wait for the lease to be active
+
+      const patchResponse = await api.v1.patchDeployment({
+        dseq: deploymentResponse.data.dseq,
+        data: {
+          services: {
+            web: {
+              env: {
+                TEST_ENV: "ac-secret://NEW_TEST_SECRET",
+                NEW_REGULAR_VAR: "new-value",
+                NEW_DATABASE_URL: "ac-secret://WEB_DATABASE_URL"
+              }
+            }
+          },
+          sealedSecrets: await encryptSecrets(api, {
+            NEW_TEST_SECRET: "new-secret-value",
+            WEB_DATABASE_URL: "postgres://user:password@host:5432/db"
+          }) // this field is hidden in the API spec
+        } as any
+      });
+
+      expect(patchResponse.data).toMatchObject({
+        deployment: expect.objectContaining({
+          id: expect.objectContaining({
+            dseq: deploymentResponse.data.dseq
+          })
+        })
+      });
+
+      const updatedDeployment = await api.v2.getDeploymentSetting({ dseq: deploymentResponse.data.dseq });
+      expect(updatedDeployment.data).toMatchObject({
+        dseq: deploymentResponse.data.dseq,
+        sdl: expect.stringMatching(/TEST_ENV=ac-secret:\/\/NEW_TEST_SECRET|NEW_REGULAR_VAR=new-value|NEW_DATABASE_URL=ac-secret:\/\/WEB_DATABASE_URL/)
+      });
+    } finally {
+      await api.v1.closeDeployment({
+        dseq: deploymentResponse.data.dseq
+      });
     }
   });
 
@@ -306,5 +385,47 @@ describe("Managed Wallet API Deployment Flow", () => {
 
       return data[0];
     });
+  }
+
+  async function createDeployment(api: Awaited<ReturnType<typeof setup>>["api"], input: { sdl: string; secrets?: Record<string, string> }) {
+    let sealedSecrets: string | undefined = undefined;
+
+    if (input.secrets) {
+      const { data: sdlSecretsContext } = await api.v1.getSDLSecretsContext();
+      const pubKey = await crypto.subtle.importKey("jwk", sdlSecretsContext.jwk, { name: "RSA-OAEP", hash: "SHA-256" }, false, ["encrypt"]);
+      sealedSecrets = await new CompactEncrypt(new TextEncoder().encode(JSON.stringify(input.secrets)))
+        .setProtectedHeader({
+          alg: "RSA-OAEP-256",
+          enc: "A256GCM",
+          kid: sdlSecretsContext.kid,
+          sub: sdlSecretsContext.sub,
+          exp: Math.floor(Date.now() / 1000) + 5 * 60
+        })
+        .encrypt(pubKey);
+    }
+
+    const deploymentResponse = await api.v1.createDeployment({
+      data: {
+        sdl: input.sdl,
+        sealedSecrets
+      } as any // sealedSecrets currently is a hidden field in the API spec
+    });
+    return deploymentResponse;
+  }
+
+  async function encryptSecrets(api: Awaited<ReturnType<typeof setup>>["api"], secrets: Record<string, string>) {
+    const { data: sdlSecretsContext } = await api.v1.getSDLSecretsContext();
+    const pubKey = await crypto.subtle.importKey("jwk", sdlSecretsContext.jwk, { name: "RSA-OAEP", hash: "SHA-256" }, false, ["encrypt"]);
+    const sealedSecrets = await new CompactEncrypt(new TextEncoder().encode(JSON.stringify(secrets)))
+      .setProtectedHeader({
+        alg: "RSA-OAEP-256",
+        enc: "A256GCM",
+        kid: sdlSecretsContext.kid,
+        sub: sdlSecretsContext.sub,
+        exp: Math.floor(Date.now() / 1000) + 5 * 60
+      })
+      .encrypt(pubKey);
+
+    return sealedSecrets;
   }
 });
